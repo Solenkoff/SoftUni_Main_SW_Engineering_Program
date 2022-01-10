4a257a67d264d1c06951b645a738a38a50f3bb31
Solenkoff <solenkoff@yahoo.com>
2022-01-10 02:06:41 +0200

    Adding homework assignment

---
+++ CSharp_Programming_Basics/L01_First Steps_In_Coding_Lab/P01_Hello_SoftUni/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_Hello_SoftUni
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
