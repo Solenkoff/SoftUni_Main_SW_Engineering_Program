d3780967b26b08079982deac03b119499c5b4800
Solenkoff <solenkoff@yahoo.com>
2022-01-12 00:08:39 +0200

    Adding a homework solution

---
+++ CSharp_Programming_Basics/L02_First_Steps_In_Coding_Exercise/P01_USD_To_BGN/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_USD_To_BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double USD = double.Parse(Console.ReadLine());
+            double converterUsdForBg = 1.79549;
+            double bgn = USD * converterUsdForBg;
+
+            Console.WriteLine(bgn);
+        }
+    }
+}
