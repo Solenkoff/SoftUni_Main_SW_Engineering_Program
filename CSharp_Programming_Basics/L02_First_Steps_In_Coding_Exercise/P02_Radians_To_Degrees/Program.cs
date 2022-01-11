874a4e63f1434311a2df879adcbcd360fc7ad78a
Solenkoff <solenkoff@yahoo.com>
2022-01-12 00:58:39 +0200

    Adding a homework solution

---
+++ CSharp_Programming_Basics/L02_First_Steps_In_Coding_Exercise/P02_Radians_To_Degrees/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02_Radians_To_Degrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radians = double.Parse(Console.ReadLine());
+            double grade = Math.Round(radians * 180 / Math.PI);
+
+            Console.WriteLine(grade);
+        }
+    }
+}
