1c0f6e7a6dfb3230b36daebdbc3cabf9290afa66
Solenkoff <solenkoff@yahoo.com>
2022-01-12 00:58:39 +0200

    Adding a homework solution

---
+++ CSharp_Programming_Basics/L02_First_Steps_In_Coding_Exercise/P03_Deposit_Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03_Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositAmount = double.Parse(Console.ReadLine());
+            int depositTime = int.Parse(Console.ReadLine());
+            double anualInterest = double.Parse(Console.ReadLine());
+            double amount = depositAmount + (depositTime * (depositAmount * anualInterest / 100) / 12);
+
+            Console.WriteLine(amount);
+        }
+    }
+}
