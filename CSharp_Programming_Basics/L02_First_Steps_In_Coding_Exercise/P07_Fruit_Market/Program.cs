6560ce0eaac20ae6eb84ebc9eac536c845049f89
Solenkoff <solenkoff@yahoo.com>
2022-01-14 16:01:35 +0200

    Adding a homework solution

---
+++ CSharp_Programming_Basics/L02_First_Steps_In_Coding_Exercise/P07_Fruit_Market/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07_Fruit_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double berriesPrice = double.Parse(Console.ReadLine());
+            double bananaQuantity = double.Parse(Console.ReadLine());
+            double orangeQuantity = double.Parse(Console.ReadLine());
+            double raspberryQuantity = double.Parse(Console.ReadLine());
+            double berriesQuantity = double.Parse(Console.ReadLine());
+
+            double raspberriesPrice = berriesPrice / 2;
+            double orangePrice = raspberriesPrice - (raspberriesPrice * 0.40);
+            double banansPrice = raspberriesPrice - (raspberriesPrice * 0.80);
+
+
+            double budget = berriesPrice * berriesQuantity + bananaQuantity * banansPrice + orangePrice * orangeQuantity + raspberriesPrice * raspberryQuantity;
+            Console.WriteLine($"{budget:f2}");
+        }
+    }
+}
