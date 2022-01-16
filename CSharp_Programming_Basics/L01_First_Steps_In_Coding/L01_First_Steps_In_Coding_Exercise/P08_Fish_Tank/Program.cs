a482f30b2052f3c15b728aeaa2595d14421c6a9c
Solenkoff <solenkoff@yahoo.com>
2022-01-16 18:59:50 +0200

    Rearranged folders/files

---
+++ CSharp_Programming_Basics/L01_First_Steps_In_Coding/L01_First_Steps_In_Coding_Exercise/P08_Fish_Tank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08_Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+            double volumeL = lenght * width * height * 0.001;
+            double litters = volumeL * (1 - percentage / 100);
+
+            Console.WriteLine(litters);
+        }
+    }
+}
---
+++ CSharp_Programming_Basics/L01_First_Steps_In_Coding/L01_First_Steps_In_Coding__Lab/P04_Inches_to_Centimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _04_Inches_to_Centimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inches = double.Parse(Console.ReadLine());
+            double centimetres = inches * 2.54;
+            Console.WriteLine(centimetres);
+        }
+    }
+}
