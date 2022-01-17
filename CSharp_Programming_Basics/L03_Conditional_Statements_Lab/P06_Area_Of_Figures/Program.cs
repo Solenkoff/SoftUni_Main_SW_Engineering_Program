3cc6d37846cd3e8c8b57856841d2d7238afbc158
Solenkoff <solenkoff@yahoo.com>
2022-01-17 19:21:08 +0200

    Added homework solution

---
+++ CSharp_Programming_Basics/L03_Conditional_Statements_Lab/P06_Area_Of_Figures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06_Area_Of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            double area = 0;
+
+            if (figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                area = side * side;
+            }
+            else if (figure == "rectangle")
+            {
+                double side1 = double.Parse(Console.ReadLine());
+                double side2 = double.Parse(Console.ReadLine());
+                area = side1 * side2;
+            }
+            else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = Math.PI * (radius * radius);
+            }
+            else if (figure == "triangle")
+            {
+                double side1 = double.Parse(Console.ReadLine());
+                double side2 = double.Parse(Console.ReadLine());
+                area = (side1 * side2) / 2;
+            }
+            Console.WriteLine($"{area:f3}");
+        }
+    }
+}
