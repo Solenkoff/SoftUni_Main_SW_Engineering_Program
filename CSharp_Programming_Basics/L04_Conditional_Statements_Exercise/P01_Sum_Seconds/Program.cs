15a41167cb16a3dfc0fb7ba42982741b6b72a8b3
Solenkoff <solenkoff@yahoo.com>
2022-01-19 01:23:16 +0200

    Added folder and solutions

---
+++ CSharp_Programming_Basics/L04_Conditional_Statements_Exercise/P01_Sum_Seconds/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01_Sum_Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time1 = int.Parse(Console.ReadLine());
+            int time2 = int.Parse(Console.ReadLine());
+            int time3 = int.Parse(Console.ReadLine());
+            int timeAllSec = time1 + time2 + time3;
+            int min = timeAllSec / 60;
+            int sec = timeAllSec % 60;
+
+
+            Console.WriteLine($"{min}:{sec:d2}");
+        }
+    }
+}
