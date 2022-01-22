2ab71be9d2dc90720241f209469009c03646d7d1
Solenkoff <solenkoff@yahoo.com>
2022-01-22 11:49:00 +0200

    Added homework solution

---
+++ CSharp_Programming_Basics/L04_Conditional_Statements_Exercise/P06_Godzilla_Vs_Kong/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06_Godzilla_Vs._Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statistsNum = int.Parse(Console.ReadLine());
+            double priceClothesperStat = double.Parse(Console.ReadLine());
+
+
+            double finalStatistsClothesprice = statistsNum * priceClothesperStat;
+            budget -= budget * 0.10;
+            if (statistsNum > 150)
+            {
+                finalStatistsClothesprice -= finalStatistsClothesprice * 0.10;
+            }
+            budget -= finalStatistsClothesprice;
+
+            if (budget >= 0)
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budget:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {Math.Abs(budget):f2} leva more.");
+            }
+        }
+    }
+}
