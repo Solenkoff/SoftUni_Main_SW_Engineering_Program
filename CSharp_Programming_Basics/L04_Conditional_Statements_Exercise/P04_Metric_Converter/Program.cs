e212f9c7dc69a776a63a91f7b5363233f787f432
Solenkoff <solenkoff@yahoo.com>
2022-01-22 11:46:40 +0200

    Added homework solution

---
+++ CSharp_Programming_Basics/L04_Conditional_Statements_Exercise/P04_Metric_Converter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _04_Metric_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string output = Console.ReadLine();
+            double result = 0;
+
+            if (input == "mm" && output == "m")
+            {
+                result = number / 1000;
+            }
+            else if (input == "m" && output == "mm")
+            {
+                result = number * 1000;
+            }
+            else
+            if (input == "cm" && output == "m")
+            {
+                result = number / 100;
+            }
+            else if (input == "m" && output == "cm")
+            {
+                result = number * 100;
+            }
+            else if (input == "mm" && output == "cm")
+            {
+                result = number / 10;
+            }
+            else if (input == "cm" && output == "mm")
+            {
+                result = number * 10;
+            }
+
+            Console.WriteLine($"{ result:f3}");
+        }
+    }
+}
