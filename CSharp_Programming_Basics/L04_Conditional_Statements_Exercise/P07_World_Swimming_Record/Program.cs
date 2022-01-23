a74954b5af4de3b6d592aaf461e0b40403c5e9a8
Solenkoff <solenkoff@yahoo.com>
2022-01-23 22:07:06 +0200

    Added homework solution

---
+++ CSharp_Programming_Basics/L04_Conditional_Statements_Exercise/P07_World_Swimming_Record/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07_World_Swimming_Record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSec = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timeInSecPerM = double.Parse(Console.ReadLine());
+            double time = distance * timeInSecPerM;
+            double delay = Math.Floor(distance / 15) * 12.5;
+            double allTime = delay + time;
+            if (recordInSec > allTime)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {allTime:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {allTime - recordInSec:f2} seconds slower.");
+            }
+        }
+    }
+}
