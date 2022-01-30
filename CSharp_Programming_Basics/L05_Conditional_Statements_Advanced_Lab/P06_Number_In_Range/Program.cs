e40010575251b816c5c6b95bea3bb3269bb899db
Solenkoff <solenkoff@yahoo.com>
2022-01-30 23:35:55 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L05_Conditional_Statements_Advanced_Lab/P06_Number_In_Range/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06_Number_In_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            bool isValid = number >= -100 && number <= 100 && number != 0;
+            if (isValid)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
