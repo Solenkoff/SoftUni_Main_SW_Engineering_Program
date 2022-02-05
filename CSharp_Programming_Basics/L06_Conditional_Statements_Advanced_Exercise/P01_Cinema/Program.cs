88c31488519974f91439798cbac4dfa8c8d07449
Solenkoff <solenkoff@yahoo.com>
2022-02-05 20:32:31 +0100

    Added folder and solutions

---
+++ CSharp_Programming_Basics/L06_Conditional_Statements_Advanced_Exercise/P01_Cinema/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01_Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projection = Console.ReadLine();
+            int row = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+            double income = 0;
+
+            switch (projection)
+            {
+                case "Premiere":
+                    income = row * colums * 12;
+                    break;
+                case "Normal":
+                    income = row * colums * 7.50;
+                    break;
+                case "Discount":
+                    income = row * colums * 5.00;
+                    break;
+            }
+            Console.WriteLine($"{income:f2} leva");
+        }
+    }
+}
