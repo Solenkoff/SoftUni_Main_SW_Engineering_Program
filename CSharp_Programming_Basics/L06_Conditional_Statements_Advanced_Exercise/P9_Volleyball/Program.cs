cf8b6fb14b1fde8c6b4cd7a72549399be72491c5
Solenkoff <solenkoff@yahoo.com>
2022-02-08 16:08:03 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L06_Conditional_Statements_Advanced_Exercise/P9_Volleyball/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _9_Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year = Console.ReadLine();
+            int holidays = int.Parse(Console.ReadLine());
+            int numWeekends = int.Parse(Console.ReadLine());
+
+            double weekendsInSofia = 48 - numWeekends;
+            double weekendsInSofiaForPlay = weekendsInSofia * 3.0 / 4.0;
+            double holidaysInSofia = holidays * 2.0 / 3.0;
+
+            double play = holidaysInSofia + weekendsInSofiaForPlay + numWeekends;
+
+            if (year == "leap")
+            {
+                play *= 1.15;
+                Console.WriteLine(Math.Floor(play));
+            }
+            else
+            {
+                Console.WriteLine(Math.Floor(play));
+            }
+        }
+    }
+}
