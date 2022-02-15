f41a76679a5b3a807e71bfa0fb9299b1b7555288
Solenkoff <solenkoff@yahoo.com>
2022-02-16 00:50:09 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L08_For_Loop_Exercise/P04_Histogram/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _04_Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p1 = 0;
+            int p2 = 0;
+            int p3 = 0;
+            int p4 = 0;
+            int p5 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < 200)
+                {
+                    p1++;
+                }
+                else if (number < 400)
+                {
+                    p2++;
+                }
+                else if (number < 600)
+                {
+                    p3++;
+                }
+                else if (number < 800)
+                {
+                    p4++;
+                }
+                else
+                {
+                    p5++;
+                }
+            }
+            double pp1 = p1 * 1.0 / n * 100;
+            double pp2 = p2 * 1.0 / n * 100;
+            double pp3 = p3 * 1.0 / n * 100;
+            double pp4 = p4 * 1.0 / n * 100;
+            double pp5 = p5 * 1.0 / n * 100;
+            Console.WriteLine($"{pp1:f2}%");
+            Console.WriteLine($"{pp2:f2}%");
+            Console.WriteLine($"{pp3:f2}%");
+            Console.WriteLine($"{pp4:f2}%");
+            Console.WriteLine($"{pp5:f2}%");
+        }
+    }
+}
