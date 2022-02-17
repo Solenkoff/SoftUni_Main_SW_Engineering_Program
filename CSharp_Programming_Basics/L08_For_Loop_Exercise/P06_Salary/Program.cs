e9c96619387c8b97aaa5ba37e1c0bba3c84bfe40
Solenkoff <solenkoff@yahoo.com>
2022-02-17 01:46:42 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L08_For_Loop_Exercise/P06_Salary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06_Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int openBrowsers = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i <= openBrowsers; i++)
+            {
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break;
+                }
+                string browsers = Console.ReadLine();
+                if (browsers == "Facebook")
+                {
+                    salary -= 150;
+                }
+                else if (browsers == "Instagram")
+                {
+                    salary -= 100;
+                }
+                else if (browsers == "Reddit")
+                {
+                    salary -= 50;
+                }
+            }
+            if (salary > 0)
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
