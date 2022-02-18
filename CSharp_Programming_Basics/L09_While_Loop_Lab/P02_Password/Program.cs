02886665eb890d1dfe1d961cd695165ed3688512
Solenkoff <solenkoff@yahoo.com>
2022-02-19 00:33:29 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L09_While_Loop_Lab/P02_Password/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02_Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string password = Console.ReadLine();
+            string input = Console.ReadLine();
+            for (; ; )
+            {
+                if (input == password)
+                {
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Welcome {name}!");
+        }
+    }
+}
