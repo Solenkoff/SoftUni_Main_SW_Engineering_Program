c734a867d80a4807b25bc879d03fc0a75deeabbb
Solenkoff <solenkoff@yahoo.com>
2022-02-19 00:35:17 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L09_While_Loop_Lab/P03_Sum_Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03_Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (sum < n)
+            {
+                int inputNum = int.Parse(Console.ReadLine());
+                sum += inputNum;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
