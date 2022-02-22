e82b728b34787092e0b79b759ebbf5ad2952bd6a
Solenkoff <solenkoff@yahoo.com>
2022-02-23 00:45:52 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L09_While_Loop_Lab/P08_Graduation_pt2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _08_Graduation_pt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int grades = 1;
+            double sum = 0;
+            int excluded = 0;
+            while (grades <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                if (grade < 4)
+                {
+                    excluded++;
+                    if (excluded > 1)
+                    {
+                        break;
+                    }
+                }
+                if (grade >= 4)
+                {
+                    sum = sum + grade;
+                    grades++;
+                }
+            }
+            if (grades > 12)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {sum / 12:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"{name} has been excluded at {grades} grade");
+            }
+        }
+    }
+}
