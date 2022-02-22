45766fb9120340924bfa533bbb8de04b3da83173
Solenkoff <solenkoff@yahoo.com>
2022-02-22 18:01:26 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L09_While_Loop_Lab/P05_Account_Balance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05_Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double balance = 0.0;
+
+
+            while (command != "NoMoreMoney")
+            {
+                double deposit = double.Parse(command);
+                if (deposit < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                balance += deposit;
+                Console.WriteLine($"Increase: {deposit:f2}");
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {balance:f2}");
+        }
+    }
+}
