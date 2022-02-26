601d7561ddf960bdf0e9c0480b805e89e8e7cb07
Solenkoff <solenkoff@yahoo.com>
2022-02-26 16:19:37 +0100

    Added folder and solutions

---
+++ CSharp_Programming_Basics/L10_While_Loop_Exercise/P01_Old_Books/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01_Old_Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookToBeFound = Console.ReadLine();
+            string bookName = Console.ReadLine();
+            int bookCounter = 0;
+            while (bookName != "No More Books")
+            {
+                if (bookName == bookToBeFound)
+                {
+                    Console.WriteLine($"You checked {bookCounter} books and found it.");
+                    break;
+                }
+                bookCounter++;
+                bookName = Console.ReadLine();
+            }
+            if (bookToBeFound != bookName)
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {bookCounter} books.");
+            }
+        }
+    }
+}
