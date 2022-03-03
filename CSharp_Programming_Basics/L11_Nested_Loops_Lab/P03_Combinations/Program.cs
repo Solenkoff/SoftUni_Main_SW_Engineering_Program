65cf5b70c955133a5ff0c99e7de4cc74c426a72c
Solenkoff <solenkoff@yahoo.com>
2022-03-03 13:35:34 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L11_Nested_Loops_Lab/P03_Combinations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03_Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int validCombinationCount = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int k = 0; k <= n; k++)
+                    {
+                        if (i + j + k == n)
+                        {
+                            validCombinationCount++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(validCombinationCount);
+        }
+    }
+}
