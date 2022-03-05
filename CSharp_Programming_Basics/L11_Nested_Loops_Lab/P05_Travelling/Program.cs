7ec7da76c011a5e4d80ba0e9bcf56b391e41c9f8
Solenkoff <solenkoff@yahoo.com>
2022-03-05 14:21:13 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L11_Nested_Loops_Lab/P05_Travelling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05_Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string destination = Console.ReadLine();
+                if (destination == "End")
+                {
+                    break;
+                }
+                double budget = double.Parse(Console.ReadLine());
+                double savings = 0;
+                while (savings < budget)
+                {
+                    double income = double.Parse(Console.ReadLine());
+                    savings += income;
+                }
+                Console.WriteLine($"Going to {destination}!");
+            }
+        }
+    }
+}
