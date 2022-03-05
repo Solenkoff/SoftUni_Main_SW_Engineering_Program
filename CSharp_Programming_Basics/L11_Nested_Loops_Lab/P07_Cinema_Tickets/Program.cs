f7d28a5751c4b8be05f1e1bcd43f85a45563c3c4
Solenkoff <solenkoff@yahoo.com>
2022-03-05 14:22:25 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L11_Nested_Loops_Lab/P07_Cinema_Tickets/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _07_Cinema_Tickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentTicket = 0;
+            int standardTicket = 0;
+            int kidTicket = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Finish")
+                {
+                    break;
+                }
+                int freeSpots = int.Parse(Console.ReadLine());
+                int cuurentFreeSpot = freeSpots;
+                while (cuurentFreeSpot > 0)
+                {
+                    string ticketType = Console.ReadLine();
+
+                    if (ticketType == "End")
+                    {
+                        break;
+                    }
+                    cuurentFreeSpot--;
+
+                    if (ticketType == "standard")
+                    {
+                        standardTicket++;
+                    }
+                    else if (ticketType == "student")
+                    {
+                        studentTicket++;
+                    }
+                    else if (ticketType == "kid")
+                    {
+                        kidTicket++;
+                    }
+                }
+                double freeSpotInPercentages = 100 - (cuurentFreeSpot * 1.0 / freeSpots * 100);
+                Console.WriteLine($"{input} - {freeSpotInPercentages:f2}% full.");
+            }
+            int totalTickets = standardTicket + studentTicket + kidTicket;
+            double studentTicketsPer = studentTicket * 1.0 / totalTickets * 100;
+            double standardTicketPer = standardTicket * 1.0 / totalTickets * 100;
+            double kidsTicketPer = kidTicket * 1.0 / totalTickets * 100;
+
+            Console.WriteLine($"Total tickets: {totalTickets}");
+            Console.WriteLine($"{studentTicketsPer:f2}% student tickets.");
+            Console.WriteLine($"{standardTicketPer:f2}% standard tickets.");
+            Console.WriteLine($"{kidsTicketPer:f2}% kids tickets.");
+        }
+    }
+}
