efe9130ee142608a59fc5deddcff48bdfaa6939b
Solenkoff <solenkoff@yahoo.com>
2022-03-08 15:29:18 +0100

    Added folder and solutions

---
+++ CSharp_Programming_Basics/L11_Nested_Loops_Lab/L12_Nested_Loops_Exercise/P01_Number_Pyramid/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02_Equal_Sums_Even_Odd_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                string currNum = i.ToString();
+                int evenSum = 0;
+                int oddSum = 0;
+                for (int j = 0; j < currNum.Length; j++)
+                {
+                    int currentDigit = int.Parse(currNum[j].ToString());
+                    if (j % 2 == 0)
+                    {
+                        evenSum += currentDigit;
+                    }
+                    else
+                    {
+                        oddSum += currentDigit;
+                    }
+                }
+                if (oddSum == evenSum)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
