ec1353eb83198d911eb2fe1cae9c7764622fa98b
Solenkoff <solenkoff@yahoo.com>
2022-03-09 15:31:44 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L11_Nested_Loops_Lab/L12_Nested_Loops_Exercise/P03_Sum_Prime_Non_Prime/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _03_Sum_Prime_Non_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int sumPrime = 0;
+            int nonPrimeSum = 0;
+            while (command != "stop")
+            {
+                int number = int.Parse(command);
+                if (number < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                }
+                else
+                {
+                    int count = 0;
+                    for (int i = 1; i <= number; i++)
+                    {
+                        if (number % i == 0)
+                        {
+                            count++;
+                        }
+                    }
+                    if (count == 2)
+                    {
+                        sumPrime += number;
+                    }
+                    else
+                    {
+                        nonPrimeSum += number;
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {sumPrime}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
+        }
+    }
+}
