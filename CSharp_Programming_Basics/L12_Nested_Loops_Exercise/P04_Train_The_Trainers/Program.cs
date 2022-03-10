5ab5ec885f91cfa48d75f926e24d01af35f52ba8
Solenkoff <solenkoff@yahoo.com>
2022-03-10 14:03:27 +0100

    Added homework solution

---
+++ CSharp_Programming_Basics/L12_Nested_Loops_Exercise/P04_Train_The_Trainers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04_Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jouryCount = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int gradeCount = 0;
+            double sumAllGrades = 0;
+            while (input != "Finish")
+            {
+                double sumOfGrades = 0;
+                for (int i = 1; i <= jouryCount; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    sumOfGrades += grade;
+                    gradeCount++;
+                    sumAllGrades += grade;
+                }
+                double average = sumOfGrades / jouryCount;
+                Console.WriteLine($"{input} - {average:f2}.");
+                input = Console.ReadLine();
+            }
+            double finalAvarage = sumAllGrades / gradeCount;
+            Console.WriteLine($"Student's final assessment is {finalAvarage:f2}.");
+        }
+    }
+}
