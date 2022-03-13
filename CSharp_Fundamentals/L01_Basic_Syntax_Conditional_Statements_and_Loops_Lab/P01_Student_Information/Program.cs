6c8292f1e9fb27fe0218020a8cdab7825d4f554d
Solenkoff <solenkoff@yahoo.com>
2022-03-14 00:26:14 +0100

    Added folder and solutions

---
+++ CSharp_Fundamentals/L01_Basic_Syntax_Conditional_Statements_and_Loops_Lab/P01_Student_Information/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P01_Student_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double avarageGrade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {avarageGrade:f2}");
+        }
+    }
+}
