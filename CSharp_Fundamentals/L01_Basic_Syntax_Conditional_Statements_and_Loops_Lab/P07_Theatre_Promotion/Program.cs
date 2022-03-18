b7d4a5730eba6175d2fd1d4cd9155fcf8b2fe071
Solenkoff <solenkoff@yahoo.com>
2022-03-18 21:53:29 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L01_Basic_Syntax_Conditional_Statements_and_Loops_Lab/P07_Theatre_Promotion/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace P07_Theatre_Promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfDay = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            int ticketPrice = 0;
+
+            if (age >= 0 && age <= 18)
+            {
+                switch (typeOfDay)
+                {
+                    case "Weekday":
+                        ticketPrice = 12;
+                        break;
+                    case "Weekend":
+                        ticketPrice = 15;
+                        break;
+                    case "Holiday":
+                        ticketPrice = 5;
+                        break;
+                }
+            }
+            else if (age > 18 && age <= 64)
+            {
+                switch (typeOfDay)
+                {
+                    case "Weekday":
+                        ticketPrice = 18;
+                        break;
+                    case "Weekend":
+                        ticketPrice = 20;
+                        break;
+                    case "Holiday":
+                        ticketPrice = 12;
+                        break;
+                }
+            }
+            else if (age > 64 && age <= 122)
+            {
+                switch (typeOfDay)
+                {
+                    case "Weekday":
+                        ticketPrice = 12;
+                        break;
+                    case "Weekend":
+                        ticketPrice = 15;
+                        break;
+                    case "Holiday":
+                        ticketPrice = 10;
+                        break;
+                }
+            }
+            if (ticketPrice == 0)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                Console.WriteLine($"{ticketPrice}$");
+            }
+        }
+    }
+}
