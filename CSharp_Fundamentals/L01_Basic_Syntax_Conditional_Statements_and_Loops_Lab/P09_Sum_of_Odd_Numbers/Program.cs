6ff008e5a9a1d4f4ad807c0d6e132647f31bc4d7
Solenkoff <solenkoff@yahoo.com>
2022-03-19 21:59:55 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L01_Basic_Syntax_Conditional_Statements_and_Loops_Lab/P09_Sum_of_Odd_Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P09_Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nNumbers = int.Parse(Console.ReadLine());
+
+            int sumOfNumbers = 0;
+            for (int i = 1; i <= nNumbers; i++)
+            {
+                Console.WriteLine("{0}", 2 * i - 1);
+                sumOfNumbers += 2 * i - 1;
+            }
+            Console.WriteLine($"Sum: {sumOfNumbers}");
+        }
+    }
+}
