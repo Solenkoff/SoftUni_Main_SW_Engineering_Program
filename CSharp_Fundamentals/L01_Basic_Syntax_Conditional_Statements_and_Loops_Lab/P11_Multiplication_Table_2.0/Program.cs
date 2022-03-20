4b8088a4fa6d8b57ca50def6dc3794ed1565e6ec
Solenkoff <solenkoff@yahoo.com>
2022-03-20 01:38:41 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L01_Basic_Syntax_Conditional_Statements_and_Loops_Lab/P11_Multiplication_Table_2.0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P11_Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+
+            if (multiplier <= 10)
+            {
+                for (int i = multiplier; i <= 10; i++)
+                {
+                    int multiplication = inputNumber * i;
+                    Console.WriteLine($"{inputNumber} X {i} = {multiplication}");
+                }
+            }
+            else if (multiplier > 10)
+            {
+                int multiplication = inputNumber * multiplier;
+                Console.WriteLine($"{inputNumber} X {multiplier} = {multiplication}");
+            }
+        }
+    }
+}
