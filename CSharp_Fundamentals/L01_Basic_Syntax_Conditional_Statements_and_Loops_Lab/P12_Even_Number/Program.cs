c3fe46c22905c03423bb93268a82c7e0b4bb8f31
Solenkoff <solenkoff@yahoo.com>
2022-03-21 18:53:55 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L01_Basic_Syntax_Conditional_Statements_and_Loops_Lab/P12_Even_Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P12_Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            while (inputNumber % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+                inputNumber = int.Parse(Console.ReadLine());
+            }
+            if (inputNumber % 2 == 0)
+            {
+                Console.WriteLine($"The number is: {Math.Abs(inputNumber)}");
+            }
+        }
+    }
+}
