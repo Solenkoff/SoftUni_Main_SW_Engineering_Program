b2c1409d252f19c454d96001f0cf4ee0c96c79ef
Solenkoff <solenkoff@yahoo.com>
2022-03-26 01:21:21 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L02_Basic_Syntax_Conditional_Statements_and_Loops_Exercise/P05_Login/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P05_Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Empty;
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            int counter = 0;
+            string input = Console.ReadLine();
+
+            while (input != password)
+            {
+                counter++;
+                if (counter == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+
+                input = Console.ReadLine();
+            }
+            if (input == password)
+            {
+                Console.WriteLine($"User {username} logged in.");
+            }
+        }
+    }
+}
