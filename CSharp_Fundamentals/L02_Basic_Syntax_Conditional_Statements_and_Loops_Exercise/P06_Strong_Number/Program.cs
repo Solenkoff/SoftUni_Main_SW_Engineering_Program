c1cdbe4cf7be47af0a1eea42fcda64b29c272eb6
Solenkoff <solenkoff@yahoo.com>
2022-03-27 12:30:12 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L02_Basic_Syntax_Conditional_Statements_and_Loops_Exercise/P06_Strong_Number/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace P06_Strong_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int number = input;
+
+            int currantDigit = 0;
+            int factorialSum = 0;
+
+            while (number != 0)
+            {
+                currantDigit = number % 10;
+                number /= 10;
+
+                int factorial = 1;
+
+                for (int i = 1; i <= currantDigit; i++)
+                {
+                    factorial *= i;
+                }
+
+                factorialSum += factorial;
+            }
+            if (factorialSum == input)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
