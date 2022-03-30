9bc35c7fc083318768eeba3e4671ea56a4bb34c1
Solenkoff <solenkoff@yahoo.com>
2022-03-30 10:10:22 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L02_Basic_Syntax_Conditional_Statements_and_Loops_Exercise/P10_Rage_Expenses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P10_Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            double rageExpences = (nLostGames / 2 * headsetPrice) + (nLostGames / 3 * mousePrice)
+                + (nLostGames / 6 * keyboardPrice) + (nLostGames / 12 * displayPrice);
+
+            Console.WriteLine($"Rage expenses: {rageExpences:f2} lv.");
+        }
+    }
+}
