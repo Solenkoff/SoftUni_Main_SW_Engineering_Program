6502c952f53d4a5d193aae115ef8d0d59a23941a
Solenkoff <solenkoff@yahoo.com>
2022-04-15 10:21:30 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L03_Data_Types_And_Variables_Lab/P02_Pounds_To_Dollars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02_Pounds_To_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal britPounds = decimal.Parse(Console.ReadLine());
+            decimal usd = britPounds * 1.31M;
+            Console.WriteLine($"{usd:f3}");
+
+        }
+    }
+}
