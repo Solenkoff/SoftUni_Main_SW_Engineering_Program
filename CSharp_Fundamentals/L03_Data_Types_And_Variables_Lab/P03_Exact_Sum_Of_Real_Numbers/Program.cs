2ec15cff9c3a9c81f9a7c19c476787a6d45fa03a
Solenkoff <solenkoff@yahoo.com>
2022-04-16 11:30:16 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L03_Data_Types_And_Variables_Lab/P03_Exact_Sum_Of_Real_Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03_Exact_Sum_Of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nNumbers = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 0; i < nNumbers; i++)
+            {
+                sum += decimal.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
