d2595b007b41d96c5a89590b078ed6d1508e1e9a
Solenkoff <solenkoff@yahoo.com>
2022-04-17 17:22:13 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L03_Data_Types_And_Variables_Lab/P04_Centuries_To_Minutes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04_Centuries_To_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nCenturies = int.Parse(Console.ReadLine());
+
+            int years = nCenturies * 100;
+            int days = (int)(years * 365.2422);
+            int hours = days * 24;
+            int min = hours * 60;
+
+            Console.WriteLine($"{nCenturies} centuries = {years} " +
+                $"years = {days} days = {hours} hours = {min} minutes");
+
+        }
+    }
+}
