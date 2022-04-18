0e869482f126cf51938fa65df6c5016e654d4cd5
Solenkoff <solenkoff@yahoo.com>
2022-04-18 10:40:53 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L03_Data_Types_And_Variables_Lab/P05_Special_Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nNumbers = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= nNumbers; i++)
+            {
+                int number = i;
+                int iDigitsSum = 0;
+                while (number > 0)
+                {
+                    iDigitsSum += number % 10;
+                    number /= 10;
+                }
+                if (iDigitsSum == 5 || iDigitsSum == 7 || iDigitsSum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+
+        }
+    }
+}
