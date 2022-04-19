067eff907f163678228e6a825924d03af4fdf9a5
Solenkoff <solenkoff@yahoo.com>
2022-04-19 13:38:34 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L03_Data_Types_And_Variables_Lab/P06_Reversed_Chars/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06_Reversed_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+            char char3 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{char3} {char2} {char1}");
+
+        }
+    }
+}
