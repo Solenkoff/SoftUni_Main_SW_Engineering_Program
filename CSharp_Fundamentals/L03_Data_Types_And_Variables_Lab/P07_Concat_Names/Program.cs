7e53eb52358e7be3b40e507528fdd45641e4c2ec
Solenkoff <solenkoff@yahoo.com>
2022-04-20 11:32:21 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L03_Data_Types_And_Variables_Lab/P07_Concat_Names/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _07_Concat_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name1 = Console.ReadLine();
+            string name2 = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            string result = name1 + delimiter + name2;
+            Console.WriteLine(result);
+
+        }
+    }
+}
