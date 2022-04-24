6cce65a814ea77f85c1e069007b51378e7b361b3
Solenkoff <solenkoff@yahoo.com>
2022-04-24 10:31:11 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L03_Data_Types_And_Variables_Lab/P09_Chars_To_String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _09_Chars_To_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+            char char3 = char.Parse(Console.ReadLine());
+
+            string oneString = char1.ToString() + char2.ToString() + char3.ToString();
+            Console.WriteLine(oneString);
+
+        }
+    }
+}
