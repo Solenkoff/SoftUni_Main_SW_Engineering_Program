eae96e0180c0c8b2de12011e6dd1b5e7bbee42e5
Solenkoff <solenkoff@yahoo.com>
2022-04-26 00:48:42 +0200

    Added folder and solutions

---
+++ CSharp_Fundamentals/L02_Basic_Syntax_Conditional_Statements_and_Loops_Exercise/P01_Ages/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P01_Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 0 && age <= 2)
+            {
+                Console.WriteLine("baby");
+            }
+            if (age > 2 && age <= 13)
+            {
+                Console.WriteLine("child");
+            }
+            if (age > 13 && age <= 19)
+            {
+                Console.WriteLine("teenager");
+            }
+            if (age > 19 && age <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            if (age >= 66)
+            {
+                Console.WriteLine("elder");
+            }
+
+        }
+    }
+}
