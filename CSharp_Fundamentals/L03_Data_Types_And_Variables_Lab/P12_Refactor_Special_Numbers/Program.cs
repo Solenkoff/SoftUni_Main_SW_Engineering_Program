fc055370f4e6e02e309684c3b93a5ed3429abbaf
Solenkoff <solenkoff@yahoo.com>
2022-04-29 10:39:43 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L03_Data_Types_And_Variables_Lab/P12_Refactor_Special_Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _12_Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nNumbers = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= nNumbers; i++)
+            {
+                int iDigitsSum = 0;
+                int number = i;
+                while (number > 0)
+                {
+                    iDigitsSum += number % 10;
+                    number /= 10;
+                }
+                bool isSpecialNum = (iDigitsSum == 5) || (iDigitsSum == 7) || (iDigitsSum == 11);
+                Console.WriteLine($"{i} -> {isSpecialNum}");
+            }
+
+        }
+    }
+}
