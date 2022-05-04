b6ccb35db5c2d3794eb540797e258142e1b1bf11
Solenkoff <solenkoff@yahoo.com>
2022-05-04 10:33:59 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L02_Basic_Syntax_Conditional_Statements_and_Loops_Exercise/L02_Basic_Syntax_Conditional_Statements_and_Loops_More_Exercise/P04_Reverse_String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04_Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            string reversInput = string.Empty;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversInput += input[i];
+            }
+            Console.WriteLine(reversInput);
+
+        }
+    }
+}
