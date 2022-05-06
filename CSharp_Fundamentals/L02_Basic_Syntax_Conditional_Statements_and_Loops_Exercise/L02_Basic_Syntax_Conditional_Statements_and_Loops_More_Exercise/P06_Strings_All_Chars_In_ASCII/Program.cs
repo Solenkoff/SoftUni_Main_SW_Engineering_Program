cc81dffabd36aff247dee99d40169ec431f0607f
Solenkoff <solenkoff@yahoo.com>
2022-05-06 11:23:52 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L02_Basic_Syntax_Conditional_Statements_and_Loops_Exercise/L02_Basic_Syntax_Conditional_Statements_and_Loops_More_Exercise/P06_Strings_All_Chars_In_ASCII/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06_Strings_All_Chars_In_ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string helloWorld = "Hello, world!";
+            foreach (char c in helloWorld)
+            {
+                Console.WriteLine(c + ": " + (int)c);
+            }
+
+        }
+    }
+}
