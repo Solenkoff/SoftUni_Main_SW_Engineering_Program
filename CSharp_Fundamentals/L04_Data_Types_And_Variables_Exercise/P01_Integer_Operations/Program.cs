f476524cde696f0c3e6e857de5a716ac949dccd8
Solenkoff <solenkoff@yahoo.com>
2022-05-08 13:51:32 +0200

    Added folder and solutions

---
+++ CSharp_Fundamentals/L04_Data_Types_And_Variables_Exercise/P01_Integer_Operations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01_Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int integer1 = int.Parse(Console.ReadLine());
+            int integer2 = int.Parse(Console.ReadLine());
+            int integer3 = int.Parse(Console.ReadLine());
+            int integer4 = int.Parse(Console.ReadLine());
+
+            int result = (integer1 + integer2) / integer3 * integer4;
+            Console.WriteLine(result);
+
+        }
+    }
+}
