a0170f565cd741ddfc518b59c4f2761dad7a9ca2
Solenkoff <solenkoff@yahoo.com>
2022-05-09 11:15:09 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L04_Data_Types_And_Variables_Exercise/P02_Sum_Digits/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02_Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            int sumOfDigits = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                //int currentNumber = (int)Char.GetNumericValue(input[i]);
+                int currentNumber = int.Parse(input[i].ToString());
+                sumOfDigits += currentNumber;
+            }
+            Console.WriteLine(sumOfDigits);
+
+        }
+    }
+}
