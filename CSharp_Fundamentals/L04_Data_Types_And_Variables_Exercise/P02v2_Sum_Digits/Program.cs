d12e835decb1f376c6db2d8ead25c02b75028be8
Solenkoff <solenkoff@yahoo.com>
2022-05-10 13:41:54 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L04_Data_Types_And_Variables_Exercise/P02v2_Sum_Digits/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02v2_Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            int sumOfDigits = 0;
+            int number = inputNumber;
+            while (number > 0)
+            {
+                sumOfDigits += number % 10;
+                number /= 10;
+            }
+            Console.WriteLine(sumOfDigits);
+
+        }
+    }
+}
