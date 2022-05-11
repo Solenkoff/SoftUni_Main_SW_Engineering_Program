981c5037efdf29f48f937b009cfdf2f9cef05c4f
Solenkoff <solenkoff@yahoo.com>
2022-05-11 10:59:21 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L04_Data_Types_And_Variables_Exercise/P03_Elevator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03_Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nPersons = int.Parse(Console.ReadLine());
+            int pCapacity = int.Parse(Console.ReadLine());
+
+            int nRuns = 0;
+            if (nPersons % pCapacity == 0)
+            {
+                nRuns = nPersons / pCapacity;         //  while (nPersons > 0)
+            }                                         //  {
+            else                                      //      nPersons -= pCapacity;
+            {                                         //      nRuns++;
+                nRuns = nPersons / pCapacity + 1;     //  }
+            }
+            Console.WriteLine(nRuns);
+
+        }
+    }
+}
