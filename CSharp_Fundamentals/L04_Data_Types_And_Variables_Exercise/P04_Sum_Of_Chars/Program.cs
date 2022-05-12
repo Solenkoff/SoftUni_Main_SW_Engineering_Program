8ab460fff624f507dd13a892e389b55c8be85318
Solenkoff <solenkoff@yahoo.com>
2022-05-12 09:53:10 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L04_Data_Types_And_Variables_Exercise/P04_Sum_Of_Chars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04_Sum_Of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nCharacters = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < nCharacters; i++)
+            {
+                char input = char.Parse(Console.ReadLine());
+                sum += input;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+
+        }
+    }
+}
