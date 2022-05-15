b85c1d6fa214dcb6bae9cb47c541e65bd5cf680e
Solenkoff <solenkoff@yahoo.com>
2022-05-15 10:52:31 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L04_Data_Types_And_Variables_Exercise/P06_Triples_Of_Latin_Letters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06_Triples_Of_Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int firstSimbol = 0; firstSimbol < n; firstSimbol++)
+            {
+                for (int secondSimbol = 0; secondSimbol < n; secondSimbol++)
+                {
+                    for (int thirdSimbol = 0; thirdSimbol < n; thirdSimbol++)
+                    {
+                        char firstChar = (char)(97 + firstSimbol);
+                        char secondChar = (char)(97 + secondSimbol);
+                        char thirdChar = (char)(97 + thirdSimbol);
+
+                        Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+                    }
+                }
+            }
+
+        }
+    }
+}
