65697a973c3d6394a2fc9fd7febd4871e3e0e74e
Solenkoff <solenkoff@yahoo.com>
2022-05-16 11:40:06 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L04_Data_Types_And_Variables_Exercise/P07_Water_Overflow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07_Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nLines = int.Parse(Console.ReadLine());
+
+            int tankVolume = 255;
+            for (int i = 0; i < nLines; i++)
+            {
+                int litersIn = int.Parse(Console.ReadLine());
+                if (litersIn <= tankVolume)
+                {
+                    tankVolume -= litersIn;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+            Console.WriteLine($"{255 - tankVolume}");
+
+        }
+    }
+}
