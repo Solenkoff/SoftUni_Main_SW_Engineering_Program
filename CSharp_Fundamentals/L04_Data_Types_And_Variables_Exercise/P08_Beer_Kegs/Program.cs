cb2697bd33c60a51d8b887a2712f9c674379da7d
Solenkoff <solenkoff@yahoo.com>
2022-05-17 13:33:39 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L04_Data_Types_And_Variables_Exercise/P08_Beer_Kegs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _08_Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            double highestVolume = 0.0;
+            string biggest = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int hight = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * Math.Pow(radius, 2) * hight;
+                if (volume > highestVolume)
+                {
+                    highestVolume = volume;
+                    biggest = model;
+                }
+            }
+            Console.WriteLine(biggest);
+
+        }
+    }
+}
