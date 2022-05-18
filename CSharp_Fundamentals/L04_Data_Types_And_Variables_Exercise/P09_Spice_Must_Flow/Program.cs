61b7c04df209959c3c5b9b37ff8d73d15aa86afd
Solenkoff <solenkoff@yahoo.com>
2022-05-18 10:40:32 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L04_Data_Types_And_Variables_Exercise/P09_Spice_Must_Flow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _09_Spice_Must_Flow_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int startYield = int.Parse(Console.ReadLine());
+
+            int totalYield = 0;
+            int expectedYield = startYield;
+
+            int days = 0;
+
+            while (expectedYield >= 100)
+            {
+                totalYield += (expectedYield - 26);
+                expectedYield -= 10;
+                days++;
+            }
+            if (days != 0)
+            {
+                totalYield -= 26;
+            }
+
+            Console.WriteLine(days);
+            Console.WriteLine(totalYield);
+
+        }
+    }
+}
