cd544bbaef69ba52e957133615dee767530257b8
Solenkoff <solenkoff@yahoo.com>
2022-05-20 11:27:11 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L04_Data_Types_And_Variables_Exercise/P10_Poke_Mon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _10_Poke_Mon_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int powerNStartValue = int.Parse(Console.ReadLine());
+            int distanceM = int.Parse(Console.ReadLine());
+            int exhaustionY = int.Parse(Console.ReadLine());
+
+            int powerN = powerNStartValue;
+            int targets = 0;
+
+            while (powerN >= distanceM)
+            {
+                powerN -= distanceM;
+                targets++;
+                if ((powerN == powerNStartValue / 2.0) && (exhaustionY != 0))
+                {
+                    powerN /= exhaustionY;
+                }
+            }
+            Console.WriteLine(powerN);
+            Console.WriteLine(targets);
+
+        }
+    }
+}
