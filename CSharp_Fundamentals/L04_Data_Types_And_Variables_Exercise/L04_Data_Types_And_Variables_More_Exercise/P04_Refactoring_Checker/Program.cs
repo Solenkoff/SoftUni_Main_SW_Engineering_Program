ee0be90488868ad11e3f42e16c4f870ef2721e2a
Solenkoff <solenkoff@yahoo.com>
2022-05-26 13:24:40 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L04_Data_Types_And_Variables_Exercise/L04_Data_Types_And_Variables_More_Exercise/P04_Refactoring_Checker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P04_Refactoring_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 2; i <= n; i++)
+            {
+                bool isTrue = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isTrue = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{i} -> {isTrue.ToString().ToLower()}");
+            }
+        }
+    }
+}
