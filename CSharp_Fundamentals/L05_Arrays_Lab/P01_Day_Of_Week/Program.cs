0c2ba4b54023131376ed36c1a3282f617a655e56
Solenkoff <solenkoff@yahoo.com>
2022-06-01 14:30:21 +0200

    Added folder and solutions

---
+++ CSharp_Fundamentals/L05_Arrays_Lab/P01_Day_Of_Week/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01_Day_Of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] days = new[]
+           {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+            int dayNum = int.Parse(Console.ReadLine());
+            if (dayNum < 1 || dayNum > days.Length)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(days[dayNum - 1]);
+            }
+
+        }
+    }
+}
