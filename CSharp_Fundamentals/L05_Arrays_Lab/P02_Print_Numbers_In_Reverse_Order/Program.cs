eb6e771e6ed53095b7c32d00d375830609051636
Solenkoff <solenkoff@yahoo.com>
2022-06-11 11:20:41 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L05_Arrays_Lab/P02_Print_Numbers_In_Reverse_Order/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02_Print_Numbers_In_Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write("{0} ", numbers[i]);
+            }
+
+        }
+    }
+}
