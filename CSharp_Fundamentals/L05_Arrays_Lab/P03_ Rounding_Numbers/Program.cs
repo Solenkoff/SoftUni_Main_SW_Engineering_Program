addc029be952783f7fa02a4ff8e4b98d718abd29
Solenkoff <solenkoff@yahoo.com>
2022-06-12 10:27:14 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L05_Arrays_Lab/P03_ Rounding_Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03__Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] arr = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"{arr[i]} => {Math.Round(arr[i], MidpointRounding.AwayFromZero)}");
+            }
+
+        }
+    }
+}
