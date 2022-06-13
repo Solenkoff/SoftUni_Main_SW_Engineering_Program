6aaea0f87600238539b40a185fd7d5c9352c14df
Solenkoff <solenkoff@yahoo.com>
2022-06-13 15:32:12 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L05_Arrays_Lab/P03v2_Rounding_Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _03v2_Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] nums = Console.ReadLine()
+                                   .Split()
+                                   .Select(double.Parse)
+                                   .ToArray();
+            int[] integers = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                integers[i] = (int)Math.Round(nums[i], MidpointRounding.AwayFromZero);
+                if (integers[i] == -0)
+                {
+                    integers[i] = 0;
+                }
+
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine($"{nums[i]} => {integers[i]}");
+            }
+
+        }
+    }
+}
