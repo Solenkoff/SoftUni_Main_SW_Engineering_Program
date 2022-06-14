c3015c26863e0aa359f5c701de0683bd80c4a94d
Solenkoff <solenkoff@yahoo.com>
2022-06-14 11:19:42 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L05_Arrays_Lab/P04_Reverse Array_Of_Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _04_Reverse_Array_Of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] words = Console.ReadLine().Split().ToArray();
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{words[i]} ");
+            }
+
+        }
+    }
+}
