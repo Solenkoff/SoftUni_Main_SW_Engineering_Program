3db2a457e7cb43045e0c6fda5f90449bd917c2a2
Solenkoff <solenkoff@yahoo.com>
2022-06-15 10:20:25 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L05_Arrays_Lab/P06_Even_And_Odd_Subtraction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_Even_And_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumOdds = 0;
+            int sumEvens = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    sumEvens += nums[i];
+                }
+                else
+                {
+                    sumOdds += nums[i];
+                }
+            }
+            Console.WriteLine($"{sumEvens - sumOdds}");
+
+        }
+    }
+}
