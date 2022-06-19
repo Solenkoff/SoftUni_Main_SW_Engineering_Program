10326bb0f96284d4c280251ca2de5db0c6cf36bf
Solenkoff <solenkoff@yahoo.com>
2022-06-19 11:35:01 +0200

    Added folder and solutions

---
+++ CSharp_Fundamentals/L06_Arrays_Exercise/P01_Train/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01_Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nVagons = int.Parse(Console.ReadLine());
+            int[] personsPerVagon = new int[nVagons];
+            int personsTotal = 0;
+            for (int i = 0; i < nVagons; i++)
+            {
+                personsPerVagon[i] = int.Parse(Console.ReadLine());
+                personsTotal += personsPerVagon[i];
+            }
+            Console.WriteLine(string.Join(" ", personsPerVagon));
+            Console.WriteLine(personsTotal);
+        }
+    }
+}
