e33a883f3e58652aaad207deacc79c93f9ee1a43
Solenkoff <solenkoff@yahoo.com>
2022-06-21 10:38:24 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L06_Arrays_Exercise/P03_Zig_Zag_Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int[] setOfNums = Console.ReadLine()
+                                         .Split()
+                                         .Select(int.Parse)
+                                         .ToArray();
+                if (i % 2 == 0)
+                {
+                    arr1[i] = setOfNums[0];
+                    arr2[i] = setOfNums[1];
+
+                }
+                else
+                {
+                    arr2[i] = setOfNums[0];
+                    arr1[i] = setOfNums[1];
+                }
+            }
+            Console.WriteLine(string.Join(" ", arr1));
+            Console.WriteLine(string.Join(" ", arr2));
+
+        }
+    }
+}
