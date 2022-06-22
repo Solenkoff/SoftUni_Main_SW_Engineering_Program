0cc5753b6c84a206a55777f5a761bf9c4093bdc4
Solenkoff <solenkoff@yahoo.com>
2022-06-22 11:48:54 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L06_Arrays_Exercise/P04_Array_Rotation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _04_Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] nums = Console.ReadLine().Split();
+            int rotationsN = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rotationsN; i++)
+            {
+                string[] currentArr = new string[nums.Length];
+
+                for (int j = 0; j < nums.Length - 1; j++)
+                {
+                    currentArr[j] = nums[j + 1];
+                }
+                currentArr[nums.Length - 1] = nums[0];
+                nums = currentArr;
+            }
+            Console.WriteLine(string.Join(" ", nums));
+
+        }
+    }
+}
