17ff32ad8dcd49849a7957a4423225795c570ead
Solenkoff <solenkoff@yahoo.com>
2022-06-23 14:39:05 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L06_Arrays_Exercise/P06_Equal_Sum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_Equal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = Console.ReadLine()
+                               .Split()
+                               .Select(int.Parse)
+                               .ToArray();
+
+            bool isFound = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int rightSum = 0;
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+                    rightSum += arr[k];
+                }
+
+                int leftSum = 0;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    leftSum += arr[j];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    isFound = true;
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
