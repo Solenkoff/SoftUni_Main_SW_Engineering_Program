bd586ae0c779dad7ab6d7a90520ba08a64ddc26e
Solenkoff <solenkoff@yahoo.com>
2022-06-24 10:18:24 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L06_Arrays_Exercise/P07_Max_Sequence_of_Equal_Elements/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _07_Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = Console.ReadLine()
+                              .Split()
+                              .Select(int.Parse)
+                              .ToArray();
+
+            int maxCounter = 0;
+            int maxSequenceNum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int counter = 1;
+                int currentNum = 0;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+
+                    if (arr[i] != arr[j])
+                    {
+                        break;
+                    }
+                    else if (arr[i] == arr[j])
+                    {
+                        currentNum = arr[i];
+                        counter++;
+                    }
+
+                }
+                if (counter > maxCounter)
+                {
+                    maxCounter = counter;
+                    maxSequenceNum = currentNum;
+                }
+            }
+            int[] maxSequence = new int[maxCounter];
+            for (int i = 0; i < maxCounter; i++)
+            {
+                maxSequence[i] = maxSequenceNum;
+            }
+            Console.WriteLine(string.Join(" ", maxSequence));
+
+        }
+    }
+}
