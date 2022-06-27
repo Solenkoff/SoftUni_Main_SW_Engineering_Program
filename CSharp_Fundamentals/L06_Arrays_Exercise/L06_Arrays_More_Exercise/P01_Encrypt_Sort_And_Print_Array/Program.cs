7b0f0826edc875cd63f12276a2f24c15cb17a799
Solenkoff <solenkoff@yahoo.com>
2022-06-27 12:28:07 +0200

    Added folder and solutions

---
+++ CSharp_Fundamentals/L06_Arrays_Exercise/L06_Arrays_More_Exercise/P01_Encrypt_Sort_And_Print_Array/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace P01_Encrypt_Sort_And_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> vowels = new List<char>
+            {
+                'a','A',
+                'e','E',
+                'i','I',
+                'o','O',
+                'u','U'
+            };
+
+
+            int linesCount = int.Parse(Console.ReadLine());
+            int[] sumOfAll = new int[linesCount];
+            for (int i = 0; i < linesCount; i++)
+            {
+                string word = Console.ReadLine();
+                int sum = 0;
+
+                for (int k = 0; k < word.Length; k++)
+                {
+                    if (vowels.Contains(word[k]))
+                    {
+                        sum += (int)word[k] * word.Length;
+
+                    }
+                    else
+                    {
+                        sum += (int)(word[k] / word.Length);
+                    }
+
+                }
+
+                sumOfAll[i] = sum;
+            }
+
+
+            var listOfSum = sumOfAll.ToList();
+            var listWithoutZero = listOfSum.Where(x => x != 0);
+            var orderedList = listWithoutZero.OrderBy(x => x);
+            foreach (var item in orderedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
