f3e36598206c35c20b71e386ef05c8c2232cd5b1
Solenkoff <solenkoff@yahoo.com>
2022-06-30 13:37:16 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L06_Arrays_Exercise/L06_Arrays_More_Exercise/P03_Recursive_Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P03_Recursive_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nInput = int.Parse(Console.ReadLine());
+
+            if (nInput <= 1)
+            {
+                Console.WriteLine(1);
+            }
+
+            else
+            {
+                double addPhi = (1 + Math.Sqrt(5)) / 2;
+                double subtractPhi = (1 - Math.Sqrt(5)) / 2;
+
+                double fibonacci = (Math.Pow(addPhi, nInput) - Math.Pow((-subtractPhi), nInput)) / Math.Sqrt(5);
+
+                long roundedFib = (long)Math.Round(fibonacci);
+
+                Console.WriteLine(roundedFib);
+            }
+        }
+    }
+}
