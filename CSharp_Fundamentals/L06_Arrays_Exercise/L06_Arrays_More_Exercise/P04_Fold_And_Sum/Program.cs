8ae8f85f32c12bf7d80e546b4fbcbb25425bd4ce
Solenkoff <solenkoff@yahoo.com>
2022-07-01 11:08:23 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L06_Arrays_Exercise/L06_Arrays_More_Exercise/P04_Fold_And_Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace P04_Fold_And_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArr = Console.ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+            int k = inputArr.Length / 4;
+            int[] arr = new int[2 * k];
+
+            for (int i = 0; i < k; i++)
+            {
+                arr[i] = inputArr[k - (i + 1)] + inputArr[k + i];
+                arr[arr.Length - 1 - i] = inputArr[arr.Length - 1 - i + k] + inputArr[(arr.Length - 1 - i) + (k + 2 * i + 1)];
+            }
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
