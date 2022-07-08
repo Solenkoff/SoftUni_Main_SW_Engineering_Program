68a2f05ecb872260d491de50f12a738a8bd1a154
Solenkoff <solenkoff@yahoo.com>
2022-07-08 18:27:47 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L07_Methods_Lab/P05_Orders/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05_Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string productOrdered = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            double price = 0.0;
+
+            switch (productOrdered)
+            {
+                case "coffee":
+                    price = 1.50;
+                    PrintTotal(price, quantity);
+                    break;
+                case "water":
+                    price = 1.00;
+                    PrintTotal(price, quantity);
+                    break;
+                case "coke":
+                    price = 1.40;
+                    PrintTotal(price, quantity);
+                    break;
+                case "snacks":
+                    price = 2.00;
+                    PrintTotal(price, quantity);
+                    break;
+
+            }
+
+        }
+
+        static void PrintTotal(double price, int quantity)
+        {
+            double total = price * quantity;
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
