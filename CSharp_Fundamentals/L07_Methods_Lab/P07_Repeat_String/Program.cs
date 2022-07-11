132b522d9ba3fe80918df280a255ed371f24fe22
Solenkoff <solenkoff@yahoo.com>
2022-07-11 11:12:16 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L07_Methods_Lab/P07_Repeat_String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07_Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            RepeatString(text, n);
+
+        }
+
+
+        private static void RepeatString(string str, int count)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < count; i++)
+            {
+                result.Append(str);
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+
+    }
+}
