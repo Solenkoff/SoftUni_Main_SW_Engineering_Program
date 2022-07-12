a1494a2c1930c953e28051a92b44ae59a7ecb2ec
Solenkoff <solenkoff@yahoo.com>
2022-07-12 14:09:21 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L07_Methods_Lab/P08_Math_Power/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08_Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            double result = RaiseToPower(num, power);
+            Console.WriteLine(result);
+
+        }
+
+        private static double RaiseToPower(double num, int power)
+        {
+            double result = 0d;
+            result = Math.Pow(num, power);
+
+            return result;
+        }
+
+    }
+}
