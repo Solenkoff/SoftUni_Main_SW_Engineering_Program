8408a972cfd6dc325b2827a86236f2def9db21f7
Solenkoff <solenkoff@yahoo.com>
2022-07-15 11:21:04 +0200

    Added folder and solutions

---
+++ CSharp_Fundamentals/L08_Methods_Exercise/P01_Smallest_Of_Three_Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01_Smallest_Of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            PrintSmallestNum(firstNum, secondNum, thirdNum);
+
+        }
+
+        private static void PrintSmallestNum(int firstNum, int secondNum, int thirdNum)
+        {
+            int smallestNum = firstNum;
+
+            if (secondNum < firstNum)
+            {
+                smallestNum = secondNum;
+            }
+            if (thirdNum < firstNum && thirdNum < secondNum)
+            {
+                smallestNum = thirdNum;
+            }
+
+            Console.WriteLine(smallestNum);
+        }
+
+    }
+}
