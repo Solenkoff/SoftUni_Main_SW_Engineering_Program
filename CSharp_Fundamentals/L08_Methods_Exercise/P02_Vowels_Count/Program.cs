312c46307decc870f3b1bb908749faf6076b600c
Solenkoff <solenkoff@yahoo.com>
2022-07-19 12:39:33 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L08_Methods_Exercise/P02_Vowels_Count/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02_Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine().ToLower();
+
+            VowelsCount(text);
+
+        }
+
+
+        private static void VowelsCount(string text)
+        {
+            int vowelsCount = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == 'a' || text[i] == 'e' || text[i] == 'i' ||
+                    text[i] == 'o' || text[i] == 'u' || text[i] == 'y')
+                {
+                    vowelsCount++;
+                }
+            }
+
+            Console.WriteLine(vowelsCount);
+        }
+
+    }
+}
