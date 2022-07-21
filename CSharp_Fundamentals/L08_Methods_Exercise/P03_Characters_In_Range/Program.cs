e98e7fd02692219d0bce1356e191aa9c3f743db9
Solenkoff <solenkoff@yahoo.com>
2022-07-21 14:26:20 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L08_Methods_Exercise/P03_Characters_In_Range/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _03_Characters_In_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char firstInput = char.Parse(Console.ReadLine());
+            char secondInput = char.Parse(Console.ReadLine());
+
+            AllCharsBetweenTwoGiven(firstInput, secondInput);
+
+        }
+
+
+        private static void AllCharsBetweenTwoGiven(char firstInput, char secondInput)
+        {
+            int firstChar = Math.Min(firstInput, secondInput);
+            int secondChar = Math.Max(firstInput, secondInput);
+
+            for (int i = firstChar + 1; i < secondChar; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+
+    }
+}
