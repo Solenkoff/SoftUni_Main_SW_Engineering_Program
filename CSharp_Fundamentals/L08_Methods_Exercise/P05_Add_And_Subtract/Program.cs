f53a7b6bc59a9f225d704dad521e16213a83d456
Solenkoff <solenkoff@yahoo.com>
2022-07-23 10:40:34 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L08_Methods_Exercise/P05_Add_And_Subtract/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05_Add_And_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstInteger = int.Parse(Console.ReadLine());
+            int secondInteger = int.Parse(Console.ReadLine());
+            int thirdInteger = int.Parse(Console.ReadLine());
+
+            SumOfFirstTwoIntegers(firstInteger, secondInteger);
+
+            Console.WriteLine(SubtractThirdFromSum(SumOfFirstTwoIntegers(firstInteger, secondInteger), thirdInteger));
+
+        }
+
+
+        private static int SubtractThirdFromSum(int SumOfFirstTwoIntegers, int thirdInteger)
+        {
+            int subtraction = SumOfFirstTwoIntegers - thirdInteger;
+            return subtraction;
+        }
+
+        private static int SumOfFirstTwoIntegers(int firstInteger, int secondInteger)
+        {
+            int sum = firstInteger + secondInteger;
+            return sum;
+        }
+
+    }
+}
