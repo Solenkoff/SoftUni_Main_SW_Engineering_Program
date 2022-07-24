e8b8a0b705155b16ca0140eed13ca763add6b1a3
Solenkoff <solenkoff@yahoo.com>
2022-07-24 11:48:10 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L08_Methods_Exercise/P06_Middle_Characters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06_Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            PrintTheMiddleChar(input);
+
+        }
+
+        private static void PrintTheMiddleChar(string input)
+        {
+            if (input.Length % 2 == 0)
+            {
+                string result = char.ToString(input[input.Length / 2 - 1]) +
+                                char.ToString(input[input.Length / 2]);
+                Console.WriteLine(result);
+            }
+            else if (input.Length % 2 == 1)
+            {
+                char result = input[input.Length / 2];
+                Console.WriteLine(result);
+            }
+        }
+
+    }
+}
