85e06667c95f519293c14c2b9ab1cc502fe23d53
Solenkoff <solenkoff@yahoo.com>
2022-07-25 10:51:49 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L08_Methods_Exercise/P07_NxN_Matrix/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07_NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+
+            PrintMatrixNumXNum(num);
+
+        }
+
+        private static void PrintMatrixNumXNum(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
