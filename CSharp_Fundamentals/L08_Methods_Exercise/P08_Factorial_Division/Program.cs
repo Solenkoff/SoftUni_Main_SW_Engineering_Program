2b23be6870593149327fb25cea3261d5290148d5
Solenkoff <solenkoff@yahoo.com>
2022-07-26 16:36:20 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L08_Methods_Exercise/P08_Factorial_Division/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08_Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstInt = int.Parse(Console.ReadLine());
+            int secondInt = int.Parse(Console.ReadLine());
+
+            double result = 0d;
+            result = CalculateFactorial(firstInt) / CalculateFactorial(secondInt);
+
+            Console.WriteLine($"{result:f2}");
+
+        }
+
+        private static double CalculateFactorial(int input)
+        {
+            double result = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+    }
+}
