0ca013929f0fc8f404044a1467d8e4e8ed6124ea
Solenkoff <solenkoff@yahoo.com>
2022-07-27 10:43:14 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L08_Methods_Exercise/P09_Palindrome_Integers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _09_Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine().ToLower();
+
+            while (input != "end")
+            {
+                //IsTheNumberPalindrome(input);
+                Console.WriteLine(IsTheNumberPalindrome(input).ToString().ToLower());
+                input = Console.ReadLine().ToLower();
+            }
+
+        }
+
+        private static bool IsTheNumberPalindrome(string number)
+        {
+            bool isPalindrome = false;
+
+            int numberOne = 0;
+            int currentNumber = 0;
+            int rem = 0;
+            int rev = 0;
+
+            currentNumber = numberOne = int.Parse(number);
+
+            while (numberOne > 0)
+            {
+                rem = numberOne % 10;
+                rev = rev * 10 + rem;
+                numberOne = numberOne / 10;
+
+            }
+
+            if (rev == currentNumber)
+            {
+                isPalindrome = true;
+            }
+
+            return isPalindrome;
+        }
+
+    }
+}
