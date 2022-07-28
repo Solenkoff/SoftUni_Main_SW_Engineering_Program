b2482a031bfd432b40377e43af445890a16bdc6d
Solenkoff <solenkoff@yahoo.com>
2022-07-28 11:18:56 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L08_Methods_Exercise/P10_Top_Number/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _10_Top_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+
+            TopNumbers(number);
+
+        }
+
+
+        private static void TopNumbers(int number)
+        {
+            for (int i = 17; i <= number; i++)
+            {
+                if (SumOfDigits(i) % 8 == 0 && HoldsOddDigits(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static bool HoldsOddDigits(int i)
+        {
+            bool holdsOddDigits = false;
+
+            while (i > 0)
+            {
+                int lastDigit = 0;
+                lastDigit = i % 10;
+                if (lastDigit % 2 == 1)
+                {
+                    holdsOddDigits = true;
+                }
+                i /= 10;
+            }
+
+            return holdsOddDigits;
+        }
+
+        private static int SumOfDigits(int i)
+        {
+            int sum = 0;
+            while (i > 0)
+            {
+                int lastDigit = 0;
+                lastDigit = i % 10;
+                sum += lastDigit;
+                i /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
