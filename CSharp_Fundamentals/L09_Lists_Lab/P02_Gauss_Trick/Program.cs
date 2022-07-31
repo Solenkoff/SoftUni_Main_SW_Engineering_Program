1e8d1162aaca68215af6f8ab5db864b1f5219b6d
Solenkoff <solenkoff@yahoo.com>
2022-07-31 10:34:24 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L09_Lists_Lab/P02_Gauss_Trick/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> nums = Console.ReadLine()
+                                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .ToList();
+            nums.Add(nums[0]);
+            nums.RemoveAt(0);
+
+            Console.WriteLine(string.Join(" ", nums));
+
+        }
+    }
+}
