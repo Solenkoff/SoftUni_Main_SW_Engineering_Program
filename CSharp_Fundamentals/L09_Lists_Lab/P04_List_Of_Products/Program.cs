2bb070ae542ded0f8b3894454a47953fa187039d
Solenkoff <solenkoff@yahoo.com>
2022-08-01 11:30:16 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L09_Lists_Lab/P04_List_Of_Products/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P04_List_Of_Products
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+
+            products.Sort();
+            int count = 1;
+            foreach (var product in products)
+            {
+                Console.WriteLine($"{count++}.{product}");
+            }
+
+        }
+    }
+}
