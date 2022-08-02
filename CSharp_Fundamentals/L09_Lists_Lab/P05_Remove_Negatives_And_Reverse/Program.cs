baa02c266e309fcb89ea62a19303f49df4f6c2e4
Solenkoff <solenkoff@yahoo.com>
2022-08-02 11:43:48 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L09_Lists_Lab/P05_Remove_Negatives_And_Reverse/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P05_Remove_Negatives_And_Reverse
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Where(n => n >= 0)
+                .Reverse()
+                .ToList();
+            if (numbers.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+
+        }
+    }
+}
