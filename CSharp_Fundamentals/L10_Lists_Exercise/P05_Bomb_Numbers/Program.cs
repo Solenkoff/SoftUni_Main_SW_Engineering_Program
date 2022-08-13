9c3eaf85017322ae484b13ee8fd281876d8102ec
Solenkoff <solenkoff@yahoo.com>
2022-08-13 10:18:12 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L10_Lists_Exercise/P05_Bomb_Numbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Bomb_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = Console.ReadLine()
+                                      .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(int.Parse)
+                                      .ToList();
+            List<int> bomb = Console.ReadLine()
+                                       .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(int.Parse)
+                                       .ToList();
+            int bombNumber = bomb[0];
+            int bombPower = bomb[1];
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int currentNumber = numbers[i];
+
+                if (currentNumber == bombNumber)
+                {
+                    int startIndex = i - bombPower;
+                    int endIndex = i + bombPower;
+
+                    if (startIndex < 0)
+                    {
+                        startIndex = 0;
+                    }
+                    if (endIndex > numbers.Count - 1)
+                    {
+                        endIndex = numbers.Count - 1;
+                    }
+
+                    int rangeToRemove = endIndex - startIndex + 1;
+                    numbers.RemoveRange(startIndex, rangeToRemove);
+
+                    i = startIndex - 1;
+                }
+            }
+
+            int sumOfRemaining = numbers.Sum();
+            Console.WriteLine(sumOfRemaining);
+
+        }
+    }
+}
