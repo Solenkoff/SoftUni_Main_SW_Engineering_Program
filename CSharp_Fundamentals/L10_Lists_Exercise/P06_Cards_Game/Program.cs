03f621384587c587b6a3dda6eabf8a6e5caad9aa
Solenkoff <solenkoff@yahoo.com>
2022-08-14 13:21:39 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L10_Lists_Exercise/P06_Cards_Game/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06_Cards_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> firstPlayerDeck = Console.ReadLine()
+                                      .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(int.Parse)
+                                      .ToList();
+            List<int> secondPlayerDeck = Console.ReadLine()
+                                       .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(int.Parse)
+                                       .ToList();
+
+            while (firstPlayerDeck.Count > 0 && secondPlayerDeck.Count > 0)
+            {
+                if (firstPlayerDeck[0] > secondPlayerDeck[0])
+                {
+                    firstPlayerDeck.Add(firstPlayerDeck[0]);
+                    firstPlayerDeck.Add(secondPlayerDeck[0]);
+                }
+                else if (secondPlayerDeck[0] > firstPlayerDeck[0])
+                {
+                    secondPlayerDeck.Add(secondPlayerDeck[0]);
+                    secondPlayerDeck.Add(firstPlayerDeck[0]);
+
+                }
+
+                firstPlayerDeck.RemoveAt(0);
+                secondPlayerDeck.RemoveAt(0);
+
+            }
+
+            if (firstPlayerDeck.Count == 0)
+            {
+                Console.WriteLine($"Second player wins! Sum: {secondPlayerDeck.Sum()}");
+            }
+            else if (secondPlayerDeck.Count == 0)
+            {
+                Console.WriteLine($"First player wins! Sum: {firstPlayerDeck.Sum()}");
+            }
+
+        }
+    }
+}
