0a6969bae9cb6df5ad17a3cfd2d2826f47b91ec5
Solenkoff <solenkoff@yahoo.com>
2022-08-15 11:34:07 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L10_Lists_Exercise/P07_Append_Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07_Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> input = Console.ReadLine()
+                                       .Split("|")
+                                       .ToList();
+
+            input.Reverse();
+
+            List<string> result = new List<string>();
+
+            foreach (string currentItem in input)
+            {
+                string[] numbers = currentItem
+                                     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                     .ToArray();
+                foreach (string numsToAdd in numbers)
+                {
+                    result.Add(numsToAdd);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', result));
+
+        }
+    }
+}
