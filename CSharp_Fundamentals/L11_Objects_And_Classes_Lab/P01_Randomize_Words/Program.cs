016e5e7134a81fe6cdc1409cbc686a2a9f23c8a6
Solenkoff <solenkoff@yahoo.com>
2022-08-17 10:50:16 +0200

    Added folder and solutions

---
+++ CSharp_Fundamentals/L11_Objects_And_Classes_Lab/P01_Randomize_Words/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P01_Randomize_Words
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] words = Console.ReadLine().Split();
+            Random rdm = new Random();
+            for (int i = 0; i < words.Length; i++)
+            {
+                int pos = rdm.Next(words.Length);
+                string word = words[i];
+                words[i] = words[pos];
+                words[pos] = word;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+
+        }
+    }
+}
