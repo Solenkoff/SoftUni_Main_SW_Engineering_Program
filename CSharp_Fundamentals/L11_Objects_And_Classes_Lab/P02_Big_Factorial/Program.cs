2875251fb40a2aef54af9ab84d877d61e7f991a4
Solenkoff <solenkoff@yahoo.com>
2022-08-18 13:44:03 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L11_Objects_And_Classes_Lab/P02_Big_Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P02_Big_Factorial
+{
+    using System;
+    using System.Numerics;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BigInteger factorial = 1;
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+
+        }
+    }
+}
