afdda9fa778feeee40a38ab0e54cd95d77561a14
Solenkoff <solenkoff@yahoo.com>
2022-09-05 11:21:01 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L12_Objects_And_Classes_Exercise/P02v2_Articles/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02v2_Articles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nArticles = int.Parse(Console.ReadLine());
+            List<Article> articles = new List<Article>();
+
+            for (int i = 0; i < nArticles; i++)
+            {
+
+
+                string[] input = Console.ReadLine()
+                                        .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                        .ToArray();
+
+                Article article = new Article(input[0], input[1], input[2]);
+                articles.Add(article);
+            }
+
+            string criteria = Console.ReadLine();
+
+            if (criteria == "title")
+            {
+                articles = articles.OrderBy(a => a.Title).ToList();
+            }
+            else if (criteria == "content")
+            {
+                articles.Sort((c1, c2) => c1.Content.CompareTo(c2.Content));
+            }
+            else if (criteria == "author")
+            {
+                articles = articles.OrderBy(a => a.Author).ToList();
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, articles));
+
+        }
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+
+}
