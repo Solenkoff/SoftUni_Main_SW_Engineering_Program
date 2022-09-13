e8e54012b52394cf9171bc0da09de8cc2ae4aad5
Solenkoff <solenkoff@yahoo.com>
2022-09-13 16:23:29 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L12_Objects_And_Classes_Exercise/P06_Order_By_Age/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _06_Order_By_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Person> register = new List<Person>().ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] personsData = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = personsData[0];
+                string iD = personsData[1];
+                int age = int.Parse(personsData[2]);
+
+                Person currentPerson = new Person(name, iD, age);
+                register.Add(currentPerson);
+
+
+                input = Console.ReadLine();
+            }
+
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Person person in register.OrderBy(x => x.Age))
+            {
+                sb.AppendLine($"{person.Name} with ID: {person.ID} is {person.Age} years old.");
+            }
+
+            Console.WriteLine(sb.ToString());
+
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, string iD, int age)
+        {
+            Name = name;
+            ID = iD;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+
+    }
+
+}
