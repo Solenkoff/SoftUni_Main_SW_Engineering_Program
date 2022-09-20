e63d5ba8d8ddf0db2e55ebc65f58a08ceb7cf182
Solenkoff <solenkoff@yahoo.com>
2022-09-20 10:28:06 +0200

    Added folder and solutions

---
+++ CSharp_Fundamentals/L13_Associative_Arrays_Lab/P01_Count_Real_Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace P01_Count_Real_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] nums = Console.ReadLine()
+               .Split()
+               .Select(double.Parse)
+               .ToArray();
+
+            SortedDictionary<double, int> count = new SortedDictionary<double, int>();
+
+            foreach (var num in nums)
+            {
+                if (count.ContainsKey(num))
+                {
+                    count[num]++;
+                }
+                else
+                {
+                    count.Add(num, 1);
+                }
+            }
+
+            foreach (var num in count)
+            {
+                Console.WriteLine($"{num.Key } -> {num.Value}");
+            }
+
+        }
+    }
+}
