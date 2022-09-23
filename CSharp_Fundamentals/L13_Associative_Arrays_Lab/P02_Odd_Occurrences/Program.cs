5c199fab559f0568360e98e14727b669d88ab1eb
Solenkoff <solenkoff@yahoo.com>
2022-09-23 11:22:26 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L13_Associative_Arrays_Lab/P02_Odd_Occurrences/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P02_Odd_Occurrences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] wordsCollection = Console.ReadLine()
+                .ToLower()
+                .Split(' ');
+
+            Dictionary<string, int> wordsDict = new Dictionary<string, int>();
+            foreach (string word in wordsCollection)
+            {
+                if (!wordsDict.ContainsKey(word))
+                {
+                    wordsDict[word] = 0;
+                }
+                wordsDict[word]++;
+            }
+
+            List<string> wordsList = wordsDict
+                .Where(w => w.Value % 2 != 0)
+                .Select(w => w.Key)
+                .ToList();
+
+            Console.WriteLine(string.Join(" ", wordsList));
+
+        }
+    }
+}
