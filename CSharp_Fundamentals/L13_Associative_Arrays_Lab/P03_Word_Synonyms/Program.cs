08403463e63288dfe2bc2db788589e248838f580
Solenkoff <solenkoff@yahoo.com>
2022-09-27 13:35:09 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L13_Associative_Arrays_Lab/P03_Word_Synonyms/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P03_Word_Synonyms
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string readLine = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!words.ContainsKey(readLine))
+                {
+
+                    words.Add(readLine, new List<string>());
+                }
+                words[readLine].Add(synonym);
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+
+        }
+    }
+}
