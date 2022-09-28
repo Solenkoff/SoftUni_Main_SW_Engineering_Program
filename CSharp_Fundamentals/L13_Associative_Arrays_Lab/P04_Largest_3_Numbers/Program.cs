bc35ecc80de548a236f2f480567b1d0442aa2381
Solenkoff <solenkoff@yahoo.com>
2022-09-28 11:24:44 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L13_Associative_Arrays_Lab/P04_Largest_3_Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P04_Largest_3_Numbers
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var threeBest = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .OrderByDescending(n => n)
+                .Take(3)
+                .ToList();
+
+            Console.WriteLine(string.Join(" ", threeBest));
+
+        }
+    }
+}
