ac56562b6badf0c6754d91b0e52560579ee7d172
Solenkoff <solenkoff@yahoo.com>
2022-09-30 10:31:16 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L13_Associative_Arrays_Lab/P05_Word_Filter/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P05_Word_Filter
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine()
+                   .Split()
+                   .Where(word => word.Length % 2 == 0)
+                   .ToList()
+                   .ForEach(word => Console.WriteLine(word));
+
+        }
+    }
+}
