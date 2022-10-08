bddfe40b849730a2916f45f11387e4d66eb52ffa
Solenkoff <solenkoff@yahoo.com>
2022-10-08 11:57:46 +0200

    Added folder and solutions

---
+++ CSharp_Fundamentals/L14_Associative_Arrays_Exercise/P01_Count_Chars_In_A_String/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_Count_Chars_In_A_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[] input = Console.ReadLine().ToCharArray();
+            Dictionary<char, int> occurences = new Dictionary<char, int>();
+
+            foreach (var character in input)
+            {
+                if (character != ' ')
+                {
+                    if (occurences.ContainsKey(character))
+                    {
+                        occurences[character]++;
+                    }
+                    else
+                    {
+                        occurences.Add(character, 1);
+                    }
+                }
+            }
+
+            foreach (var character in occurences)
+            {
+                char charKey = character.Key;
+                int charValue = character.Value;
+
+                Console.WriteLine($"{charKey} -> {charValue}");
+            }
+
+        }
+    }
+}
