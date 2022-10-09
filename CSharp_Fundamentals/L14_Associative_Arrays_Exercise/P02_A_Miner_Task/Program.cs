06d1fc3a790b59414b3cb06415728e3d8ad5d92d
Solenkoff <solenkoff@yahoo.com>
2022-10-09 13:54:37 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L14_Associative_Arrays_Exercise/P02_A_Miner_Task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> output = new Dictionary<string, int>();
+
+            string resource = Console.ReadLine();
+
+            while (resource != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (output.ContainsKey(resource))
+                {
+                    output[resource] += quantity;
+                }
+                else
+                {
+                    output.Add(resource, quantity);
+                }
+
+                resource = Console.ReadLine();
+            }
+
+            foreach (var item in output)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+        }
+    }
+}
