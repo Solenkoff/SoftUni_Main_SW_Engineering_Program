1d4ddea61deb761b5973b4b67104f15521232174
Solenkoff <solenkoff@yahoo.com>
2022-10-11 13:35:42 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L14_Associative_Arrays_Exercise/P04_Orders/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _04_Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, List<double>> products = new Dictionary<string, List<double>>();
+
+            string input = Console.ReadLine();
+            while (input != "buy")
+            {
+                string[] currentProduct = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string productName = currentProduct[0];
+                double productPrice = double.Parse(currentProduct[1]);
+                double productQuantity = double.Parse(currentProduct[2]);
+
+                if (products.ContainsKey(productName))
+                {
+                    products[productName][0] = productPrice;
+                    products[productName][1] += productQuantity;
+                }
+                else
+                {
+                    List<double> productData = new List<double> { productPrice, productQuantity };
+                    products.Add(productName, productData);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in products)
+            {
+                double totalPrice = item.Value[0] * item.Value[1];
+                Console.WriteLine($"{item.Key} -> {totalPrice:f2}");
+            }
+
+        }
+    }
+}
