26b3d621b2494acd5d4f59a4e7f84dff7ff832b4
Solenkoff <solenkoff@yahoo.com>
2022-10-16 10:34:42 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L14_Associative_Arrays_Exercise/P08_Company_Users/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08_Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+            while (input != "End")
+            {
+                string[] inputData = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string companyName = inputData[0];
+                string employeeID = inputData[1];
+
+                if (!companies.ContainsKey(companyName))
+                {
+                    companies.Add(companyName, new List<string>());
+
+                }
+                if (!companies[companyName].Contains(employeeID))
+                {
+                    companies[companyName].Add(employeeID);
+                }
+
+                input = Console.ReadLine();
+            }
+
+
+            foreach (var company in companies.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{company.Key}");
+                foreach (var emploee in company.Value)
+                {
+                    Console.WriteLine($"-- {emploee}");
+                }
+            }
+
+        }
+    }
+}
