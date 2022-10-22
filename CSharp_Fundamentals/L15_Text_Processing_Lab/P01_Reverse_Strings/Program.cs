f1a8e47ef0da3a21d26fe7043441e00d4f1765a3
Solenkoff <solenkoff@yahoo.com>
2022-10-22 13:55:55 +0200

    Added folder and solutions

---
+++ CSharp_Fundamentals/L15_Text_Processing_Lab/P01_Reverse_Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P01_Reverse_Strings
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+
+            while (text != "end")
+            {
+                string revesedText = string.Empty;
+
+                for (int i = text.Length - 1; i >= 0; i--)
+                {
+                    revesedText += text[i];
+                }
+
+                Console.WriteLine($"{text} = {revesedText}");
+
+                text = Console.ReadLine();
+            }
+
+        }
+    }
+}
