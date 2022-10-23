ba187a885bdcf5a1076fd57d2bb3a63f1480242c
Solenkoff <solenkoff@yahoo.com>
2022-10-23 13:12:47 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L15_Text_Processing_Lab/P02_Repeat_Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P02_Repeat_Strings
+{
+    using System;
+    using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] inputs = Console.ReadLine().Split();
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (var input in inputs)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    output.Append(input);
+                }
+            }
+
+            Console.WriteLine(output);
+
+        }
+    }
+}
