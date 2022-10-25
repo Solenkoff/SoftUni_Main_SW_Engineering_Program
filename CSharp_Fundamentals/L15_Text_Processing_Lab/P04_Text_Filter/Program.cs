cbedd32b555b3906865597827aa96254783a56f7
Solenkoff <solenkoff@yahoo.com>
2022-10-25 10:14:47 +0200

    Added homework solution

---
+++ CSharp_Fundamentals/L15_Text_Processing_Lab/P04_Text_Filter/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P04_Text_Filter
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] banWords = Console.ReadLine().Split(", ");
+
+            string text = Console.ReadLine();
+
+            foreach (var banWord in banWords)
+            {
+                if (text.Contains(banWord))
+                {
+                    text = text.Replace(banWord,
+                        new string('*', banWord.Length));
+                }
+            }
+
+            Console.WriteLine(text);
+
+        }
+    }
+}
