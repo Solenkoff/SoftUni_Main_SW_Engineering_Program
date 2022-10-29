7740f5a8977d5f7910e066981c4739f78100cb73
Solenkoff <solenkoff@yahoo.com>
2022-10-29 11:47:58 +0200

    Added folder and solutions

---
+++ CSharp_Fundamentals/L16_Text_Processing_Exercise/P01_Valid_Usernames/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01_Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine()
+                                   .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in input)
+            {
+                bool isVallid = true;
+                foreach (char charachter in word)
+                {
+                    if (!Char.IsLetterOrDigit(charachter) && charachter != '_' && charachter != '-')
+                    {
+                        isVallid = false;
+                    }
+                }
+
+                if (isVallid && word.Length >= 3 && word.Length <= 16)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+
+        }
+    }
+}
