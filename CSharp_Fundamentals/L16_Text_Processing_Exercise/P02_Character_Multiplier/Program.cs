b9537ccc4c0f6f822570d80877be36d09efd7a98
Solenkoff <solenkoff@yahoo.com>
2022-10-30 15:29:05 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L16_Text_Processing_Exercise/P02_Character_Multiplier/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _02_Character_Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine()
+                                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string stringOne = input[0];
+            string stringTwo = input[1];
+            string longString = stringOne;
+            string shortString = stringTwo;
+
+            if (stringOne.Length < stringTwo.Length)
+            {
+                longString = stringTwo;
+                shortString = stringOne;
+            }
+
+            int total = SumOfMultipliedCharacters(longString, shortString);
+
+            Console.WriteLine(total);
+
+        }
+
+        private static int SumOfMultipliedCharacters(string longString, string shortString)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < shortString.Length; i++)
+            {
+                int multiplication = shortString[i] * longString[i];
+                sum += multiplication;
+            }
+            for (int i = shortString.Length; i < longString.Length; i++)
+            {
+                sum += longString[i];
+            }
+
+            return sum;
+        }
+
+    }
+}
