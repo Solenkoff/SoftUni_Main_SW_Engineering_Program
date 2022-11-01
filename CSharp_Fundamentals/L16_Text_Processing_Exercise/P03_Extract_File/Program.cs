c3733dab204c5bc00f00e1cc4dc6e67c34583e5b
Solenkoff <solenkoff@yahoo.com>
2022-11-01 10:42:33 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L16_Text_Processing_Exercise/P03_Extract_File/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03_Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] inputString = Console.ReadLine().Split('\\', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] outputString = inputString[inputString.Length - 1].Split('.');
+
+            Console.WriteLine($"File name: {outputString[0]}");
+            Console.WriteLine($"File extension: {outputString[1]}");
+
+        }
+    }
+}
