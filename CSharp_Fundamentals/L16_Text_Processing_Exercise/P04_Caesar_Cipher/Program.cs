c58aba696d72d69a6704199fa412aa9d86b42a05
Solenkoff <solenkoff@yahoo.com>
2022-11-02 11:22:04 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L16_Text_Processing_Exercise/P04_Caesar_Cipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04_Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+            string encryptedString = string.Empty;
+
+            foreach (var item in input)
+            {
+                char newChar = (char)(item + 3);
+                encryptedString += newChar;
+            }
+
+            Console.WriteLine(encryptedString);
+
+        }
+    }
+}
