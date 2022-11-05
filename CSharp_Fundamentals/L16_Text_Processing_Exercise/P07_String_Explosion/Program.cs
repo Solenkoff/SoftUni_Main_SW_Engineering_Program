1da88e476aa19eaf2f4ca5b0014d42a2723bbdf4
Solenkoff <solenkoff@yahoo.com>
2022-11-05 11:12:17 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L16_Text_Processing_Exercise/P07_String_Explosion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07_String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[] input = Console.ReadLine().ToCharArray();
+
+            List<char> charInput = input.ToList();
+            int power = 0;
+            for (int i = 0; i < charInput.Count; i++)
+            {
+                if (charInput[i] == '>')
+                {
+                    power += (int)char.GetNumericValue(charInput[i + 1]) - 1;
+                    charInput.RemoveAt(i + 1);
+                }
+                else
+                {
+                    if (power > 0)
+                    {
+                        charInput.RemoveAt(i);
+                        power--;
+                        i--;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join("", charInput));
+
+        }
+    }
+}
