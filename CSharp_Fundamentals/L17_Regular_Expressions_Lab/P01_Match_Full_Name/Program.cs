43b62c287798ccb68f456a29b9c97eab114a654e
Solenkoff <solenkoff@yahoo.com>
2022-11-08 14:35:45 +0100

    Added folder and solutions

---
+++ CSharp_Fundamentals/L17_Regular_Expressions_Lab/P01_Match_Full_Name/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P01_Match_Full_Name
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MatchCollection matches = Regex.Matches(Console.ReadLine(), @"\b[A-Z][a-z]+ [A-Z][a-z]+");
+
+            foreach (Match match in matches)
+            {
+                Console.Write($"{match.Value} ");
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
