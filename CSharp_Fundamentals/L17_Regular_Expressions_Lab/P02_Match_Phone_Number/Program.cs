32da3544215f296147b0cd57bc518ac9df9288cc
Solenkoff <solenkoff@yahoo.com>
2022-11-09 10:37:24 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L17_Regular_Expressions_Lab/P02_Match_Phone_Number/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace P02_Match_Phone_Number
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MatchCollection output = Regex.Matches(Console.ReadLine(), @"\+359([ -])2\1\d{3}\1\d{4}\b");
+
+            Console.WriteLine(string.Join(", ", output));
+
+        }
+    }
+}
