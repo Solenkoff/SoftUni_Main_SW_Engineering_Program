cde1e177c8dc25a8a80a0a7e2e7a5f8bad5f0a13
Solenkoff <solenkoff@yahoo.com>
2022-11-10 11:43:03 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L17_Regular_Expressions_Lab/P03_Match_Dates/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P03_Match_Dates
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MatchCollection dates = Regex
+                .Matches(Console.ReadLine(), @"(?<day>[0-9]{2})(?<separator>.)(?<month>[A-Za-z]{3})\k<separator>(\d{4})");
+
+
+            foreach (Match day in dates)
+            {
+                Console.WriteLine($"Day: {day.Groups["day"]}, Month: {day.Groups["month"]}, Year: {day.Groups[1]}");
+            }
+
+        }
+    }
+}
