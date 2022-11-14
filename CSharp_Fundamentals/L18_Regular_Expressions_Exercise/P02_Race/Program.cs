044c492bf3ed5052581a2dd73a26b82ebd50bc77
Solenkoff <solenkoff@yahoo.com>
2022-11-14 11:04:01 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L18_Regular_Expressions_Exercise/P02_Race/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> participants = Console.ReadLine()
+                                             .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                             .ToList();
+
+            Dictionary<string, int> validRacers = new Dictionary<string, int>();
+
+            foreach (var name in participants)
+            {
+                validRacers.Add(name, 0);
+            }
+
+            string namePattern = @"[\W\d]";
+            string distancePattern = @"[^\d]";
+
+            string input = Console.ReadLine();
+
+            while (input != "end of race")
+            {
+
+                string name = Regex.Replace(input, namePattern, "");
+
+                if (validRacers.ContainsKey(name))
+                {
+                    string distanceNums = Regex.Replace(input, distancePattern, "");
+                    int distance = 0;
+
+                    foreach (var digit in distanceNums)
+                    {
+                        int currentDigit = int.Parse(digit.ToString());
+                        distance += currentDigit;
+                    }
+
+                    validRacers[name] += distance;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            List<string> winners = new List<string>();
+            foreach (var item in validRacers.OrderByDescending(x => x.Value))
+            {
+                winners.Add(item.Key);
+            }
+
+            Console.WriteLine($"1st place: {winners[0]}");
+            Console.WriteLine($"2nd place: {winners[1]}");
+            Console.WriteLine($"3rd place: {winners[2]}");
+
+        }
+    }
+}
