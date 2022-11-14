7aee3c5c95baa819624cd904089f435d0c88da98
Solenkoff <solenkoff@yahoo.com>
2022-11-14 13:43:03 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L18_Regular_Expressions_Exercise/P03_SoftUni_Bar_Income/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03_SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string pattern = @"%(?<name>[A-Z][a-z]+)%[^\.\|\$\%]*<(?<product>\w+)>[^\.\|\$\%]*\|(?<count>\d+)\|[^\.\|\$\%]*?(?<price>\d+\.?\d*)\$";
+
+            Regex regex = new Regex(pattern);
+
+            double total = 0;
+
+            string input = Console.ReadLine();
+
+            while (input != "end of shift")
+            {
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    string name = match.Groups["name"].Value;
+                    string product = match.Groups["product"].Value;
+                    int quantity = int.Parse(match.Groups["count"].Value);
+                    double price = double.Parse(match.Groups["price"].Value);
+                    double orderTotal = price * quantity;
+
+                    total += orderTotal;
+
+                    Console.WriteLine($"{name}: {product} - {orderTotal:f2}");
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total income: {total:f2}");
+
+        }
+    }
+}
