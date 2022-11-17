6cd5fd5784082d6411ec7e80799bf53f21cb76b3
Solenkoff <solenkoff@yahoo.com>
2022-11-17 14:11:59 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/L18_Regular_Expressions_Exercise/P06_Extract_Emails/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P06_Extract_Emails
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            string pattern = @"(?<=\s)(?<user>[A-Za-z0-9]+[.-]*\w*)*@(?<host>[a-z]+?([.-][a-z]*)*(\.[a-z]{2,}))";
+
+            var correct = Regex.Matches(input, pattern);
+
+            Console.WriteLine(String.Join(Environment.NewLine, correct));
+
+        }
+    }
+}
