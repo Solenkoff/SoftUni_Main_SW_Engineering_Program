6252e03728dbeff610d379c5050a1d7ded33aa20
Solenkoff <solenkoff@yahoo.com>
2022-11-20 11:12:51 +0100

    Added homework solution

---
+++ CSharp_Fundamentals/Exams/Programming_Fundamentals_2021-04-09_Final_Exam_Retake(M)/P02_Employees_Info/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P02_Employees_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool uh = 100f == 100d;
+
+            Console.WriteLine(uh);
+        }
+    }
+}
