9c226630d6c437bfe114a51fb6592024130f672f
Solenkoff <solenkoff@yahoo.com>
2022-11-23 14:54:03 +0100

    Added folder and solutions

---
+++ CSharp_Fundamentals/Exams/Programming_Fundamentals_2020-02-29_Mid_Exam_Group_1/P01_Bonus_Scoring_System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_Bonus_Scoring_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
