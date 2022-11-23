ab096a203de7971bbe6325991cef79542b875e5c
Solenkoff <solenkoff@yahoo.com>
2022-11-23 15:03:34 +0100

    Added problem solution

---
+++ CSharp_Fundamentals/Exams/Programming_Fundamentals_2020-02-29_Mid_Exam_Group_1/P02_Mu_Online/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_Mu_Online
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
