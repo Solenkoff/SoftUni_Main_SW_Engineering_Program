9d06304d8d5bc76eee68070f15067d70ba975bd0
Solenkoff <solenkoff@yahoo.com>
2022-11-29 11:41:00 +0100

    Added problem solution

---
+++ CSharp_Fundamentals/Exams/Programming_Fundamentals_2020-02-29_Mid_Exam_Group_2/P01_National_Court/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01_National_Court
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstEmploeeE = int.Parse(Console.ReadLine());
+            int secondEmploeeE = int.Parse(Console.ReadLine());
+            int thirdEmploeeE = int.Parse(Console.ReadLine());
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            int totalPerHour = firstEmploeeE + secondEmploeeE + thirdEmploeeE;
+
+            int hours = 0;
+
+            while (peopleCount > 0)
+            {
+                peopleCount -= totalPerHour;
+                hours++;
+
+                if (hours % 4 == 0)
+                {
+                    hours++;
+                }
+
+            }
+
+            Console.WriteLine($"Time needed: {hours}h.");
+
+        }
+    }
+}
