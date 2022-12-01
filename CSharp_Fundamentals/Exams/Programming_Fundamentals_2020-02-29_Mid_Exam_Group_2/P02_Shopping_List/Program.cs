bcea6e9dcf65de378ac6877d8e33f9a239c1e244
Solenkoff <solenkoff@yahoo.com>
2022-12-01 13:51:30 +0100

    Added problem solution

---
+++ CSharp_Fundamentals/Exams/Programming_Fundamentals_2020-02-29_Mid_Exam_Group_2/P02_Shopping_List/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02_Shopping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> groceries = Console.ReadLine()
+                                           .Split('!', StringSplitOptions.RemoveEmptyEntries)
+                                           .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "Go Shopping!")
+            {
+                string[] commands = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string command = commands[0];
+                string item = commands[1];
+
+                if (command == "Urgent")
+                {
+
+                    if (!groceries.Contains(item))
+                    {
+                        groceries.Insert(0, item);
+                    }
+
+                }
+                else if (command == "Unnecessary")
+                {
+
+                    if (groceries.Contains(item))
+                    {
+                        groceries.Remove(item);
+                    }
+
+                }
+                else if (command == "Correct")
+                {
+                    string newItem = commands[2];
+                    if (groceries.Contains(item))
+                    {
+                        int index = groceries.IndexOf(item);
+                        groceries.Remove(item);
+                        groceries.Insert(index, newItem);
+
+                    }
+
+                }
+                else if (command == "Rearrange")
+                {
+                    if (groceries.Contains(item))
+                    {
+                        groceries.Remove(item);
+                        groceries.Add(item);
+                    }
+
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", groceries));
+
+        }
+    }
+}
