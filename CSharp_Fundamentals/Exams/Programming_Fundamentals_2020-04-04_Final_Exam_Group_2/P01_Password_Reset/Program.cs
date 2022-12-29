e53252fe51ccca26ea010bb12321d6eb31cf0547
Solenkoff <solenkoff@yahoo.com>
2022-12-29 14:47:05 +0100

    Added folder and solutions

---
+++ CSharp_Fundamentals/Exams/Programming_Fundamentals_2020-04-04_Final_Exam_Group_2/P01_Password_Reset/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _01_Password_Reset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputText = Console.ReadLine();
+
+            string newPassword = string.Empty;
+
+            string input = Console.ReadLine();
+            while (input != "Done")
+            {
+                string[] commands = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string command = commands[0];
+
+                if (command == "TakeOdd")
+                {
+
+                    for (int i = 0; i < inputText.Length; i++)
+                    {
+                        if (i % 2 == 1)
+                        {
+                            newPassword += inputText[i];
+                        }
+
+                    }
+
+                    Console.WriteLine(newPassword);
+                }
+                else if (command == "Cut")
+                {
+                    int index = int.Parse(commands[1]);
+                    int lenght = int.Parse(commands[2]);
+
+                    newPassword = newPassword.Remove(index, lenght);
+
+                    Console.WriteLine(newPassword);
+
+                }
+                else if (command == "Substitute")
+                {
+                    string substring = commands[1];
+                    string substitute = commands[2];
+
+                    if (newPassword.Contains(substring))
+                    {
+                        newPassword = newPassword.Replace(substring, substitute);
+
+                        Console.WriteLine(newPassword);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing to replace!");
+                    }
+
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your password is: {newPassword}");
+
+        }
+    }
+}
