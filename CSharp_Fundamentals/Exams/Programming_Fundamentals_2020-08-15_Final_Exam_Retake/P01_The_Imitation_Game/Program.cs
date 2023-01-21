77a5b9338e426a0876d2402e78abc0c4011b1891
Solenkoff <solenkoff@yahoo.com>
2023-01-21 10:41:45 +0100

    Added folder and solutions

---
+++ CSharp_Fundamentals/Exams/Programming_Fundamentals_2020-08-15_Final_Exam_Retake/P01_The_Imitation_Game/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _01_The_Imitation_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string message = Console.ReadLine();
+
+
+            string input = Console.ReadLine();
+
+            while (input != "Decode")
+            {
+                string[] commands = input.Split('|', StringSplitOptions.RemoveEmptyEntries);
+                string command = commands[0];
+
+                if (command == "Move")
+                {
+                    int numLetters = int.Parse(commands[1]);
+                    string beginning = message.Substring(0, numLetters);
+                    message = message.Remove(0, numLetters);
+                    message = message + beginning;
+
+                }
+                if (command == "Insert")
+                {
+                    int index = int.Parse(commands[1]);
+                    string toInsert = commands[2];
+
+                    message = message.Substring(0, index) + toInsert + message.Substring(index);
+                }
+                if (command == "ChangeAll")
+                {
+                    string toReplace = commands[1];
+                    string replacement = commands[2];
+
+                    message = message.Replace(toReplace, replacement);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The decrypted message is: {message}");
+
+        }
+    }
+}
