026cfc0a8f63f4d882769f2fe1e57679538f391c
Solenkoff <solenkoff@yahoo.com>
2023-01-28 15:14:02 +0100

    Added folder and solutions

---
+++ CSharp_Fundamentals/Exams/Programming_Fundamentals_2020-11-07_Mid_Exam(M)/P01_Bakery/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01_Bakery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int bisquitsPerWorker = int.Parse(Console.ReadLine());
+            int nWorkers = int.Parse(Console.ReadLine());
+            int competitorsTotal = int.Parse(Console.ReadLine());
+
+            int bisquits3thDay = (int)Math.Floor(nWorkers * bisquitsPerWorker * 0.75) * 10;
+            int factoryTotal = nWorkers * bisquitsPerWorker * 20 + bisquits3thDay;
+
+            Console.WriteLine($"You have produced {factoryTotal} biscuits for the past month.");
+
+            if (factoryTotal > competitorsTotal)
+            {
+                double difference = 1.0 * (factoryTotal - competitorsTotal) / competitorsTotal * 100;
+                Console.WriteLine($"You produce {difference:f2} percent more biscuits.");
+            }
+            else if (competitorsTotal > factoryTotal)
+            {
+                double difference = 1.0 * (competitorsTotal - factoryTotal) / competitorsTotal * 100;
+                Console.WriteLine($"You produce {difference:f2} percent less biscuits.");
+            }
+
+        }
+    }
+}
