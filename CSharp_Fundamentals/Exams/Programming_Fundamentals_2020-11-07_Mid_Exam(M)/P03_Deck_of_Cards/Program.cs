669aa792c7b3550cb639984cccd6512c7a2cd01a
Solenkoff <solenkoff@yahoo.com>
2023-01-31 11:10:06 +0100

    Added problem solution

---
+++ CSharp_Fundamentals/Exams/Programming_Fundamentals_2020-11-07_Mid_Exam(M)/P03_Deck_of_Cards/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_Deck_of_Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> tomsCars = Console.ReadLine()
+                                           .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                                           .ToList();
+            int nComands = int.Parse(Console.ReadLine());
+
+            List<string> comand = Console.ReadLine().Split().ToList;
+
+            for (int i = 0; i < nComands - 1; i++)
+            {
+
+
+
+            }
+        }
+    }
+}
