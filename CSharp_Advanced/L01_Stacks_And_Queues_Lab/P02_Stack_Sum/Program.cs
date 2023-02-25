d131b2aa9b674f43a8777eb6835eacd3602dd7d6
Solenkoff <solenkoff@yahoo.com>
2023-02-25 13:13:16 +0100

    Added homework solution

---
+++ CSharp_Advanced/L01_Stacks_And_Queues_Lab/P02_Stack_Sum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var stack = new Stack<int>(inputArray);
+                        
+            while (true)
+            {
+                string[] commands = Console.ReadLine().ToLower().Split();
+                
+                if (commands[0] == "add")
+                {
+                    stack.Push(int.Parse(commands[1]));
+                    stack.Push(int.Parse(commands[2]));
+                }
+                else if (commands[0] == "remove")
+                {
+                    int numsToRemove = int.Parse(commands[1]);
+                    if (stack.Count >= numsToRemove)
+                    {
+                        for (int i = 0; i < numsToRemove; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+         
+            Console.WriteLine($"Sum: {stack.Sum()}");
+
+        }
+    }
+}
---
+++ CSharp_Advanced/L01_Stacks_And_Queues_Lab/P03_Simple_Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var orderedStack = new Stack<string>(input.Reverse());
+          
+            while (orderedStack.Count > 1)
+            {
+                int elementOne = int.Parse(orderedStack.Pop());
+                string operation = orderedStack.Pop();
+                int elementTwo = int.Parse(orderedStack.Pop());
+
+                if (operation == "+")
+                {
+                    orderedStack.Push((elementOne + elementTwo).ToString());
+                }
+                else if (operation == "-")
+                {
+                    orderedStack.Push((elementOne - elementTwo).ToString());
+                }
+            }
+
+            Console.WriteLine(orderedStack.Pop());
+
+        }
+    }
+}
