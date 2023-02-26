b34ece09f85d003cc2674ea2a73149cdfe950562
Solenkoff <solenkoff@yahoo.com>
2023-02-26 12:05:45 +0100

    Added homework solution

---
+++ CSharp_Advanced/L01_Stacks_And_Queues_Lab/P05_Print_Even_Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = Console.ReadLine()
+                                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            var queueOfIntegers = new Queue<int>(input);
+
+            var evenIntegers = new List<int>();
+
+            while (queueOfIntegers.Any())
+            {
+                if (queueOfIntegers.Peek() % 2 == 0)
+                {
+                    evenIntegers.Add(queueOfIntegers.Dequeue());
+                }
+                else
+                {
+                    queueOfIntegers.Dequeue();
+                }
+            }
+                          
+            Console.WriteLine(string.Join(", ", evenIntegers));
+
+        }
+    }
+}
