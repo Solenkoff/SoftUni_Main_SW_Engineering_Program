9cb57677720b3f3a91864dc349e4011655edee6a
Solenkoff <solenkoff@yahoo.com>
2023-02-26 12:05:25 +0100

    Added homework solution

---
+++ CSharp_Advanced/L01_Stacks_And_Queues_Lab/P04_Matching_Brackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            var parenthesis = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    parenthesis.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int indexBeginning = parenthesis.Pop();
+                    Console.WriteLine(input.Substring(indexBeginning, i - indexBeginning + 1));
+                }
+            }
+       
+        }
+    }
+}
