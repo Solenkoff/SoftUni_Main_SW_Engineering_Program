214e5d949a0b504bef8ec63b2bed879426d215c3
Solenkoff <solenkoff@yahoo.com>
2023-02-27 15:10:25 +0100

    Added homework solution

---
+++ CSharp_Advanced/L01_Stacks_And_Queues_Lab/P06_Supermarket/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new Queue<string>();
+
+            string name = Console.ReadLine();
+
+            while (name != "End")
+            {
+                if (name == "Paid")
+                {
+                    foreach (var item in names)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    names.Clear();
+                }
+                else
+                {
+                    names.Enqueue(name);
+                }
+
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{names.Count} people remaining.");
+
+        }
+    }
+}
