caf85c9fc3e1c315c86e37cb762b72501ce20446
Solenkoff <solenkoff@yahoo.com>
2023-02-28 16:29:52 +0100

    Added homework solution

---
+++ CSharp_Advanced/L01_Stacks_And_Queues_Lab/P07_Hot_Potato/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07_Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] kidsNames = Console.ReadLine().Split();
+            int tosses = int.Parse(Console.ReadLine());
+
+            var kids = new Queue<string>(kidsNames);
+            int tossesCounter = 1;
+            while (kids.Count > 1)
+            {
+               
+                if (tossesCounter == tosses)
+                {
+                    Console.WriteLine($"Removed {kids.Dequeue()}"); 
+                    tossesCounter = 1;
+                }
+                else
+                {
+                    string kid = kids.Dequeue();
+                    kids.Enqueue(kid);
+                    tossesCounter++;
+                }
+            }
+
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+
+        }
+    }
+}
