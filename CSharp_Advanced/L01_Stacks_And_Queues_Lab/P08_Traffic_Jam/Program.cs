58817d33e1b66f1ad559d68cdcd3eaf95f78f27e
Solenkoff <solenkoff@yahoo.com>
2023-03-01 14:09:12 +0100

    Added homework solution

---
+++ CSharp_Advanced/L01_Stacks_And_Queues_Lab/P08_Traffic_Jam/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08_Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int carLimit = int.Parse(Console.ReadLine());
+
+            var cars = new Queue<string>();
+            int nCars = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end") 
+                {
+                    break;
+                }
+                else if (input == "green")
+                {
+                    if (carLimit > cars.Count)
+                    {
+                        carLimit = cars.Count;
+                    }
+                    for (int i = 0; i < carLimit; i++)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        nCars++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+
+            }
+
+            Console.WriteLine($"{nCars} cars passed the crossroads.");
+
+        }
+    }
+}
