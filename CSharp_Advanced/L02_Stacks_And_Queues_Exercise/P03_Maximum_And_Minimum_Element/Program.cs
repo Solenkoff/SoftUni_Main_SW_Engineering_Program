ae0779c325c2157ca3027a7b98c1e8d878627d1b
Solenkoff <solenkoff@yahoo.com>
2023-03-02 16:52:31 +0100

    Added homework solution

---
+++ CSharp_Advanced/L02_Stacks_And_Queues_Exercise/P03_Maximum_And_Minimum_Element/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_Maximum_And_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nQueries = int.Parse(Console.ReadLine());
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < nQueries; i++)
+            {
+                int[] queries = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if (queries[0] == 1)
+                {
+                    stack.Push(queries[1]);
+                }
+                else if (queries[0] == 2)
+                {
+                    if(stack.Any())
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (queries[0] == 3)
+                {
+                    if (stack.Any())
+                    {
+                        Console.WriteLine(stack.Max());
+                    }                      
+                }
+                else if (queries[0] == 4)
+                {
+                    if (stack.Any())
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+
+        }
+    }
+}
