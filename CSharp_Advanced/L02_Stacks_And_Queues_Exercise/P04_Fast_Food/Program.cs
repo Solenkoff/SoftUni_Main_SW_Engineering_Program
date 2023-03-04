6a9b370f536d41ea0591c1392365943ca212ebf0
Solenkoff <solenkoff@yahoo.com>
2023-03-04 11:20:08 +0100

    Added homework solution

---
+++ CSharp_Advanced/L02_Stacks_And_Queues_Exercise/P04_Fast_Food/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int food = int.Parse(Console.ReadLine());
+
+            int[] input = Console.ReadLine()
+                                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            var orders = new Queue<int>(input);
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Any())
+            {
+                if (food >= orders.Peek())
+                {
+                    food -= orders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            if (orders.Any())
+            {
+                Console.WriteLine($"Orders left: {string.Join(' ', orders)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+
+        }
+    }
+}
