d021ba76cb7652a6b7139896d9403f59675dd8a2
Solenkoff <solenkoff@yahoo.com>
2023-03-06 11:12:46 +0100

    Added homework solution

---
+++ CSharp_Advanced/L02_Stacks_And_Queues_Exercise/P06_Songs_Queue/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06_Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            var songs = new Queue<string>(input);
+
+            while (songs.Any())
+            {
+                string songsInput = Console.ReadLine();
+                string[] commands = songsInput.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string command = commands[0];
+
+                if (command == "Play")
+                {
+                    songs.Dequeue();
+                }
+                else if (command == "Add")
+                {            
+                    string song = songsInput.Substring(4);
+
+                    if (songs.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(song);
+                    }
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+               
+            }
+
+            Console.WriteLine("No more songs!");
+
+        }
+    }
+}
