d21c8929c256e093d91f4e944134177343d4278f
Solenkoff <solenkoff@yahoo.com>
2023-03-06 11:11:11 +0100

    Added homework solution

---
+++ CSharp_Advanced/L02_Stacks_And_Queues_Exercise/P05v2_Fashion_Boutique/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05v2_Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = Console.ReadLine()
+                                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            var clothes = new Stack<int>(input);
+
+            int currentRackCapacity = rackCapacity;
+            int racks = 1;
+
+            while (clothes.Any())
+            {
+                int onePiece = clothes.Pop();
+                currentRackCapacity -= onePiece;
+
+                if (currentRackCapacity < 0)
+                {
+                    racks++;
+                    currentRackCapacity = rackCapacity - onePiece;
+                    //currentRackCapacity -= onePiece;
+                }
+            }
+
+            Console.WriteLine(racks);
+
+        }
+    }
+}
