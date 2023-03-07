e327f6957e8f5ce080465f427bf842e57563918e
Solenkoff <solenkoff@yahoo.com>
2023-03-07 18:11:12 +0100

    Added homework solution

---
+++ CSharp_Advanced/L02_Stacks_And_Queues_Exercise/P08_Balanced_Parenthesis/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08_Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            var stack = new Stack<char>();
+
+            bool isBalanced = true;
+
+            foreach (var ch in input)
+            {
+                if (ch == '(' || ch == '{' || ch == '[')
+                {
+                    stack.Push(ch);
+                }
+                else if (ch == ')' || ch == '}' || ch == ']')
+                {
+                    if (!stack.Any())
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                    else
+                    {
+                        char lastChar = stack.Pop();
+                        if (ch == ')' && lastChar == '(' || 
+                            ch == '}' && lastChar == '{' || 
+                            ch == ']' && lastChar == '[')
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            isBalanced = false;
+                            break;
+                        }
+
+                    }
+                }
+            }
+            
+          //if (stack.Any())
+          //{
+          //    isBalanced = false;
+          //}
+
+            if(isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }     
+    }
+}
