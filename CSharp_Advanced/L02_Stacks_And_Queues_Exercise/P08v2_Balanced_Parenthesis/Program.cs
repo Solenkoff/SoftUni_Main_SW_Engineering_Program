45d7d995c453a4fd7c9d2e1a5b4cbd08da6997ae
Solenkoff <solenkoff@yahoo.com>
2023-03-07 18:11:34 +0100

    Added homework solution

---
+++ CSharp_Advanced/L02_Stacks_And_Queues_Exercise/P08v2_Balanced_Parenthesis/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08v2_Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+
+            if (IsWellFormatted(input))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }
+
+
+        private static bool IsWellFormatted(string line)
+        {
+            Stack<char> lastOpen = new Stack<char>();
+            foreach (var c in line)
+            {
+                switch (c)
+                {
+                    case ')':
+                        if (lastOpen.Count == 0 || lastOpen.Pop() != '(') return false;
+                        break;
+                    case ']':
+                        if (lastOpen.Count == 0 || lastOpen.Pop() != '[') return false;
+                        break;
+                    case '}':
+                        if (lastOpen.Count == 0 || lastOpen.Pop() != '{') return false;
+                        break;
+                    case '(': lastOpen.Push(c); break;
+                    case '[': lastOpen.Push(c); break;
+                    case '{': lastOpen.Push(c); break;
+                }
+            }
+            if (lastOpen.Count == 0) return true;
+            else return false;
+
+        }
+    }
+}
