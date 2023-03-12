cfb0a08a5884ea89317eda35037bb55be1865a31
Solenkoff <solenkoff@yahoo.com>
2023-03-12 11:02:24 +0100

    Added folder and solutions

---
+++ CSharp_Advanced/L03_Multidimensional_Arrays_Lab/P01_Sum_Matrix_Elements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = Console.ReadLine()
+                                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+            int rows = input[0];
+            int cols = input[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine()
+                                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            int sum = 0;
+
+            foreach (var item in matrix)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
