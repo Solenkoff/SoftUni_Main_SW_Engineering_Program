4bb3292ab24dd3d1a551aba66a80d7f8a4e2b616
Solenkoff <solenkoff@yahoo.com>
2023-03-14 14:05:17 +0100

    Added homework solution

---
+++ CSharp_Advanced/L03_Multidimensional_Arrays_Lab/P03_Primary_Diagonal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int inputN = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[inputN, inputN];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine()
+                                       .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(int.Parse)
+                                       .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            int primeDiagonalSum = 0;
+
+            for (int i = 0; i < inputN; i++)
+            {
+                primeDiagonalSum += matrix[i,i];
+            }
+
+            Console.WriteLine(primeDiagonalSum);
+
+        }
+    }
+}
