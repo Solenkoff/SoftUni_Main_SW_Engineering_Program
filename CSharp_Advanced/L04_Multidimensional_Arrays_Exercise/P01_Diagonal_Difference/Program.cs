efbf6820913e9668599effd0b9ca6fd2b4894bd9
Solenkoff <solenkoff@yahoo.com>
2023-03-17 16:39:06 +0100

    Added folder and solutions

---
+++ CSharp_Advanced/L04_Multidimensional_Arrays_Exercise/P01_Diagonal_Difference/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowData = Console.ReadLine()
+                                       .Split()
+                                       .Select(int.Parse)
+                                       .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            
+            int primaryDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                primaryDiagonalSum += matrix[i, i];
+                secondaryDiagonalSum += matrix[i, (n - 1 - i)];
+            }
+            
+            Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
+            
+        }
+    }
+}
