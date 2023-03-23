2abb45cfdeaf07c3319a74afdddb505ab01099ca
Solenkoff <solenkoff@yahoo.com>
2023-03-23 15:38:56 +0100

    Added folder and solutions

---
+++ CSharp_Advanced/L05_Sets_And_Dictionaries_Advanced_Lab/P01_Count_Same_Values_In_Array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Count_Same_Values_In_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] nums = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(double.Parse)
+                                   .ToArray();
+
+            Dictionary<double, int> numsAppearances = new Dictionary<double, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(!numsAppearances.ContainsKey(nums[i]))
+                {
+                    numsAppearances.Add(nums[i], 0);
+
+                }
+
+                numsAppearances[nums[i]]++;
+                
+            }
+
+            foreach (var num in numsAppearances)
+            {
+                Console.WriteLine($"{num.Key} - {num.Value} times");
+            }
+
+        }
+    }
+}
