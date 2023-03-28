5c49122b3c86a5b6dd94b039d203eead65ef5871
Solenkoff <solenkoff@yahoo.com>
2023-03-28 23:24:26 +0200

    Added homework solution

---
+++ CSharp_Advanced/L05_Sets_And_Dictionaries_Advanced_Lab/P06_Parking_Lot/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _06_Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+           
+            HashSet<string> cars = new HashSet<string>();
+
+            while (input != "END")
+            {
+                
+                var data = Regex.Split(input, ", ");
+
+                string direction = data[0];
+                string carNumber = data[1];
+
+                if (direction == "IN")
+                {
+                    cars.Add(carNumber);
+                }
+                else if (direction == "OUT")
+                {
+                    cars.Remove(carNumber);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (cars.Any())
+            {
+                foreach (var carNumber in cars)
+                {
+                    Console.WriteLine(carNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            
+        }
+    }
+}
