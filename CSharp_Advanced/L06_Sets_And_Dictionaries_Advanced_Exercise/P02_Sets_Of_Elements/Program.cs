e4c8ce2ffc9e3d62c07fc82541fd52d6881de9f9
Solenkoff <solenkoff@yahoo.com>
2023-03-31 10:54:18 +0200

    Added homework solution

---
+++ CSharp_Advanced/L06_Sets_And_Dictionaries_Advanced_Exercise/P02_Sets_Of_Elements/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Sets_Of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] setsLength = Console.ReadLine()
+                                      .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(int.Parse)
+                                      .ToArray();
+
+            HashSet<int> setOne = new HashSet<int>();
+            HashSet<int> setTwo = new HashSet<int>();
+
+            int setOneLenght = setsLength[0];
+            int setTwoLenght = setsLength[1];
+            int totalLenght = setOneLenght + setTwoLenght;
+
+            for (int i = 0; i < setOneLenght; i++)
+            {
+                int numOne = int.Parse(Console.ReadLine());
+                setOne.Add(numOne);
+            }
+            for (int i = 0; i < setTwoLenght; i++)
+            {
+                int numTwo = int.Parse(Console.ReadLine());
+                setTwo.Add(numTwo);
+            }
+
+            foreach (var item in setOne)
+            {
+                if(setTwo.Contains(item))
+                {
+                    Console.Write(item + " ");
+                }
+            }
+
+        }
+    }
+}
