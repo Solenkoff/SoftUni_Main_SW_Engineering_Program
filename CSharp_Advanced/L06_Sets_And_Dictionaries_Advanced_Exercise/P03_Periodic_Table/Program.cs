a82e29f8cef465322189dd92a145cddd9f968d43
Solenkoff <solenkoff@yahoo.com>
2023-04-01 13:43:24 +0200

    Added homework solution

---
+++ CSharp_Advanced/L06_Sets_And_Dictionaries_Advanced_Exercise/P03_Periodic_Table/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> elements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elsementsInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var item in elsementsInput)
+                {
+                    elements.Add(item);
+                }
+            }
+
+            foreach (var item in elements)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+    }
+}
