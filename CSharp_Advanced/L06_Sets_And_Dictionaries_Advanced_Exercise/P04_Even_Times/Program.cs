985128f08395d4d5ffc455790b12240afea0fe17
Solenkoff <solenkoff@yahoo.com>
2023-04-02 15:52:54 +0200

    Added homework solution

---
+++ CSharp_Advanced/L06_Sets_And_Dictionaries_Advanced_Exercise/P04_Even_Times/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<int> nums = new HashSet<int>();
+            HashSet<int> numsEvenOccurrences = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (nums.Contains(num))
+                {
+
+                    if(numsEvenOccurrences.Contains(num))
+                    {
+                        numsEvenOccurrences.Remove(num);
+                    }
+                    else
+                    {
+                        numsEvenOccurrences.Add(num);
+                    }
+
+                }
+                else
+                {
+                    nums.Add(num);
+                }
+
+            }
+
+            foreach (var item in numsEvenOccurrences)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
