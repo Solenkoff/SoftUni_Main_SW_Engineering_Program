9fe7401c471eabdd69a3000d7699f32ca226815e
Solenkoff <solenkoff@yahoo.com>
2023-04-03 22:28:04 +0200

    Added homework solution

---
+++ CSharp_Advanced/L06_Sets_And_Dictionaries_Advanced_Exercise/P05_Count_Symbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05_Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            SortedDictionary<char, int> accurrances = new SortedDictionary<char, int>();
+
+            foreach (var ch in input)
+            {
+                if(!accurrances.ContainsKey(ch))
+                {
+                    accurrances.Add(ch, 0);
+                }
+
+                accurrances[ch]++;
+            }
+
+            foreach (var ch in accurrances)
+            {
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+
+            }
+        }
+    }
+}
