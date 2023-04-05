249cb506924f42e3ded11ce918425aeebbbe72df
Solenkoff <solenkoff@yahoo.com>
2023-04-05 13:51:10 +0200

    Added folder and solutions

---
+++ CSharp_Advanced/L07_Streams_Files_And_Directories_Lab/P01_File_Operations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace _01_File_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Input.txt"))
+            {
+                string currentRow = reader.ReadLine();
+                int row = 0;
+
+                using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+                {
+                    while (currentRow != null)
+                    {
+                        if (row % 2 != 0)
+                        {
+                            writer.WriteLine(currentRow);
+                        }
+
+                        row++;
+                        currentRow = reader.ReadLine();
+
+                    }
+                }
+            }
+
+        }
+    }
+}
