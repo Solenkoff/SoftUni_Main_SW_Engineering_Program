5b449b041cf5e331140c72413e497b713aae9685
Solenkoff <solenkoff@yahoo.com>
2023-04-06 11:04:21 +0200

    Added homework solution

---
+++ CSharp_Advanced/L07_Streams_Files_And_Directories_Lab/P02_Line_Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace _02_Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamReader reader = new StreamReader("../../../Input.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../Output.txt", true))
+                {
+                    string line = reader.ReadLine();
+                    int row = 1;
+
+                    while (line != null)
+                    {                        
+                        writer.WriteLine($"{row}. {line}");
+                        row++;
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+           
+        }
+    }
+}
