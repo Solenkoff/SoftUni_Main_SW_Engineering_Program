6dd2b10f8988f4ceaf7ce9594dbf149c0c96d5a8
Solenkoff <solenkoff@yahoo.com>
2023-04-07 10:18:12 +0200

    Added homework solution

---
+++ CSharp_Advanced/L07_Streams_Files_And_Directories_Lab/P04_Merge_Files/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04_Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            string[] file1 = File.ReadAllLines("*DIRECTORY*\FIRSTFILE.txt");
+            string[] file2 = File.ReadAllLines("*DIRECTORY*\SECONDFILE.txt");
+
+            using (StreamWriter writer = File.CreateText(@"*DIRECTORY*\FINALOUTPUT.txt"))
+            {
+                int lineNum = 0;
+                while (lineNum < file1.Length || lineNum < file2.Length)
+                {
+                    if (lineNum < file1.Length)
+                        writer.WriteLine(file1[lineNum]);
+                    if (lineNum < file2.Length)
+                        writer.WriteLine(file2[lineNum]);
+                    lineNum++;
+                }
+            }
+
+
+        }
+    }
+}
