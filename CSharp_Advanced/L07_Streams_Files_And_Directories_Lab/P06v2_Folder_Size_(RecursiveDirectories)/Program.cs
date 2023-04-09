879939407f69e04fece42bba71fc5382b4a2e4e5
Solenkoff <solenkoff@yahoo.com>
2023-04-09 16:14:17 +0200

    Added homework solution

---
+++ CSharp_Advanced/L07_Streams_Files_And_Directories_Lab/P06v2_Folder_Size_(RecursiveDirectories)/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace _06v2_Folder_Size__RecursiveDirectories_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string folderPath = Console.ReadLine();
+            Console.WriteLine(ScanFolderRecursively(folderPath, 0));
+
+        }
+
+
+        static double ScanFolderRecursively(string folderPath, int identation)
+        {
+           
+            var files = Directory.GetFiles(folderPath);
+
+            double fileSizes = 0;
+
+            var directories = Directory.GetDirectories(folderPath);
+
+            foreach (var directory in directories)
+            {
+                fileSizes += ScanFolderRecursively(directory, identation + 4);
+            }
+
+            foreach (var filePath in files)
+            {
+                FileInfo file = new FileInfo(filePath);
+
+                Console.WriteLine($"{new string(' ', identation)}{file.FullName}");
+              
+                fileSizes += file.Length;
+            }
+      
+            return  fileSizes / 1024.0 / 1024.0;
+        }
+    }
+}
