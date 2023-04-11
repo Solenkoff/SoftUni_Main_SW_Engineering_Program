8a8f8fc65ced84d511aefae101d86a8377bffaf9
Solenkoff <solenkoff@yahoo.com>
2023-04-11 13:29:51 +0200

    Added folder and solutions

---
+++ CSharp_Advanced/L08_Streams_Files_And_Directories_Exercise/P01_Even_Lines/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01_Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine("Data", "Text.txt");
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+
+                string line = reader.ReadLine();
+                int lineCounter = 0;
+
+                while (line != null)
+                {
+                    if (lineCounter % 2 == 0)
+                    {
+                        Regex pattern = new Regex("[-,.!?]");
+                        line = pattern.Replace(line, "@");
+                        var array = line.Split().ToArray().Reverse();
+                        Console.WriteLine(string.Join(" ", array));
+                    }
+                    lineCounter++;
+                    line = reader.ReadLine();
+                }
+
+            }
+
+        }
+    }
+}
