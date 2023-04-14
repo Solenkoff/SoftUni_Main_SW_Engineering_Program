191f89a869beca7b8207b7908061ec45b4769d19
Solenkoff <solenkoff@yahoo.com>
2023-04-14 15:13:59 +0200

    Added homework solution

---
+++ CSharp_Advanced/L08_Streams_Files_And_Directories_Exercise/P06_Zip_And_Extract/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _06_Zip_And_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            ZipFile.CreateFromDirectory(@"D:\01_Source", @"D:\02_Create\MyZipFile.zip");
+            ZipFile.ExtractToDirectory(@"D:\02_Create\MyZipFile.zip", @"D:\03_Extarcted");
+
+        }  
+    }
+}
