6b7c66718f1b5808233ccdf987aba97f71bcc0d0
Solenkoff <solenkoff@yahoo.com>
2023-04-28 12:28:19 +0300

    Added problem solution

---
+++ CSharp_Advanced/L11_Defining_Classes_Lab/P03_Car_Constructors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car bmv = new Car("BMW", "X6", 1993, 5003, -50);
+            Car defaultGolf = new Car();
+            Console.WriteLine($"Default Golf: " + defaultGolf.WhoAmI());
+
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+
+            car.Drive(0.5);
+
+            Car v12 = new Car();
+
+            v12.Make = "v12";
+            v12.Model = "100km/h";
+            v12.Year = 2025;
+            v12.FuelQuantity = 2000000;
+            v12.FuelConsumption = 0;
+            v12.Drive(0.5);
+
+            Console.WriteLine(v12.WhoAmI());
+            Console.WriteLine(car.WhoAmI());
+
+        }
+    }
+}
