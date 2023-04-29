08cb3a0783acbfcb15294bdb25ccad11af71ee01
Solenkoff <solenkoff@yahoo.com>
2023-04-29 15:57:30 +0300

    Added problem solution

---
+++ CSharp_Advanced/L11_Defining_Classes_Lab/P04_Car_Engine_And_Tires/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Engine v12 = new Engine(580, 1200);
+            Tire[] tires = new Tire[]
+            {
+                new Tire(2018, 2.3),
+                new Tire(2018, 2.2),
+                new Tire(2018, 2.5),
+                new Tire(2018, 2.3),
+            };
+
+            Car bmv = new Car("BMW", "X6", 1993, 5003, -50, v12, tires);
+
+            Console.WriteLine("Horse power: " + bmv.Engine.HorsePower);
+
+            foreach (var tire in bmv.Tires)
+            {
+                Console.WriteLine($"{tire.Year} - {tire.Pressure}");
+            }
+
+        }
+
+    }
+}
