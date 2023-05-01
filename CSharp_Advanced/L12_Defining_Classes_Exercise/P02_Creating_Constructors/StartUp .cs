3ce9a82ec5ac3e40e0af7c8addcf8a06041fbd2a
Solenkoff <solenkoff@yahoo.com>
2023-05-01 11:16:01 +0300

    Added problem solution

---
+++ CSharp_Advanced/L12_Defining_Classes_Exercise/P02_Creating_Constructors/StartUp .cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var person = new Person("Pesho", 18);
+
+            Console.WriteLine($"{person.Name} - {person.Age}");
+
+        }
+    }
+}
