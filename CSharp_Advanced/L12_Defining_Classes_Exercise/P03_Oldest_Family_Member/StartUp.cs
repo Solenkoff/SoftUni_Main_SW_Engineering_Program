d59f14d5c35899576f06f5eb6db23b95d7fd10a5
Solenkoff <solenkoff@yahoo.com>
2023-05-01 11:16:23 +0300

    Added problem solution

---
+++ CSharp_Advanced/L12_Defining_Classes_Exercise/P03_Oldest_Family_Member/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personData = Console.ReadLine()
+                                             .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+
+                Person currPerson = new Person(name, age);
+                family.AddMember(currPerson);
+            }
+
+            Person oldestPerson = family.GetOldestMember();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+
+        }
+    }
+}
