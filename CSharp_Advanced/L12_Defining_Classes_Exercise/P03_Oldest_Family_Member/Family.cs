d721141408fb8b6c80c2cce5f01a731261a2347e
Solenkoff <solenkoff@yahoo.com>
2023-05-01 11:16:23 +0300

    Added problem solution

---
+++ CSharp_Advanced/L12_Defining_Classes_Exercise/P03_Oldest_Family_Member/Family.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {      
+        public Family()
+        {
+            this.Members = new List<Person>();
+        }
+
+        public List<Person> Members { get; set; }
+
+        public void AddMember(Person member)
+        {
+            this.Members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldest = new Person();
+            oldest = this.Members.OrderByDescending(x => x.Age).First();
+
+            return oldest;
+        }
+
+    }  
+}
