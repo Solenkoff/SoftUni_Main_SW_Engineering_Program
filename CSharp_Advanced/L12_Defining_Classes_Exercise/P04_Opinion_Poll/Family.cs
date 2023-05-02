673d28309fa19a290d01f09e53b7e90ffeab448c
Solenkoff <solenkoff@yahoo.com>
2023-05-02 11:25:14 +0300

    Added problem solution

---
+++ CSharp_Advanced/L12_Defining_Classes_Exercise/P04_Opinion_Poll/Family.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            this.Members = new List<Person>();
+        }
+
+        public List<Person> Members { get; set; }
+
+        public void AddMember(Person member)
+        {
+            Members.Add(member);
+        }
+     
+    }
+}
