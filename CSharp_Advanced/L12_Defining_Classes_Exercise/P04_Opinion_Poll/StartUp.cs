235f5424101dd117711deb324465de0d13567ea6
Solenkoff <solenkoff@yahoo.com>
2023-05-02 11:25:14 +0300

    Added problem solution

---
+++ CSharp_Advanced/L12_Defining_Classes_Exercise/P04_Opinion_Poll/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> members = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personData = Console.ReadLine()
+                                             .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+
+                Person currPerson = new Person(name, age);
+
+                members.Add(currPerson);
+                
+            }
+          
+            foreach (var person in members.OrderBy(x => x.Name).Where(x => x.Age > 30))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+
+        }
+    }
+}
