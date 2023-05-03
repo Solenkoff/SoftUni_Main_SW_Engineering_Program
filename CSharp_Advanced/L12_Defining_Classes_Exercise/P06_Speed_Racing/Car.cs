7d825ad5213c0f3f52659fd8a9735c4991147e15
Solenkoff <solenkoff@yahoo.com>
2023-05-03 13:30:15 +0300

    Added problem solution

---
+++ CSharp_Advanced/L12_Defining_Classes_Exercise/P06_Speed_Racing/Car.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace _06_Speed_Racing
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double consumption, int travelledDistance)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelPerKm = consumption;
+            this.TravelledDistance = travelledDistance;
+        }
+
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelPerKm { get; set; }
+        public double TravelledDistance { get; set; }
+
+
+        public static bool DistanceCovoringPossibility(Car currCar, double amountOfKm)
+        {
+            bool isPossible = true;
+
+            if (currCar.FuelAmount < currCar.FuelPerKm * amountOfKm)
+            {
+                isPossible = false;
+            }
+
+            return isPossible;
+
+        }
+
+    }
+}
