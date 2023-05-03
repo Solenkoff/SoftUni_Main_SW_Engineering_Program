7b412d056330ec286831041389bc12b9308e998c
Solenkoff <solenkoff@yahoo.com>
2023-05-03 13:30:54 +0300

    Added problem solution

---
+++ CSharp_Advanced/L12_Defining_Classes_Exercise/P06v2_Speed_Racing/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06v2_Speed_Racing
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TravelledDistance { get; set; }
+
+        public bool Drive(double distanceTraveled)
+        {
+            double neededFuel = this.FuelConsumptionPerKilometer * distanceTraveled;
+
+            if(neededFuel > this.FuelAmount)
+            {
+                return false;
+            }
+
+            this.FuelAmount -= neededFuel;
+            this.TravelledDistance += distanceTraveled;
+
+            return true;
+        }
+    }
+}
