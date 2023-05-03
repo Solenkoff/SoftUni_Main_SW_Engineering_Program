0cd06f45c2efe4095b4c44b8034b0fb5a1e0d63b
Solenkoff <solenkoff@yahoo.com>
2023-05-03 13:29:55 +0300

    Added problem solution

---
+++ CSharp_Advanced/L12_Defining_Classes_Exercise/P05_Date_Modifier/DateModifier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05_Date_Modifier
+{
+    public class DateModifier
+    {
+        public string StartDate { get; set; }
+
+        public string EndDate { get; set; }
+
+
+        public static int DaysDifference (string stringDateOne, string stringDateTwo)
+        {
+            int difference = 0;
+
+            DateTime startDate = DateTime.Parse(stringDateOne);
+            DateTime endDate = DateTime.Parse(stringDateTwo);
+
+             //diff = Math.Abs((startDate.Date - endDate.Date).Days);
+
+            TimeSpan diff = startDate - endDate;
+            difference = Math.Abs(diff.Days);
+
+            return difference;
+        }
+
+    }
+}
