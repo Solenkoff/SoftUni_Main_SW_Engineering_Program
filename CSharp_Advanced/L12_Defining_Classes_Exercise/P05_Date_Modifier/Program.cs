e700ed3758d8babb3597c599f69ea5e9a298fb60
Solenkoff <solenkoff@yahoo.com>
2023-05-03 13:29:55 +0300

    Added problem solution

---
+++ CSharp_Advanced/L12_Defining_Classes_Exercise/P05_Date_Modifier/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05_Date_Modifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringOne = Console.ReadLine();
+            string stringTwo = Console.ReadLine();
+
+            int daysDifference = DateModifier.DaysDifference(stringOne, stringTwo);
+
+            Console.WriteLine(daysDifference);
+
+        }
+
+    }
+}
