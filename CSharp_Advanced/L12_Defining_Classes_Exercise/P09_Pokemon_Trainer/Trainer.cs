d34a2ffed4b65a86bc29ae57ebf08d8e283e8c0d
Solenkoff <solenkoff@yahoo.com>
2023-05-04 10:22:41 +0300

    Added problem solution

---
+++ CSharp_Advanced/L12_Defining_Classes_Exercise/P09_Pokemon_Trainer/Trainer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09_Pokemon_Trainer
+{
+    public class Trainer
+    {
+
+        private string name;
+        private int numberOfBadges;
+        private List<Pokemon> pokemons;
+
+        public Trainer(string name)
+        {
+            this.name = name;
+            this.numberOfBadges = 0;
+            this.pokemons = new List<Pokemon>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+        public int NumberOfBadges
+        {
+            get
+            {
+                return this.numberOfBadges;
+            }
+        }
+        public List<Pokemon> Pokemons
+        {
+            get
+            {
+                return this.pokemons;
+            }
+        }
+        public void IncreaceNumberOfBadges()
+        {
+            this.numberOfBadges++;
+        }
+    }
+}
