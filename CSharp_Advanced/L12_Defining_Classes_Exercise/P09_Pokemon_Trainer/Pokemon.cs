2c170002834fda2cd90cbb0edc48492f288a6b83
Solenkoff <solenkoff@yahoo.com>
2023-05-04 10:22:41 +0300

    Added problem solution

---
+++ CSharp_Advanced/L12_Defining_Classes_Exercise/P09_Pokemon_Trainer/Pokemon.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09_Pokemon_Trainer
+{
+    public class Pokemon
+    {
+
+        private string name;
+        private string element;
+        private int health;
+
+        public Pokemon(string name, string element, int health)
+        {
+            this.name = name;
+            this.element = element;
+            this.health = health;
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+        public string Element
+        {
+            get
+            {
+                return this.element;
+            }
+        }
+        public int Health
+        {
+            get
+            {
+                return this.health;
+            }
+        }
+        public void ReduceHealth()
+        {
+            this.health -= 10;
+        }
+    }
+}
