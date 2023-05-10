9184363b5c07ae103bcb18c856482d4690a2fba8
Solenkoff <solenkoff@yahoo.com>
2023-05-10 11:25:54 +0300

    Added Demo

---
+++ CSharp_Advanced/L13_Implementing_Linked_List/LinkedLIst/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LinkedLIst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Node head = new Node(1);
+            //Node node2 = new Node(2);
+            //Node node3 = new Node(3);
+            //Node node4 = new Node(4);
+
+            //head.Next = node2;
+            //node2.Next = node3;
+            //node3.Next = node4;
+
+            //Node currNode = head;
+
+            SoftuniLinkedList linkedList = new SoftuniLinkedList();
+
+            linkedList.AddHead(new Node(1));
+            linkedList.AddHead(new Node(2));
+            linkedList.AddHead(new Node(3));
+            linkedList.AddHead(new Node(4));
+
+            var currNode = linkedList.Head;
+
+
+            
+            while (currNode != null)
+            {
+                Console.WriteLine(currNode);
+                currNode = currNode.Next;
+            }
+        }
+    }
+}
---
+++ CSharp_Advanced/L13_Implementing_Linked_List/LinkedLIst/SoftuniLinkedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedLIst
+{
+    class SoftuniLinkedList
+    {
+
+        public Node Head { get; set; }
+
+        public void AddHead(Node node)
+        {
+            if(Head == null)
+            {
+                Head = node;
+                return;
+            }
+            node.Next = Head;
+            Head = node;
+        }
+    }
+}
