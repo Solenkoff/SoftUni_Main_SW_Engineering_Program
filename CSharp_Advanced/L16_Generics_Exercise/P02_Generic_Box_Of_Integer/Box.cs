284aca36e54eaafe2af2a9193d23564937d82c6c
Solenkoff <solenkoff@yahoo.com>
2023-05-16 11:06:49 +0300

    Added problem solution

---
+++ CSharp_Advanced/L16_Generics_Exercise/P02_Generic_Box_Of_Integer/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_Generic_Box_Of_Integer
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+
+        public override string ToString()
+        {
+            Type valueType = this.Value.GetType();
+            string valueTypeFullname = valueType.FullName;
+
+            return $"{valueTypeFullname}: {this.Value}";
+        }
+
+    }
+}
