fbbc075eaee1c55722fea17c6ec2130ce3cb1022
Solenkoff <solenkoff@yahoo.com>
2023-05-18 10:25:44 +0300

    Added problem solution

---
+++ CSharp_Advanced/L16_Generics_Exercise/P04_Generic_Swap_Method_Integer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_Generic_Swap_Method_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Box<int>> boxes = new List<Box<int>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<int> currBox = new Box<int>(int.Parse(Console.ReadLine()));
+                boxes.Add(currBox);
+            }
+
+            int[] indexes = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            int indexOne = indexes[0];
+            int indexTwo = indexes[1];
+
+            SwapBoxes(boxes, indexOne, indexTwo);
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+
+
+        private static void SwapBoxes<T>(List<Box<T>> boxes, int indexOne, int indexTwo)
+        {
+            Box<T> tempBox = boxes[indexOne];
+            boxes[indexOne] = boxes[indexTwo];
+            boxes[indexTwo] = tempBox;
+        }
+
+    }
+}
