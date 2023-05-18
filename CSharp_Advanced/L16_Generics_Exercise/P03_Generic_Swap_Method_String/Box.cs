4f46563237a616765b5cc3a530ec5e78072780e2
Solenkoff <solenkoff@yahoo.com>
2023-05-18 10:25:25 +0300

    Added problem solution

---
+++ CSharp_Advanced/L16_Generics_Exercise/P03_Generic_Swap_Method_String/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_Generic_Swap_Method_String
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+
+        public override string ToString()
+        {
+            Type valueType = this.Value.GetType();
+            string valueTypeFullname = valueType.FullName;
+
+            return $"{valueTypeFullname}: {this.Value}";
+        }
+
+    }
+}
---
+++ CSharp_Advanced/L16_Generics_Exercise/P03_Generic_Swap_Method_String/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_Generic_Swap_Method_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Box<string>> boxes = new List<Box<string>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> currBox = new Box<string>(Console.ReadLine());
+                boxes.Add(currBox);
+            }
+
+            int[] indexes = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            int indexOne = indexes[0];
+            int indexTwo = indexes[1];
+
+            SwapBoxes(boxes, indexOne, indexTwo);
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+
+
+        private static void SwapBoxes<T>(List<Box<T>> boxes, int indexOne, int indexTwo)
+        {
+            Box<T> tempBox = boxes[indexOne];
+            boxes[indexOne] = boxes[indexTwo];
+            boxes[indexTwo] = tempBox;
+        }
+        
+    }
+}
