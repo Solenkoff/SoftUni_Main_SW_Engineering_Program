f2168a9fc1b1270f34d69967baedcdf8bcd907d5
Solenkoff <solenkoff@yahoo.com>
2023-05-19 13:38:14 +0300

    Added problem solution

---
+++ CSharp_Advanced/L16_Generics_Exercise/P05_Generic_Count_Method_String/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05_Generic_Count_Method_String
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+
+       
+        public override string ToString()
+        {
+            Type valueType = this.Value.GetType();
+            string valueTypeFullname = valueType.FullName;
+
+            return $"{valueTypeFullname}: {this.Value}";
+        }
+
+    }
+}
---
+++ CSharp_Advanced/L16_Generics_Exercise/P05_Generic_Count_Method_String/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05_Generic_Count_Method_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<Box<string>> boxes = new List<Box<string>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> currBox = new Box<string>(Console.ReadLine());
+                boxes.Add(currBox);
+            }
+
+
+            string value = Console.ReadLine();
+            Box<string> boxToCompareTo = new Box<string>(value);
+
+            int count = GetCountOfGreaterElements(boxes, boxToCompareTo);
+
+            Console.WriteLine(count);
+        }
+
+
+        private static int GetCountOfGreaterElements<T>(List<Box<T>> boxes, Box<T> comparator)
+           where T : IComparable
+        {
+            int count = 0;
+            foreach (Box<T> box in boxes)
+            {
+                if (box.Value.CompareTo(comparator.Value) > 0)
+                {
+                    count++;
+                }
+
+            }
+
+            return count;
+        }
+    }
+}
