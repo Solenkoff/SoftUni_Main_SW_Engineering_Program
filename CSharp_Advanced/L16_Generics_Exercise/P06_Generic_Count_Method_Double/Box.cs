bb68c144f1a3198ce3481e52ac78dea7c1d19231
Solenkoff <solenkoff@yahoo.com>
2023-05-20 11:29:09 +0300

    Added problem solution

---
+++ CSharp_Advanced/L16_Generics_Exercise/P06_Generic_Count_Method_Double/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_Generic_Count_Method_Double
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+
+
+        public override string ToString()
+        {
+            Type valueType = this.Value.GetType();
+            string valueTypeFullname = valueType.FullName;
+
+            return $"{valueTypeFullname}: {this.Value}";
+        }
+    }
+}
---
+++ CSharp_Advanced/L16_Generics_Exercise/P06_Generic_Count_Method_Double/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_Generic_Count_Method_Double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Box<double>> boxes = new List<Box<double>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<double> currBox = new Box<double>(double.Parse(Console.ReadLine()));
+                boxes.Add(currBox);
+            }
+
+
+            double value = double.Parse(Console.ReadLine());
+            Box<double> boxToCompareTo = new Box<double>(value);
+
+            int count = GetCountOfGreaterElements(boxes, boxToCompareTo);
+
+            Console.WriteLine(count);
+
+        }
+
+
+        private static int GetCountOfGreaterElements<T>(List<Box<T>> boxes, Box<T> comparator)
+          where T : IComparable
+        {
+            int count = 0;
+            foreach (Box<T> box in boxes)
+            {
+                if (box.Value.CompareTo(comparator.Value) > 0)
+                {
+                    count++;
+                }
+
+            }
+
+            return count;
+        }
+
+    }
+}
