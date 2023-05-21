98295efb98a83d9d4943bcadaa1493d060f3d660
Solenkoff <solenkoff@yahoo.com>
2023-05-21 11:41:15 +0300

    Added folder and solutions

---
+++ CSharp_Advanced/L10_Functional_Programming_Exercise/P01_Action_Print/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string[]> printNames = name => Console.WriteLine(string.Join(Environment.NewLine, name));
+
+            printNames(names);
+
+        }
+    }
+}
