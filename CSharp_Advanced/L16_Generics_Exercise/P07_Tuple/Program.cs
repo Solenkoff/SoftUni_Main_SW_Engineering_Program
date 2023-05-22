627db5edfa7ad1856d9dfc023bb492f3cb6d6454
Solenkoff <solenkoff@yahoo.com>
2023-05-22 15:25:40 +0300

    Added problem solution

---
+++ CSharp_Advanced/L16_Generics_Exercise/P07_Tuple/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _07_Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] firstTupleData = Console.ReadLine()
+                                             .Split(" ");
+            string fullName = $"{firstTupleData[0]} {firstTupleData[1]}";
+            string address = firstTupleData[2];
+
+            MyTuple<string, string> firstTuple =
+                     new MyTuple<string, string>(fullName, address);
+
+
+            string[] secondTupleData = Console.ReadLine()
+                                              .Split(" ");
+            string name = secondTupleData[0];
+            int amountOfBeer = int.Parse(secondTupleData[1]);
+
+            MyTuple<string, int> secondTuple =
+                     new MyTuple<string, int>(name, amountOfBeer);
+
+
+            string[] thirdTupleData = Console.ReadLine()
+                                             .Split(" ");
+            int integerNum = int.Parse(thirdTupleData[0]);
+            double doubleNum = double.Parse(thirdTupleData[1]);
+
+            MyTuple<int, double> thirdTuple =
+                     new MyTuple<int, double>(integerNum, doubleNum);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+
+        }
+    }
+}
