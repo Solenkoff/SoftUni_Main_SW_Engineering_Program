c150e8f7d31e71e8729a4e7ea160a47415ef7a99
Solenkoff <solenkoff@yahoo.com>
2023-05-22 14:42:23 +0300

    Added problem solution

---
+++ CSharp_Advanced/L10_Functional_Programming_Exercise/P02_Knights_Of_Honor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Knights_Of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            List<string> newNames = names.Select(name => $"Sir {name}").ToList();
+
+            Action<List<string>> printNames = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            printNames(newNames);
+
+        }
+    }
+}
