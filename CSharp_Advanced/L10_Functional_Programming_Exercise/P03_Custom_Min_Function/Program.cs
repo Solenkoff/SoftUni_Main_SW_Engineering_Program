560477043059ba3405dc9efbb3ad717ff03590bf
Solenkoff <solenkoff@yahoo.com>
2023-05-22 14:42:42 +0300

    Added problem solution

---
+++ CSharp_Advanced/L10_Functional_Programming_Exercise/P03_Custom_Min_Function/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] integers = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(n => int.Parse(n))
+                                    .ToArray();
+
+            Func<int[], int> smallest = numbers =>
+            {
+                int minNum = int.MaxValue;
+
+                foreach (var num in numbers)
+                {
+                    if (num < minNum)
+                    {
+                        minNum = num;
+                    }
+                }
+                return minNum;
+            };
+
+            int minNumber = smallest(integers);
+            Console.WriteLine(minNumber);
+
+        }
+    }
+}
