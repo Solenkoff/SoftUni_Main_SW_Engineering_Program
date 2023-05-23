01f559ac1a7246003268fd3e9a8b572b4ab7bd35
Solenkoff <solenkoff@yahoo.com>
2023-05-23 11:17:04 +0300

    Added problem solution

---
+++ CSharp_Advanced/L16_Generics_Exercise/P09_Custom_Generic_Linked_List/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09_Custom_Generic_Linked_List
+{
+    public class Node<T>
+    {
+        public Node(int value)
+        {
+            Value = value;
+        }
+        public int Value { get; set; }
+
+        public Node Next { get; set; }
+
+    }
+}
---
+++ CSharp_Advanced/L16_Generics_Exercise/P09_Custom_Generic_Linked_List/SoftuniLinkedList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09_Custom_Generic_Linked_List
+{
+    public class SoftuniLinkedList<T>
+    {
+        public Node Head { get; set; }
+
+        public void AddHead(Node node)
+        {
+            if (Head == null)
+            {
+                Head = node;
+                return;
+            }
+            node.Next = Head;
+            Head = node;
+        }
+    }
+}
