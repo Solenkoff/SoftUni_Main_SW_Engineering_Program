d4588c923d6d9f40f91afa774a96c1bceafb5552
Solenkoff <solenkoff@yahoo.com>
2023-05-23 11:17:04 +0300

    Added problem solution

---
+++ CSharp_Advanced/L16_Generics_Exercise/P09_Custom_Generic_Linked_List/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _09_Custom_Generic_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoftuniLinkedList linkedList = new SoftuniLinkedList();
+
+            linkedList.AddHead(new Node(1));
+            linkedList.AddHead(new Node(2));
+            linkedList.AddHead(new Node(3));
+            linkedList.AddHead(new Node(4));
+
+            var currNode = linkedList.Head;
+
+
+
+            while (currNode != null)
+            {
+                Console.WriteLine(currNode);
+                currNode = currNode.Next;
+            }
+        }
+    }
+}
