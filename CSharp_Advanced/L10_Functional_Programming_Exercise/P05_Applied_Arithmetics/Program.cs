65ab92c0eb2c81638d13eff2b1c7a5e634c2d70f
Solenkoff <solenkoff@yahoo.com>
2023-05-24 11:37:43 +0300

    Added problem solution

---
+++ CSharp_Advanced/L10_Functional_Programming_Exercise/P05_Applied_Arithmetics/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = Console.ReadLine()
+                                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(int.Parse)
+                                       .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                Func<int, int> arithmetics = num => num;
+                Action<List<int>> print = nums => Console.WriteLine(string.Join(" ", nums));
+
+                if (input == "add")
+                {
+                    arithmetics = num => num + 1;
+                    numbers = numbers.Select(n => arithmetics(n)).ToList();
+                }
+                else if (input == "multiply")
+                {
+                    arithmetics = num => num * 2;
+                    numbers = numbers.Select(n => arithmetics(n)).ToList();
+                }
+                else if (input == "subtract")
+                {
+                    arithmetics = num => num - 1;
+                    numbers = numbers.Select(n => arithmetics(n)).ToList();
+                }
+                else if (input == "print")
+                {
+                    print(numbers);
+                }
+
+                input = Console.ReadLine();
+
+            }
+
+        }
+    }
+}
