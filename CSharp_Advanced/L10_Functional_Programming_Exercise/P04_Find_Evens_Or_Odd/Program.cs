73210fd39813c4d768f499a1bfdd2b401fabb11c
Solenkoff <solenkoff@yahoo.com>
2023-05-24 11:37:18 +0300

    Added problem solution

---
+++ CSharp_Advanced/L10_Functional_Programming_Exercise/P04_Find_Evens_Or_Odd/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_Find_Evens_Or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] bounds = Console.ReadLine()
+                                  .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            int lowBound = bounds.Min();
+            int highBound = bounds.Max();
+
+            string criteria = Console.ReadLine();
+
+            Func<int, int, List<int>> selection = (s, e) =>
+            {
+                List<int> nums = new List<int>();
+
+                for (int i = s; i <= e; i++)
+                {
+                    nums.Add(i);
+                }
+
+                return nums;
+
+            };
+
+            List<int> numbers = selection(lowBound, highBound) ;
+
+            Predicate<int> predicate = n => true;
+
+            if (criteria == "odd")
+            {
+                predicate = n => n % 2 != 0;
+            }
+            else if (criteria == "even")
+            {
+                predicate = n => n % 2 == 0;
+            }
+
+            Console.WriteLine(string.Join(" ", MyWhere(numbers, predicate)));
+
+        }
+
+
+        static List<int> MyWhere(List<int> numbers, Predicate<int> predicate)
+        {
+            List<int> newNumbers = new List<int>();
+
+            foreach (var num in numbers)
+            {
+                if (predicate(num))
+                {
+                    newNumbers.Add(num);
+                }
+            }
+
+            return newNumbers;
+
+        }
+    }
+}
