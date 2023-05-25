f914ab3ec36847ec2925024f5d4ca468c7840cb2
Solenkoff <solenkoff@yahoo.com>
2023-05-25 10:27:44 +0300

    Added problem solution

---
+++ CSharp_Advanced/L10_Functional_Programming_Exercise/P06_Reverse_And_Exclude/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06_Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = Console.ReadLine()
+                                      .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(int.Parse)
+                                      .Reverse()
+                                      .ToList();
+
+            int divisor = int.Parse(Console.ReadLine());
+
+            List<int> newList = new List<int>();
+
+            Func<List<int>, List<int>> divisibles = nums =>
+            {            
+                foreach (var num in nums)
+                {
+                    if(num % divisor != 0)
+                    {
+                        newList.Add(num);
+                    }
+                }
+                return newList;
+            };
+
+            numbers = divisibles(numbers);
+
+            Action<List<int>> print = list => Console.WriteLine(string.Join(" ", list));
+
+            print(numbers);
+     
+        }
+    }
+}
