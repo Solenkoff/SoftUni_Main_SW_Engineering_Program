2fcbdc97b136a16582d02b60b8ced1c1a4b5214a
Solenkoff <solenkoff@yahoo.com>
2023-05-26 10:42:17 +0300

    Added problem solution

---
+++ CSharp_Advanced/L10_Functional_Programming_Exercise/P07_Predicate_For_Names/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07_Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                                          .Split(" ", StringSplitOptions.RemoveEmptyEntries)                                    
+                                          .ToList();
+
+           // names = names.Where(x => x.Length <= n).ToList();
+           //  Or instead -> next 2 lines
+
+            Func<string, int, bool> func = (name, length) => name.Length <= length;
+            names = names.Where(name => func(name, n)).ToList();
+
+            Action<List<string>> print = list => Console.WriteLine(string.Join(Environment.NewLine, list));
+
+            print(names);
+
+        }
+    }
+}
