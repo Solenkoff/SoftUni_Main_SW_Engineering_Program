2e4c445f96ddffa064b726d2cb82f51f9a01af57
Solenkoff <solenkoff@yahoo.com>
2023-05-28 10:29:40 +0300

    Added problem solution

---
+++ CSharp_Advanced/L17_Iterators_And_Comparators_Lab/P02_LibraryIterator/Library.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library<Book> : IEnumerable<Book>
+    {
+
+        private readonly List<Book> books;
+
+        public Library(IEnumerable<Book> books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public Library()
+        {
+            books = new List<Book>();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIteraror(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+             
+    }
+}
---
+++ CSharp_Advanced/L17_Iterators_And_Comparators_Lab/P02_LibraryIterator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IteratorsAndComparators
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
