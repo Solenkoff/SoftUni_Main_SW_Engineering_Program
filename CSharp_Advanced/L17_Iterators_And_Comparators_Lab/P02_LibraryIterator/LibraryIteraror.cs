7fecf18a2596ecebc8cce8167372940ca895f733
Solenkoff <solenkoff@yahoo.com>
2023-05-28 10:29:40 +0300

    Added problem solution

---
+++ CSharp_Advanced/L17_Iterators_And_Comparators_Lab/P02_LibraryIterator/LibraryIteraror.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class LibraryIteraror : IEnumerator<Book>
+    {
+        private List<Book> books;
+
+        private int currentIndex = -1;
+
+        public LibraryIteraror(List<Book> books)
+        {
+            this.books = books;
+        }
+
+        
+        public Book Current => books[currentIndex];
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            currentIndex++;
+
+            return currentIndex < books.Count;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+    }
+}
