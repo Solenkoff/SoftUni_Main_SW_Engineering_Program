36238abe3fb74e7e53243ba1b797653e07d2e87f
Solenkoff <solenkoff@yahoo.com>
2023-06-06 18:11:01 +0300

    Added problem solution

---
+++ CSharp_Advanced/L19_Exam_Preparation/CSharp_Advanced_Exam_Preparation_SN/P01_Meal_Plan_2022-04-13/StartUp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_Meal_Plan
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> mealTypes = new Dictionary<string, int> {
+                {"salad", 350 },
+                {"soup", 490 },
+                {"pasta", 680 },
+                {"steak", 790 }
+            };
+
+
+            string[] mealsInput = Console.ReadLine()
+                                         .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int[] caloriesInput = Console.ReadLine()
+                                         .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                         .Select(int.Parse)
+                                         .ToArray();
+
+            Queue<string> meals = new Queue<string>(mealsInput);
+            Stack<int> calories = new Stack<int>(caloriesInput);
+
+            
+            int mealsEaten = 0;
+
+            while (meals.Count > 0 && calories.Count > 0)
+            {
+
+                string currMeal = meals.Dequeue();
+                int mealCalories = mealTypes[currMeal];
+
+                int currDailyCalories = calories.Pop();
+
+                mealsEaten++;
+
+                if (currDailyCalories - mealCalories > 0)
+                {
+                    calories.Push(currDailyCalories - mealCalories);               
+                }
+                else
+                {
+                    int mealCaloriesLeft = Math.Abs(currDailyCalories - mealCalories);
+                    if(calories.Count == 0)
+                    {
+                        break;
+                    }
+
+                    calories.Push(calories.Pop() - mealCaloriesLeft);
+
+                }
+
+            }
+
+            if(meals.Count == 0)
+            {
+                Console.WriteLine($"John had {mealsEaten} meals.");
+                Console.WriteLine($"For the next few days, he can eat {string.Join(", ", calories)} calories.");
+            }
+            else if(calories.Count == 0)
+            {
+                Console.WriteLine($"John ate enough, he had {mealsEaten} meals.");
+                Console.WriteLine($"Meals left: {string.Join(", ", meals)}.");
+            }
+            
+        }
+    }
+}
