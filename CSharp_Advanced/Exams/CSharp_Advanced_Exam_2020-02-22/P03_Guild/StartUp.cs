86ed834aff22cad59f246d1bf0a0b9c1798d8dc0
Solenkoff <solenkoff@yahoo.com>
2023-06-09 10:26:18 +0300

    Added problem solution

---
+++ CSharp_Advanced/Exams/CSharp_Advanced_Exam_2020-02-22/P03_Guild/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Guild
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            Player newP = new Player("Gogo", "Bau");
+            Player newP2 = new Player("Goro", "Don");
+            newP.Class = "Bro";
+
+            System.Console.WriteLine(newP);
+
+            Guild newG = new Guild("NewG", 3);
+
+            newG.AddPlayer(newP);
+            newG.AddPlayer(newP2);
+
+            //newG.RemovePlayer("Goro");
+            newG.KickPlayersByClass("Bro");
+
+           Console.WriteLine(newG.Report());
+            
+
+        }
+    }
+}
