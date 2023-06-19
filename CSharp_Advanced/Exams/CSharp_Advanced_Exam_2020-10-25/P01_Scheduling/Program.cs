531671d7d9d4e6fe14f09937607458842461ceda
Solenkoff <solenkoff@yahoo.com>
2023-06-19 11:38:53 +0300

    Added folder and solutions

---
+++ CSharp_Advanced/Exams/CSharp_Advanced_Exam_2020-10-25/P01_Scheduling/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] tasksData = Console.ReadLine()
+                                     .Split(", ")
+                                     .Select(int.Parse)
+                                     .ToArray();
+            Stack<int> tasks = new Stack<int>(tasksData);
+           
+            int[] threadsData = Console.ReadLine()
+                                     .Split(" ")
+                                     .Select(int.Parse)
+                                     .ToArray();
+
+            Queue<int> threads = new Queue<int>(threadsData);
+
+            int taskToKill = int.Parse(Console.ReadLine());
+
+
+            while (true)
+            {
+                int currThread = threads.Peek();
+                int currTask = tasks.Peek();
+
+                if (currTask == taskToKill)
+                {
+                    Console.WriteLine($"Thread with value {currThread} killed task {currTask}");
+                    break;
+                }
+                else
+                {
+                    if(currThread >= currTask)
+                    {                       
+                        tasks.Pop();
+                    }
+
+                    threads.Dequeue();
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", threads));
+         
+        }
+    }
+}
