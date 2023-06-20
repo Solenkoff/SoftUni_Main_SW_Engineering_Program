5e2c67eb20b1c97f0bd7119834a0b2e695992bad
Solenkoff <solenkoff@yahoo.com>
2023-06-20 11:20:11 +0300

    Added problem solution

---
+++ CSharp_Advanced/Exams/CSharp_Advanced_Exam_2020-10-25/P03_Classroom/Classroom.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+        public Classroom(int capacity)
+        {
+            this.Capacity = capacity;
+            
+            this.students = new List<Student>();
+        }
+
+        public int Capacity { get; set; }
+        public int Count { get { return students.Count; }  }
+
+
+        public string RegisterStudent(Student student)
+        {
+            if (this.Count < this.Capacity)
+            {
+                students.Add(student);
+               
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+            if (student == null)
+            {
+                return "Student not found";
+
+            }
+            else
+            {
+                this.students.Remove(student);
+                
+                return $"Dismissed student {firstName} {lastName}";
+            }
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+
+            List<Student> sameObjectStudents = students.FindAll(x => x.Subject == subject);
+            
+            if (sameObjectStudents.Count > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine("Students:");
+
+                foreach (var student in sameObjectStudents)
+                {
+                    sb.AppendLine($"{student.FirstName} {student.LastName}");
+                }
+                return sb.ToString();
+            }
+            else
+            {
+                return "No students enrolled for the subject";
+            }
+        }
+
+
+        public int GetStudentsCount()
+        {
+            return this.Count;
+        }
+
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+            return student;
+        }
+    }
+}
---
+++ CSharp_Advanced/Exams/CSharp_Advanced_Exam_2020-10-25/P03_Classroom/StartUp.cs
@@ -0,0 +1,69 @@
+﻿namespace ClassroomProject
+{
+    using System;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // Initialize the repository
+            Classroom classroom = new Classroom(10);
+            // Initialize entities
+            Student student = new Student("Peter", "Parker", "Geometry");
+            Student studentTwo = new Student("Sarah", "Smith", "Algebra");
+            Student studentThree = new Student("Sam", "Winchester", "Algebra");
+            Student studentFour = new Student("Dean", "Winchester", "Music");
+            // Print Student
+            Console.WriteLine(student); // Student: First Name = Peter, Last Name = Parker, Subject = Geometry
+                                        // Register Student
+            string register = classroom.RegisterStudent(student);
+            Console.WriteLine(register); // Added student Peter Parker
+            string registerTwo = classroom.RegisterStudent(studentTwo);
+            string registerThree = classroom.RegisterStudent(studentThree);
+            string registerFour = classroom.RegisterStudent(studentFour);
+            // Dismiss Student
+            string dismissed = classroom.DismissStudent("Peter", "Parker");
+            Console.WriteLine(dismissed); // Dismissed student Peter Parker
+            string dismissedTwo = classroom.DismissStudent("Ellie", "Goulding");
+            Console.WriteLine(dismissedTwo); // Student not found
+                                             // Subject info
+            string subjectInfo = classroom.GetSubjectInfo("Algebra");
+            Console.WriteLine(subjectInfo);
+            // Subject: Algebra
+            // Students:
+            // Sarah Smith
+            // Sam Winchester
+            string anotherInfo = classroom.GetSubjectInfo("Art");
+            Console.WriteLine(anotherInfo); // No students enrolled for the subject
+                                            // Get Student
+            Console.WriteLine(classroom.GetStudent("Dean", "Winchester"));
+            // Student: First Name = Dean, Last Name = Winchester, Subject = Music
+
+
+
+            //Classroom classroom = new Classroom(3);
+
+            //Student student1 = new Student("Gogo", "Nachev", "history");
+            //Student student2 = new Student("Pesho", "Peshov", "history");
+            //Student student3 = new Student("Kunio", "Lilov", "math");
+            //Student student4 = new Student("Vili", "Dikov", "history");
+
+
+
+            //Console.WriteLine(student1);
+
+            //classroom.RegisterStudent(student1);
+            //classroom.RegisterStudent(student2);
+            //classroom.RegisterStudent(student3);
+            //classroom.RegisterStudent(student4);
+
+
+            //Console.WriteLine(classroom.GetStudent("Gogo", "Nachev"));
+
+            //Console.WriteLine(classroom.GetSubjectInfo("bobo"));
+
+            //string dismissed = classroom.DismissStudent("Gogo", "Nachev");
+            //Console.WriteLine(dismissed);
+            //Console.WriteLine(classroom.GetStudentsCount());
+        }
+    }
+}
