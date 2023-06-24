22cbeed397a5e730a762811f89f90f3580d59c4c
Solenkoff <solenkoff@yahoo.com>
2023-06-24 14:36:30 +0300

    Added problem solution

---
+++ CSharp_Advanced/Exams/CSharp_Advanced_Exam_2020-12-16_Retake/P03_Bakery_Openning/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace BakeryOpenning
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            Employee nov = new Employee("Gogo", 20, "Bulgaria");
+            Employee nov2 = new Employee("Bobo", 10, "Gana");
+
+            Bakery bake = new Bakery("bake", 3);
+
+            bake.Add(nov);
+            bake.Add(nov2);
+
+            bake.Remove(nov2.Name);
+
+            Console.WriteLine(nov);
+
+            Console.WriteLine(bake.Report());
+
+        }
+    }
+}
