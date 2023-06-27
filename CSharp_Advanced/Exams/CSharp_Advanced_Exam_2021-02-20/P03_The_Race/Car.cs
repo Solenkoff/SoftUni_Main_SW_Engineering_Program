c1d1f94adcdbe7a4c6648ba51361168fe3a4fd70
Solenkoff <solenkoff@yahoo.com>
2023-06-27 10:23:34 +0300

    Added problem solution

---
+++ CSharp_Advanced/Exams/CSharp_Advanced_Exam_2021-02-20/P03_The_Race/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_The_Race
+{
+    public class Car
+    {
+        public Car(string name, int speed)
+        {
+            this.Name = name;
+            this.Speed = speed;
+        }
+
+        public string Name { get; set; }
+
+        public int Speed { get; set; }
+
+    }
+}
