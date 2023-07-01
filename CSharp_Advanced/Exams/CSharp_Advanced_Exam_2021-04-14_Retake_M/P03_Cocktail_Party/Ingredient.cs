e37d412a418429eb54252a39e468d4f4f1b2328f
Solenkoff <solenkoff@yahoo.com>
2023-07-01 10:57:39 +0300

    Added problem solution

---
+++ CSharp_Advanced/Exams/CSharp_Advanced_Exam_2021-04-14_Retake_M/P03_Cocktail_Party/Ingredient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Ingredient
+    {
+        public Ingredient(string name, int alcohol, int quantity)
+        {
+            Name = name;
+            Alcohol = alcohol;
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+        public int Alcohol { get; set; }
+        public int Quantity { get; set; }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Ingredient: {this.Name}");
+            sb.AppendLine($"Quantity: {this.Quantity}");
+            sb.AppendLine($"Alcohol: {this.Alcohol}");
+
+            return sb.ToString().Trim();
+        }
+       
+    }     
+        
+}
+
+              
+                     
+
+
