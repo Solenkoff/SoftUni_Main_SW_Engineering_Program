d15d1adee0218af3ed49ba869e15363a58743655
Solenkoff <solenkoff@yahoo.com>
2023-07-08 14:35:23 +0300

    Added folder and solutions

---
+++ CSharp_Advanced/Exams/CSharp_Advanced_Exam_2021-08-18_Retake/P01_Birthday_Celebration/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_Birthday_Celebration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] inputGuests = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] inputPlates = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            Stack<int> guests = new Stack<int>(inputGuests.Reverse());
+            Stack<int> plates = new Stack<int>(inputPlates);
+
+
+
+            int wastedFood = 0;
+
+            while (guests.Count > 0 && plates.Count > 0)
+            {
+
+                int eating = guests.Pop();
+                int food = plates.Pop();
+
+
+                if (food >= eating)
+                {
+                    wastedFood += (food - eating);
+
+                }
+                else
+                {
+                    guests.Push(eating - food);
+                }
+
+
+
+
+
+            }
+
+            if (guests.Count > 0)
+            {
+                Console.WriteLine($"Guests: " + string.Join(" ", guests));
+            }
+            else if (plates.Count > 0)
+            {
+                Console.WriteLine($"Plates: " + string.Join(" ", plates));
+            }
+
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
+        }
+    }
+}
