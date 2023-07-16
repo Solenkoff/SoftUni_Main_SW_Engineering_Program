383c6eb5d4aaa66e16f678d7b2a790508a4cb2e7
Solenkoff <solenkoff@yahoo.com>
2023-07-16 11:12:42 +0300

    Added problem solution

---
+++ CSharp_OOP/L01_Inheritance_Lab/P05_Random_List_Of_T/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            RandomList<string> list = new RandomList<string>();
+
+            list.Add("Pesho");
+            list.Add("Gogi");
+            list.Add("Gigi");
+            list.Add("Mishi");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(list.RandomString());
+            }
+
+        }
+    }
+}
