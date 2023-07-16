140b5ccf085e04aaae139a850190e86a56429d2d
Solenkoff <solenkoff@yahoo.com>
2023-07-16 11:12:42 +0300

    Added problem solution

---
+++ CSharp_OOP/L01_Inheritance_Lab/P05_Random_List_Of_T/RandomList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList<T> : List<T>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+
+        public void Add(T el)
+        {
+            base.Add(el);
+            Console.WriteLine($"Addded the string {el} and we have costum functinalities");
+        }
+
+        public T RandomString()
+        {
+            if(Count == 0)
+            {
+                return default;
+            }
+
+            return this[random.Next(0, this.Count)];
+        }
+
+    }
+}
