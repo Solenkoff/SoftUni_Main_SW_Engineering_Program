50f4abb50127173d039eed5ffcfb5784b8eebcbf
Solenkoff <solenkoff@yahoo.com>
2023-07-17 10:25:43 +0300

    Added problem solution

---
+++ CSharp_OOP/L01_Inheritance_Lab/P06_Stack_Of_Strings/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            StackOfStrings stack = new StackOfStrings();
+
+            Console.WriteLine(stack.IsEmpty());
+
+            stack.Push("Edno");
+            stack.Push("Dve");
+            stack.Push("Tri");
+            stack.Push("Chetiri");
+            stack.Push("Pet");
+
+            stack.AddRange(new List<string>()
+            { 
+                "Shest",
+                "Sedem",
+                "Osem"
+            
+            });
+                
+            Console.WriteLine(stack.IsEmpty());
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
