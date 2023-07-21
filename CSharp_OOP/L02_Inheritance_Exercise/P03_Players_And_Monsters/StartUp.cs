936292f481be2cdde45a2529538b65140d5a2c40
Solenkoff <solenkoff@yahoo.com>
2023-07-21 11:20:28 +0300

    Added problem solution

---
+++ CSharp_OOP/L02_Inheritance_Exercise/P03_Players_And_Monsters/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Hero hero = new Hero("Gogi", 19);
+
+            Console.WriteLine(hero);
+
+            BladeKnight knight = new BladeKnight("Bobo", 83);
+
+            Console.WriteLine(knight);
+
+        }
+    }
+}
