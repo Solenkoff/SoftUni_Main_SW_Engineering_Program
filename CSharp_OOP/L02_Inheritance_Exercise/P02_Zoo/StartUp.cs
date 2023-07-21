19b9c550beba0b312f6d8b795d55d96bfbb10a02
Solenkoff <solenkoff@yahoo.com>
2023-07-21 11:20:00 +0300

    Added problem solution

---
+++ CSharp_OOP/L02_Inheritance_Exercise/P02_Zoo/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            Snake snake = new Snake(name);
+
+            Console.WriteLine(snake);
+
+        }
+    }
+}
