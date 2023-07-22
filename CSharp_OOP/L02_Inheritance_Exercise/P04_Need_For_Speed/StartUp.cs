dc67b90aae582513dcd4f81163edcb53b0b4bd30
Solenkoff <solenkoff@yahoo.com>
2023-07-22 10:30:18 +0300

    Added problem solution

---
+++ CSharp_OOP/L02_Inheritance_Exercise/P04_Need_For_Speed/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Car car = new Car(333, 50);
+
+            car.Drive(15);
+
+
+            Console.WriteLine(car);
+        }
+    }
+}
