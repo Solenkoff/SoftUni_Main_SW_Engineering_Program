6c24b002097f680a51c3691275de90087e3f174a
Solenkoff <solenkoff@yahoo.com>
2023-07-22 10:30:44 +0300

    Added problem solution

---
+++ CSharp_OOP/L02_Inheritance_Exercise/P05_Restaurant/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Cake cake = new Cake("Domashna");
+
+            Console.WriteLine(cake);
+
+        }
+    }
+}
