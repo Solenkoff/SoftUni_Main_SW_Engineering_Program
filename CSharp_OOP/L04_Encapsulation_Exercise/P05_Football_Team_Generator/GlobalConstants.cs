23b2af6b3784f461ee0c810fe87c55f98e228303
Solenkoff <solenkoff@yahoo.com>
2023-07-27 11:07:25 +0300

    Added problem solution

---
+++ CSharp_OOP/L04_Encapsulation_Exercise/P05_Football_Team_Generator/GlobalConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootbalTeamGenerator
+{
+    public class GlobalConstants
+    {
+
+        public const string InvalidNameErrorMessage = "A name should not be empty.";
+    }
+}
---
+++ CSharp_OOP/L04_Encapsulation_Exercise/P05_Football_Team_Generator/Validator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootbalTeamGenerator
+{
+    public static class Validator
+    {
+        public static void ThrowIfStringIsNullOrWhiteSpace(string str, string exceptionMessage)
+        {
+            if(String.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentException(exceptionMessage);
+            }
+        }
+
+
+        public static void ThrowIfNumberIsNotInRange(int number, int min, int max, string exceptionMessage)
+        {
+            if(number < min || number > max)
+            {
+                throw new ArgumentException(exceptionMessage);
+            }
+        }
+    }
+}
