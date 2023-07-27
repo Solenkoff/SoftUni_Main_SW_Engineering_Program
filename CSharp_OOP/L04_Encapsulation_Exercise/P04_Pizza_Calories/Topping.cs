018b0baf32d86b7098700dccbf0b3e2587d00004
Solenkoff <solenkoff@yahoo.com>
2023-07-27 11:07:04 +0300

    Added problem solution

---
+++ CSharp_OOP/L04_Encapsulation_Exercise/P04_Pizza_Calories/Topping.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Topping
+    {
+        private const int MinWeight = 1;
+        private const int MaxWeight = 50;
+
+        private string name;
+        private int weight;
+
+
+        public Topping(string name, int weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                var valueToLower = value.ToLower();
+                if (valueToLower != "meat" &&
+                   valueToLower != "veggies" &&
+                   valueToLower != "cheese" &&
+                   valueToLower != "sauce")
+                {
+                    throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+                }
+
+                this.name = value;
+            }
+        }
+
+
+        public int Weight
+        {
+            get => this.weight;
+            set
+            {
+                Validator.ThrowIfNumberIsNotInRange(MinWeight, MaxWeight, value,
+                    $"{this.Name} weight should be in the range [{MinWeight}..{MaxWeight}].");
+
+                this.weight = value;
+            }
+        }
+
+
+        public double GetCalories()
+        {
+            var modifier = GetModifier();
+
+            return this.Weight * 2 * modifier;
+        }
+
+        private double GetModifier()
+        {
+            var nameToLower = this.Name.ToLower();
+
+            if (nameToLower == "meat")
+            {
+                return 1.2;
+            }
+
+            if (nameToLower == "veggies")
+            {
+                return 0.8;
+            }
+
+            if (nameToLower == "cheese")
+            {
+                return 1.1;
+            }
+
+            return 0.9;
+
+        }
+    }
+}
---
+++ CSharp_OOP/L04_Encapsulation_Exercise/P04_Pizza_Calories/Validator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Validator
+    {
+        public static void ThrowIfNumberIsNotInRange(int min, int max, int number, string exceptionMessage)
+        {
+            if(number < min || number > max)
+            {
+                throw new ArgumentException(exceptionMessage);
+            }
+        }
+
+
+        //!!! !!! !!! Da dopalnq za vsichki exceptioni  ->
+
+        //public static void ThrowIfValueIsNotAllowed(HashSet<string> allowedValues, string value, string exceptionMessage)
+        //{
+        //    if(allowedValues.Contains(value))
+        //    {
+        //        throw new ArgumentException(exceptionMessage);
+        //    }
+        //}
+
+
+
+    }
+}
