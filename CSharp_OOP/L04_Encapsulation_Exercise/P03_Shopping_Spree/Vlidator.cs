8f55aa0171b9f3686cb542908c3c947967a9fbb8
Solenkoff <solenkoff@yahoo.com>
2023-07-27 11:06:40 +0300

    Added problem solution

---
+++ CSharp_OOP/L04_Encapsulation_Exercise/P03_Shopping_Spree/Vlidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02_Shopping_Spree
+{
+    public static class Validator
+    {
+
+        public static void ThrowIfStringIsNullOrEmpty(string str, string exceptionMessage)
+        {
+            if(string.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentException(exceptionMessage);
+            }
+        }
+
+
+        public static void ThrowIfNumberIsNegative(decimal number, string exceptionMassege)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException(exceptionMassege);
+            }
+        }
+        
+    }
+}
