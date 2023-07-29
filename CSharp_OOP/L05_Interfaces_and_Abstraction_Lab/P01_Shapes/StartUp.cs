b31ec9b6a21d3f51345e29be6284b56801b5de11
Solenkoff <solenkoff@yahoo.com>
2023-07-29 15:26:52 +0300

    Added folder and solutions

---
+++ CSharp_OOP/L05_Interfaces_and_Abstraction_Lab/P01_Shapes/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<IDrawable> drawableObjects = new List<IDrawable>();
+
+            drawableObjects.Add(new Rectangle(4, 6));
+            drawableObjects.Add(new Circle(7));
+
+
+            foreach (var item in drawableObjects)
+            {
+                item.Draw();
+            }
+
+           
+        }
+    }
+}
