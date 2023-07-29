603c48f87a3cacb9fad22ed9f2d2bfc1fd64fd9c
Solenkoff <solenkoff@yahoo.com>
2023-07-29 15:27:16 +0300

    Added problem solution

---
+++ CSharp_OOP/L05_Interfaces_and_Abstraction_Lab/P02_Cars/Tesla.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+
+        public Tesla(string model, string color, int battery)
+        {
+            this.Battery = battery;
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public int Battery { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} Tesla {Model} with {Battery} Batteries");
+            sb.AppendLine(this.Start());
+            sb.Append(this.Stop());
+
+            return sb.ToString();    
+        }
+
+    }
+}
