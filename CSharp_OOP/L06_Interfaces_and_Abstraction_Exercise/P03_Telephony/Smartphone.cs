5973e5c6d4761d02b191baa62656a6159d60b732
Solenkoff <solenkoff@yahoo.com>
2023-07-31 11:24:13 +0300

    Added problem solution

---
+++ CSharp_OOP/L06_Interfaces_and_Abstraction_Exercise/P03_Telephony/Smartphone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telephony
+{
+    public class Smartphone : ICallable, IBrowsable
+    {
+        public Smartphone()
+        {
+
+        }
+        //public string Number  { get; set; }
+        public void Browse(string url)
+        {
+            if (url.Any(char.IsDigit))
+            {
+                Console.WriteLine("Invalid URL!"); 
+            }
+            else
+            {
+                Console.WriteLine($"Browsing: {url}!"); 
+            }
+        }
+
+        public void Call(string number)
+        {
+            if (number.All(char.IsDigit))
+            {
+                Console.WriteLine($"Calling... {number}"); 
+            }
+            else
+            {
+                Console.WriteLine("Invalid number!"); 
+            }
+        }
+    }
+}
---
+++ CSharp_OOP/L06_Interfaces_and_Abstraction_Exercise/P03_Telephony/StationaryPhone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telephony
+{
+    public class StationaryPhone : ICallable
+    {
+        public StationaryPhone()
+        {
+
+        }
+        public void Call(string number)
+        {
+            if (number.All(char.IsDigit))
+            {
+                Console.WriteLine($"Dialing... {number}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number!");
+            }
+        }
+    }
+}
