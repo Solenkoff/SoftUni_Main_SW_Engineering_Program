e44148eaf911438b18992fde0d0cb2c4a30de939
Solenkoff <solenkoff@yahoo.com>
2023-07-31 11:24:13 +0300

    Added problem solution

---
+++ CSharp_OOP/L06_Interfaces_and_Abstraction_Exercise/P03_Telephony/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Telephony
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> numbers = Console.ReadLine().Split().ToList();
+            List<string> urls = Console.ReadLine().Split().ToList();
+
+
+
+
+            foreach (var item in numbers)
+            {
+                if(item.Length == 10)
+                {
+                    ICallable smartphone = new Smartphone();
+                    smartphone.Call(item);
+                }
+                else
+                {
+                    ICallable stationaryPhone = new StationaryPhone();
+                    stationaryPhone.Call(item);
+                }
+            }
+
+            foreach (var item in urls)
+            {
+                IBrowsable smartphone = new Smartphone();
+                smartphone.Browse(item);
+            }
+
+        }
+    }
+}
