e02654e550a8794e9083b97b5046a5d41ad1087d
Solenkoff <solenkoff@yahoo.com>
2023-07-31 11:25:15 +0300

    Added problem solution

---
+++ CSharp_OOP/L06_Interfaces_and_Abstraction_Exercise/P05_Birthday_Celebrations/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Pet : IBirthable
+    {
+        public Pet(string name, string birthdate)
+        {
+            this.Name = name;
+            this.Birthdate = birthdate;
+        }
+        public string Birthdate { get; private set; }
+
+        public string Name { get; private set; }
+    }
+}
