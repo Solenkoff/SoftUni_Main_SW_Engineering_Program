22d2eeeb8045139bca0756cf7082a60fd567b696
Solenkoff <solenkoff@yahoo.com>
2023-07-31 11:24:33 +0300

    Added problem solution

---
+++ CSharp_OOP/L06_Interfaces_and_Abstraction_Exercise/P04_Border_Control/Citizen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+
+        public Citizen(string name, int age, string id)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+       
+    }
+}
---
+++ CSharp_OOP/L06_Interfaces_and_Abstraction_Exercise/P04_Border_Control/IPerson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public interface IPerson : IIdentifiable
+    {
+
+       string Name { get; set; }
+
+       int Age { get; set; }
+    }
+}
