e19eccbec94c41b14b4d6352480937c4a49ec4bb
Solenkoff <solenkoff@yahoo.com>
2023-08-01 10:17:42 +0300

    Added problem solution

---
+++ CSharp_OOP/L06_Interfaces_and_Abstraction_Exercise/P07_Military_Elite/Contracts/ISpecialisedSoldier.cs
@@ -0,0 +1,13 @@
+﻿using MilitaryElite.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Contracts
+{
+    public interface ISpecialisedSoldier : IPrivate
+    {
+        Corps Corps { get; }
+
+    }
+}
