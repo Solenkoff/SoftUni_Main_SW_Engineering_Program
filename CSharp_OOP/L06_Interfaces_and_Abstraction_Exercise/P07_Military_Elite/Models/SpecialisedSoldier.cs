4cd567409305c7d1a453d1bf46a97951d9168b16
Solenkoff <solenkoff@yahoo.com>
2023-08-01 10:17:42 +0300

    Added problem solution

---
+++ CSharp_OOP/L06_Interfaces_and_Abstraction_Exercise/P07_Military_Elite/Models/SpecialisedSoldier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MilitaryElite.Contracts;
+using MilitaryElite.Enums;
+
+namespace MilitaryElite.Models
+{
+    public abstract class SpecialisedSoldier : Private, ISpecialisedSoldier
+    {
+        protected SpecialisedSoldier(
+            string id, 
+            string firstName, 
+            string lastName, 
+            decimal salary, 
+            Corps corps)
+            : base(id, firstName, lastName, salary)
+        {
+            this.Corps = corps;
+        }
+
+        public Corps Corps { get; private set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + Environment.NewLine + $"Corps: {this.Corps}";
+        }
+
+    }
+}
