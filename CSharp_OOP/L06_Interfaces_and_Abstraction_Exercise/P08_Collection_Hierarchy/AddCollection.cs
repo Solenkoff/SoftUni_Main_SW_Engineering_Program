45dce5bc57d3c0f341fec081415938a426d9be93
Solenkoff <solenkoff@yahoo.com>
2023-08-02 16:18:28 +0300

    Added problem solution

---
+++ CSharp_OOP/L06_Interfaces_and_Abstraction_Exercise/P08_Collection_Hierarchy/AddCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionHierarchy
+{
+    public class AddCollection : Collection, IAddable
+    {
+        private int index = 0;
+
+        public AddCollection()
+            : base()
+        {
+
+        }
+        public int Add(string @string)
+        {
+           this.Strings.Add(@string);
+            return index++;
+        }
+    }
+}
---
+++ CSharp_OOP/L06_Interfaces_and_Abstraction_Exercise/P08_Collection_Hierarchy/Collection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionHierarchy
+{
+    public abstract class Collection
+    {
+
+        private List<string> strings;
+        public Collection()
+        {
+            this.Strings = new List<string>(100);
+        }
+
+
+
+        public List<string> Strings { get; set; }
+
+    }
+}
