a5da0ccd4554e5ab74407320a213aa20d982118f
Solenkoff <solenkoff@yahoo.com>
2023-08-02 16:18:55 +0300

    Added problem solution

---
+++ CSharp_OOP/L06_Interfaces_and_Abstraction_Exercise/P09_Explicit_Interfaces/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExplicitInterfaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Citizen> citizens = new List<Citizen>();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if(line == "End")
+                {
+                    break;
+                }
+
+                string[] citizenData = line.Split();
+
+                string name = citizenData[0];
+                string country = citizenData[1];
+                int age = int.Parse(citizenData[2]);
+                
+                Citizen newCitizen = new Citizen(name, country, age);
+
+                citizens.Add(newCitizen);
+
+            }
+
+            foreach (var citizen in citizens)
+            {
+                //IPerson citizenIPerson = citizen;
+                Console.WriteLine(citizen.GetName());
+
+                IResident citizenIResident = citizen;
+                Console.WriteLine(citizenIResident.GetName());
+            }
+        }
+    }
+}
