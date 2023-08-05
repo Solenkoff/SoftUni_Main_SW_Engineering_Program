8bb7a93661d645633c8a48cac1c09d0afe727a7b
Solenkoff <solenkoff@yahoo.com>
2023-08-05 14:27:01 +0300

    Added folder and solutions

---
+++ CSharp_OOP/L08_Polymorphism_Exercise/P01_Vehicles/StartUp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace P01v2_Vehicles
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car = CreateVehicle();
+            Vehicle truck = CreateVehicle();
+
+            int numOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfCommands; i++)
+            {
+                string[] parts = Console.ReadLine().Split();
+                string command = parts[0];
+                string vehicleType = parts[1];
+                double parameter = double.Parse(parts[2]);
+
+                if (command == "Drive")
+                {
+                    try
+                    {
+                        if (vehicleType == nameof(Car))
+                        {
+                            car.Drive(parameter);
+                        }
+                        else if (vehicleType == nameof(Truck))
+                        {
+                            truck.Drive(parameter);
+                        }
+
+                        Console.WriteLine($"{vehicleType} travelled {parameter} km");
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    if (vehicleType == nameof(Car))
+                    {
+                        car.Refuel(parameter);
+                    }
+                    else if (vehicleType == nameof(Truck))
+                    {
+                        truck.Refuel(parameter);
+                    }
+                }
+            }
+
+            Console.WriteLine(car);
+            Console.WriteLine(truck);
+
+        }
+
+
+
+        private static Vehicle CreateVehicle()
+        {
+
+            string[] parts = Console.ReadLine().Split();
+
+            string type = parts[0];
+            double fuelQuantity = double.Parse(parts[1]);
+            double fuelConsomption = double.Parse(parts[2]);
+
+            Vehicle vehicle = null;
+
+            if(type == nameof(Car))
+            {
+                vehicle = new Car(fuelQuantity, fuelConsomption);
+            }
+            else if(type == nameof(Truck))
+            {
+                vehicle = new Truck(fuelQuantity, fuelConsomption);
+            }
+
+            return vehicle;
+
+        }
+    }
+}
---
+++ CSharp_OOP/L08_Polymorphism_Exercise/P01_Vehicles/Truck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01v2_Vehicles
+{
+    public class Truck : Vehicle
+    {
+        private const double TruckAirConditionModifier = 1.6;
+
+        public Truck(double fuelQuantity, double fuelConsumption)
+        : base(fuelQuantity, fuelConsumption, TruckAirConditionModifier)
+        {
+
+        }
+
+
+        public override void Refuel(double refuelQuantiy)
+        {
+            base.Refuel(refuelQuantiy * 0.95);
+        }
+    }
+}
