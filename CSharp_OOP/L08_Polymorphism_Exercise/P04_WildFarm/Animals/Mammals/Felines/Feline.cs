8cfc81014b39964b98950dba721fb63b8fb08a16
Solenkoff <solenkoff@yahoo.com>
2023-08-07 11:40:13 +0300

    Added problem solution

---
+++ CSharp_OOP/L08_Polymorphism_Exercise/P04_WildFarm/Animals/Mammals/Felines/Feline.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm.Animals.Mammals
+{
+    public abstract class Feline : Mammal
+    {
+        protected Feline(string name,
+                         double weight,
+                         int foodEaten,
+                         HashSet<string> allowedFoods,
+                         double weightModifier,
+                         string livingRegion,
+                         string breed)
+            : base(name, weight, foodEaten, allowedFoods, weightModifier, livingRegion)
+        {
+            this.Breed = breed;
+        }
+
+        public string Breed { get; private set; }
+
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name} [{this.Name}, {this.Breed}, {this.Weight}, {this.LivingRegion}, {this.FoodEaten}]";
+        }
+
+    }
+}
