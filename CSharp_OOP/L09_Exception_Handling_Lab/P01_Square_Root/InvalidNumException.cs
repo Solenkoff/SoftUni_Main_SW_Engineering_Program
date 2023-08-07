d9d0c8b7d19ae86eae8a32fe5265c7a296e8d7db
Solenkoff <solenkoff@yahoo.com>
2023-08-07 11:38:34 +0300

    Added folder and solutions

---
+++ CSharp_OOP/L09_Exception_Handling_Lab/P01_Square_Root/InvalidNumException.cs
@@ -0,0 +1,15 @@
+﻿namespace P01_Square_Root
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class InvalidNumException : Exception
+    {
+        public InvalidNumException(string message)
+            :base(message)
+        {
+           
+        }
+    }
+}
