123b926fa0040c3dfd80e7d31a540eebdb63ff00
Solenkoff <solenkoff@yahoo.com>
2023-08-07 11:38:34 +0300

    Added folder and solutions

---
+++ CSharp_OOP/L09_Exception_Handling_Lab/P01_Square_Root/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P01_Square_Root
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if(num < 0)
+                {
+                    throw new ArgumentException("Invalid number.");  // InvalidNumException
+                }
+
+                double sqrRootOfNum = Math.Sqrt(num);
+                Console.WriteLine(sqrRootOfNum);
+            }
+            catch (ArgumentException ex)                        // InvalidNumException
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+        }
+    }
+}
