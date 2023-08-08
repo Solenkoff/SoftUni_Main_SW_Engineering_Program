dcb42896f8ae881be916129b56cde7d31d25e02a
Solenkoff <solenkoff@yahoo.com>
2023-08-08 10:45:55 +0300

    Added problem solution

---
+++ CSharp_OOP/L09_Exception_Handling_Lab/P04_Sum_of_Integers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P04_Sum_of_Integers
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] elements = Console.ReadLine().Split();
+
+            int sum = 0;
+
+            foreach (var element in elements)
+            {
+                try
+                {
+                    int num = Convert.ToInt32(element);
+
+                    sum += num;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{element}' is in wrong format!");
+                }
+                catch(OverflowException)
+                {
+                    Console.WriteLine($"The element '{element}' is out of range!");
+                }
+
+                Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+
+        }
+
+    }
+}
