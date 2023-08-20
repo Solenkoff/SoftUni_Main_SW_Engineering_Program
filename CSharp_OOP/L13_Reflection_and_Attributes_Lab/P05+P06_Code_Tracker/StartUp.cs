ed24a09dfc2165b4f3563733f77bd96788dbd69b
Solenkoff <solenkoff@yahoo.com>
2023-08-20 15:32:52 +0300

    Added problem solution

---
+++ CSharp_OOP/L13_Reflection_and_Attributes_Lab/P05+P06_Code_Tracker/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Az")]
+    [Author("ti")]
+    [Author("nie")]
+
+    public class StartUp
+    {
+
+        [Author("Otnovo az")]
+
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+
+            tracker.PrintMethodsByAuthor();
+        }
+
+        [Author("Dimitrichko")]
+
+        private static void NextGen()
+        {
+
+        }
+    }
+}
---
+++ CSharp_OOP/L13_Reflection_and_Attributes_Lab/P05+P06_Code_Tracker/TestClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthorProblem
+{
+    [Author("Viktor")]
+    class TestClass
+    {
+
+        [Author("Gogicha")]
+        public void GogiMethod()
+        {
+
+        }
+
+        [Author("Dimitrichko")]
+        public void AlotOfWork()
+        {
+
+        }
+
+    }
+}
