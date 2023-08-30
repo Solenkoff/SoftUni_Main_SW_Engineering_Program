3c4defc09c6117f0fde06caf4c0ac32db13708b4
Solenkoff <solenkoff@yahoo.com>
2023-08-30 15:55:24 +0300

    Added problem solution

---
+++ CSharp_OOP/L17_Mocking_and_Test_Driven_Development_Lab/P03_Students_DB.Test/CourseTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using StudentsDB.Test.Fakes;
+using System;
+
+namespace StudentsDB.Test
+{
+
+    [TestFixture]
+    public class CourseTests
+    {
+
+        [Test]
+        public void When_DisplayAllStudentsIsCalled_ShouldDisplayAll()
+        {
+            Course course = new Course(new FakeDatabase());
+
+            course.DisplayAllStudents();
+
+            course.SignStudent(1);
+        }
+    }
+}
---
+++ CSharp_OOP/L17_Mocking_and_Test_Driven_Development_Lab/P03_Students_DB.Test/Fakes/FakeDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentsDB.Test.Fakes
+{
+    public class FakeDatabase : IDatabase
+    {
+        public IEnumerable<Student> GetAllstudents()
+        {
+            return new Student[] { new Student(1, "Pesho"), new Student(2, "Gosho"), new Student(3, "Tosho") };
+        }
+
+        public Student GetStudentById(int id)
+        {
+            return new Student(1, "Gosheto");
+        }
+
+        public void UpdateStudent(int id, Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
