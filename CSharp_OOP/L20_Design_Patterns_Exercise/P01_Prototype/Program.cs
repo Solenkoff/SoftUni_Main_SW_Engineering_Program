125d8b63f324591e2ea3c061d01b75b584b493bb
Solenkoff <solenkoff@yahoo.com>
2023-09-27 11:41:13 +0300

    Added folder and solutions

---
+++ CSharp_OOP/L20_Design_Patterns_Exercise/P01_Prototype/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1.Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SandwichMenu menu = new SandwichMenu();
+
+            menu["BLT"] = new Sandwich("Wheat", "Bacon", "", "lettcue, Tomato");
+            menu["PB&J"] = new Sandwich("White", "", "", "Peanut Butter, Jelly");
+            menu["Turkey"] = new Sandwich("Rye", "Turkey", "", "lettcue, Onion, Tomato");
+
+            menu["LoadedBLT"] = new Sandwich("Wheat", "Turkey,Bacon", "American", "lettuce, Tomato,Onion,Olives");
+            menu["ThreeMeatCombo"] = new Sandwich("Rye", "Turkey, Ham, Salami", "Provolone", "Lettuce, Onion");
+            menu["Vegetarian"] = new Sandwich("What", "", "", "Letuce, Onion, Tomato, Olives, Spinach");
+
+            Sandwich sandwich1 = menu["BLT"].Clone() as Sandwich;
+            Sandwich sandwich2 = menu["ThreeMeatCombo"].Clone() as Sandwich;
+            Sandwich sandwich3 = menu["Vegetarian"].Clone() as Sandwich;
+
+            Sandwich burger = new Sandwich("Wheat", "Bacon", "Amrican", "Lettuce, Tomato");
+            Sandwich burgerCopy = burger.Clone() as Sandwich;
+            burger = new Sandwich("White", "Bacon", "American", "Lettuce");
+            ;
+
+        }
+    }
+}
