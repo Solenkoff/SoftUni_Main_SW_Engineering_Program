babb2993c3eb338bf1d341044b3514a9aed83946
Solenkoff <solenkoff@yahoo.com>
2023-10-03 18:12:54 +0300

    Added folder and solutions

---
+++ CSharp_OOP/Exams/CSharp_OOP_2020-08-22_Exam_Retake/P01_EasterRaces_Structure/Models/Drivers/Entities/Driver.cs
@@ -0,0 +1,59 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Models.Drivers.Contracts;
+using EasterRaces.Utilities;
+using EasterRaces.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Drivers.Entities
+{
+    public class Driver : IDriver
+    {
+        private const int NameMinLenght = 5;
+
+        private string name;
+      
+        private ICar car;
+
+        public Driver(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                Validator.ThrowIfStringIsNullEmptyOrLessThan(value, NameMinLenght, string.Format
+                                            (ExceptionMessages.InvalidName, value, NameMinLenght));
+
+                this.name = value;
+            }
+        }
+
+        public ICar Car { get; private set; }
+
+        public int NumberOfWins { get; private set; }
+
+        public bool CanParticipate => this.Car != null;
+        
+
+        public void AddCar(ICar car)
+        {
+            if(car == null)
+            {
+                throw new ArgumentNullException(ExceptionMessages.CarInvalid);
+            }
+
+            this.Car = car;
+ 
+        }
+
+        public void WinRace()
+        {
+            this.NumberOfWins += 1;
+        }
+    }
+}
---
+++ CSharp_OOP/Exams/CSharp_OOP_2020-08-22_Exam_Retake/P01_EasterRaces_Structure/Utilities/Validator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Utilities
+{
+    public static class Validator
+    {
+
+        public static void ThrowIfStringIsNullEmptyOrLessThan(string str, int minLenght, string message)
+        {
+            if (string.IsNullOrEmpty(str) || str.Length < minLenght)
+            {
+                throw new ArgumentException(message);
+            }
+
+        }
+    }
+}
