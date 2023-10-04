335ee5f3e09238df788310ee926951d0df287c28
Solenkoff <solenkoff@yahoo.com>
2023-10-04 18:26:56 +0300

    Added folder and solutions

---
+++ CSharp_OOP/Exams/CSharp_OOP_2020-12-12_Exam/P01_&_P02_Bakery_Structure_&_Business_Logic/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace Bakery
+{
+    using Bakery.Core;
+    using Bakery.Models.BakedFoods;
+    using Bakery.Models.Tables;
+    using System.Globalization;
+    using System.Threading;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
+
+            //Don't forget to comment out the commented code lines in the Engine class!
+            var engine = new Engine();
+
+            engine.Run();
+        }
+    }
+}
