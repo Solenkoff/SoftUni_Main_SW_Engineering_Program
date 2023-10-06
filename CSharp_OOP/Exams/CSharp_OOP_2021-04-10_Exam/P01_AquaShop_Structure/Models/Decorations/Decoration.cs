1c9f37bfa42c15824461ee50f8d563bf9f0f762a
Solenkoff <solenkoff@yahoo.com>
2023-10-06 15:32:19 +0300

    Added folder and solutions

---
+++ CSharp_OOP/Exams/CSharp_OOP_2021-04-10_Exam/P01_AquaShop_Structure/Models/Decorations/Decoration.cs
@@ -0,0 +1,20 @@
+﻿namespace AquaShop.Models.Decorations
+{
+    using Models.Decorations.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public abstract class Decoration : IDecoration
+    {
+        protected Decoration(int comfort, decimal price)
+        {
+            this.Comfort = comfort;
+            this.Price = price;
+        }
+
+        public int Comfort { get; private set; }
+
+        public decimal Price { get; private set; }
+    }
+}
---
+++ CSharp_OOP/Exams/CSharp_OOP_2021-04-10_Exam/P01_AquaShop_Structure/Repositories/DecorationRepository.cs
@@ -0,0 +1,41 @@
+﻿namespace AquaShop.Repositories
+{
+    using Models.Decorations.Contracts;
+    using Repositories;
+    using Repositories.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class DecorationRepository : IRepository<IDecoration>
+    {
+        private readonly List<IDecoration> decorations;
+
+        public DecorationRepository()
+        {
+            this.decorations = new List<IDecoration>();
+        }
+
+
+        public IReadOnlyCollection<IDecoration> Models => this.decorations.AsReadOnly();
+
+        public void Add(IDecoration model)
+        {
+            this.decorations.Add(model);
+        }
+
+
+        public bool Remove(IDecoration model)
+        {
+            return this.decorations.Remove(model);
+        }
+
+
+        public IDecoration FindByType(string type)
+        {
+            return this.decorations.FirstOrDefault(d => d.GetType().Name == type);   //  !!!   NULL  !!!
+        }
+
+    }
+}
