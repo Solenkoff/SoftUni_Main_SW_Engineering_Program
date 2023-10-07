e7675108fee5605be0ccd9fdb92ac6e55634e771
Solenkoff <solenkoff@yahoo.com>
2023-10-07 10:41:13 +0300

    Added problem solution

---
+++ CSharp_OOP/Exams/CSharp_OOP_2021-04-10_Exam/P02_AquaShop_Business Logic/Models/Aquariums/Aquarium.cs
@@ -0,0 +1,102 @@
+﻿namespace AquaShop.Models.Aquariums
+{
+    using Utilities;
+    using Models.Aquariums.Contracts;
+    using Models.Decorations.Contracts;
+    using Models.Fish.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Utilities.Messages;
+    using System.Linq;
+
+    public abstract class Aquarium : IAquarium
+    {
+        private string name;
+        private ICollection<IDecoration> decorations;
+        private ICollection<IFish> fish;
+
+        protected Aquarium(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.decorations = new HashSet<IDecoration>();
+            this.fish = new HashSet<IFish>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                Validator.ThrowIfStringIsNullOrWhiteSpace(value, ExceptionMessages.InvalidAquariumName);
+
+                this.name = value;
+            }
+        }
+
+        public int Capacity { get; private set; }
+
+        public int Comfort => this.decorations.Sum(d => d.Comfort);
+
+        public ICollection<IDecoration> Decorations => this.decorations;
+
+        public ICollection<IFish> Fish => this.fish;
+
+
+        public void AddFish(IFish fish)
+        {
+            if(this.Capacity == this.fish.Count)
+            {
+                throw new InvalidOperationException(ExceptionMessages.NotEnoughCapacity);
+            }
+
+            this.fish.Add(fish);
+        }
+
+        public bool RemoveFish(IFish fish)
+        {
+            return this.fish.Remove(fish);
+        }
+
+
+        public void AddDecoration(IDecoration decoration)
+        {
+            this.Decorations.Add(decoration);
+        }
+     
+
+        public void Feed()
+        {
+            foreach (var fishItem in fish)
+            {
+                fishItem.Eat();
+            }
+        }
+
+        public string GetInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] fishnames = this.Fish.Select(f => f.Name).ToArray();
+
+            sb.AppendLine($"{this.Name} ({this.GetType().Name}):");
+
+            if(this.Fish.Count == 0)
+            {
+                sb.AppendLine("Fish: none");
+            }
+            else
+            {
+                sb.AppendLine($"Fish: {string.Join(", ", fishnames)}");
+            }
+
+            sb.AppendLine($"Decorations: {this.Decorations.Count}");
+            sb.AppendLine($"Comfort: {this.Comfort}");
+
+            return sb.ToString().Trim();
+        }
+            
+
+    }
+}
---
+++ CSharp_OOP/Exams/CSharp_OOP_2021-04-10_Exam/P02_AquaShop_Business Logic/Utilities/Validator.cs
@@ -0,0 +1,17 @@
+﻿namespace AquaShop.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public static class Validator
+    {
+        public static void ThrowIfStringIsNullOrWhiteSpace(string value, string message)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException(message);
+            }
+        }
+    }
+}
