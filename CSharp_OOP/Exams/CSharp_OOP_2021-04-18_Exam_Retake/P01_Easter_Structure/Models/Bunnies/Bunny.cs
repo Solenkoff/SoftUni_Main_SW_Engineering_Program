f26ee93359baaf5fbbdcff91b4a37056349c6ba2
Solenkoff <solenkoff@yahoo.com>
2023-10-08 11:11:13 +0300

    Added folder and solutions

---
+++ CSharp_OOP/Exams/CSharp_OOP_2021-04-18_Exam_Retake/P01_Easter_Structure/Models/Bunnies/Bunny.cs
@@ -0,0 +1,56 @@
+﻿namespace Easter.Models.Bunnies
+{
+    using Easter.Models.Bunnies.Contracts;
+    using Easter.Models.Dyes.Contracts;
+    using Easter.Utilities.Messages;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public abstract class Bunny : IBunny
+    {
+        private string name;
+        private readonly ICollection<IDye> dyes;
+
+        protected Bunny(string name, int energy)
+        {
+            this.Name = name;
+            this.Energy = energy;
+            this.dyes = new List<IDye>();
+        }
+
+        public string Name 
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidBunnyName);
+                }
+
+                this.name = value;
+            }
+        }
+
+
+        public int Energy { get; protected set; }
+
+        public ICollection<IDye> Dyes => this.dyes;
+
+        public void AddDye(IDye dye)
+        {
+            this.Dyes.Add(dye);
+        }
+
+        public virtual void Work()
+        {
+            this.Energy -= 10;
+
+            if (this.Energy < 0)
+            {
+                this.Energy = 0;
+            }
+        }
+    }
+}
---
+++ CSharp_OOP/Exams/CSharp_OOP_2021-04-18_Exam_Retake/P01_Easter_Structure/Repositories/Contracts/BunnyRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace Easter.Repositories.Contracts
+{
+    using Easter.Models.Bunnies.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BunnyRepository : IRepository<IBunny>
+    {
+        private readonly List<IBunny> bunnies;
+
+        public BunnyRepository()
+        {
+            this.bunnies = new List<IBunny>();
+        }
+
+        public IReadOnlyCollection<IBunny> Models => this.bunnies.AsReadOnly();
+
+        public void Add(IBunny model)
+        {
+            this.bunnies.Add(model);
+        }
+
+        public bool Remove(IBunny model)
+        {
+            return this.bunnies.Remove(model);
+        }
+
+        public IBunny FindByName(string name)
+        {
+            return this.bunnies.FirstOrDefault(b => b.Name == name);
+        }
+
+        
+    }
+}
