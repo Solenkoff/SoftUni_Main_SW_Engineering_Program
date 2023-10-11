383ecc8d880088bb5288a54f0d59d9e545ad6d50
Solenkoff <solenkoff@yahoo.com>
2023-10-11 10:33:00 +0300

    Added folder and solutions

---
+++ CSharp_OOP/Exams/CSharp_OOP_2021-08-15_Exam(M)/P01_CarRacing_Structure/CarRacing/Models/Racers/ProfessionalRacer.cs
@@ -0,0 +1,23 @@
+﻿namespace CarRacing.Models.Racers
+{
+    using CarRacing.Models.Cars.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ProfessionalRacer : Racer
+    {
+        private const int InitialDrivingExperience = 30;
+        private const string StrictRacingBehavior = "strict";
+        public ProfessionalRacer(string username, ICar car) 
+            : base(username, StrictRacingBehavior, InitialDrivingExperience, car)
+        {
+        }
+
+        public override void Race()
+        {
+            base.Race();
+            this.DrivingExperience += 10;
+        }
+    }
+}
