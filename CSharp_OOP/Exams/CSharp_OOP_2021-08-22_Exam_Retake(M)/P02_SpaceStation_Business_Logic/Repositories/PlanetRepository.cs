a37afa71146a1d2b3d818cdd4a62d28b80b9f2c0
Solenkoff <solenkoff@yahoo.com>
2023-10-14 15:32:57 +0300

    Added problem solution

---
+++ CSharp_OOP/Exams/CSharp_OOP_2021-08-22_Exam_Retake(M)/P02_SpaceStation_Business_Logic/Repositories/PlanetRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace SpaceStation.Repositories
+{
+    using SpaceStation.Models.Planets.Contracts;
+    using SpaceStation.Repositories.Contracts;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private readonly IList<IPlanet> planets;
+
+        public PlanetRepository()
+        {
+            this.planets = new List<IPlanet>();
+        }
+
+        public IReadOnlyCollection<IPlanet> Models
+            => (IReadOnlyCollection<IPlanet>)this.planets;
+
+        public void Add(IPlanet model)
+            => this.planets.Add(model);
+
+        public IPlanet FindByName(string name)
+            => this.planets.FirstOrDefault(p => p.Name == name);
+
+        public bool Remove(IPlanet model)
+            => this.planets.Remove(model);
+    }
+}
