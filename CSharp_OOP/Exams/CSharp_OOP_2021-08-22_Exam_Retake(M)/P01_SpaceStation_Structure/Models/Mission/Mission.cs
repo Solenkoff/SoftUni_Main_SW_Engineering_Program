cf6118c429dcb6c04a9d5f4403ff2d258e3ec978
Solenkoff <solenkoff@yahoo.com>
2023-10-14 15:31:35 +0300

    Added folder and solutions

---
+++ CSharp_OOP/Exams/CSharp_OOP_2021-08-22_Exam_Retake(M)/P01_SpaceStation_Structure/Models/Mission/Mission.cs
@@ -0,0 +1,48 @@
+﻿namespace SpaceStation.Models.Mission
+{
+    using System.Linq;
+    using SpaceStation.Models.Astronauts.Contracts;
+    using SpaceStation.Models.Mission.Contracts;
+    using SpaceStation.Models.Planets.Contracts;
+
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+            while (true)
+            {
+                var astronaut = astronauts
+                    .FirstOrDefault(x => x.CanBreath);
+
+                if (astronaut == null)
+                {
+                    break;
+                }
+
+                var item = planet
+                    .Items
+                    .FirstOrDefault();
+
+                if (item == null)
+                {
+                    break;
+                }
+
+                astronaut.Breath();
+
+                astronaut
+                    .Bag
+                    .Items
+                    .Add(item);
+
+                planet
+                    .Items
+                    .Remove(item);
+            }
+        }
+    }
+}
