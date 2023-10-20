39863b4999173eb4cd47e4654676778e751e016a
Solenkoff <solenkoff@yahoo.com>
2023-10-20 13:48:11 +0300

    Added problem solution

---
+++ CSharp_OOP/Exams/CSharp_OOP_2022-04-09_Exam/P01_Formula1-Structure/Formula1/Repositories/FormulaOneCarRepository.cs
@@ -0,0 +1,39 @@
+﻿namespace Formula1.Repositories
+{
+    using Formula1.Models.Contracts;
+    using Formula1.Repositories.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class FormulaOneCarRepository : IRepository<IFormulaOneCar>
+    {
+        private readonly ICollection<IFormulaOneCar> cars;
+
+        public FormulaOneCarRepository()
+        {
+            this.cars = new HashSet<IFormulaOneCar>();
+        }
+
+        public IReadOnlyCollection<IFormulaOneCar> Models => (IReadOnlyCollection<IFormulaOneCar>)this.cars;
+
+        public void Add(IFormulaOneCar model)
+        {
+
+            this.cars.Add(model);
+        }
+
+        public bool Remove(IFormulaOneCar model)
+        {
+            return this.cars.Remove(model);
+        }
+
+        public IFormulaOneCar FindByName(string name)
+        {
+            return this.cars.FirstOrDefault(c => c.Model == name);
+        }
+
+       
+    }
+}
