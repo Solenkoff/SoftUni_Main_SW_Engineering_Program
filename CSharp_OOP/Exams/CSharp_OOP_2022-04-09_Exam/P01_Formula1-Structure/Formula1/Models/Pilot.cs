50895e465106fc127cb07bed9dd217d2e7879e82
Solenkoff <solenkoff@yahoo.com>
2023-10-20 13:48:11 +0300

    Added problem solution

---
+++ CSharp_OOP/Exams/CSharp_OOP_2022-04-09_Exam/P01_Formula1-Structure/Formula1/Models/Pilot.cs
@@ -0,0 +1,69 @@
+﻿namespace Formula1.Models.Contracts
+{
+    using Formula1.Utilities;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Pilot : IPilot
+    {
+        private string fullName;
+        //private bool canRace;
+        private IFormulaOneCar car;
+
+        public Pilot(string fullName)
+        {
+            this.FullName = fullName;
+            this.CanRace = false;
+        }
+
+        public string FullName
+        {
+            get => this.fullName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidPilot, value));
+                }
+
+                this.fullName = value;
+            }
+        }
+
+        public bool CanRace { get; private set; }
+
+        public IFormulaOneCar Car
+        {
+            get => this.car;
+            private set
+            {
+                if(value == null)
+                {
+                    throw new NullReferenceException(ExceptionMessages.InvalidCarForPilot);
+                }
+
+                this.car = value;
+            }
+        }
+
+        public int NumberOfWins { get; private set; }
+
+        public void AddCar(IFormulaOneCar car)
+        {
+            this.Car = car;
+            this.CanRace = true;
+        }
+
+        public void WinRace()
+        {
+            this.NumberOfWins++;
+        }
+
+        public override string ToString()
+        {
+            return $"Pilot {this.FullName} has {this.NumberOfWins} wins.";
+        }
+
+    }
+}
---
+++ CSharp_OOP/Exams/CSharp_OOP_2022-04-09_Exam/P01_Formula1-Structure/Formula1/Repositories/RaceRepository.cs
@@ -0,0 +1,38 @@
+﻿namespace Formula1.Repositories
+{
+    using Formula1.Models.Contracts;
+    using Formula1.Repositories.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class RaceRepository : IRepository<IRace>
+    {
+        private readonly ICollection<IRace> races;
+
+        public RaceRepository()
+        {
+            this.races = new HashSet<IRace>();
+        }
+
+
+        public IReadOnlyCollection<IRace> Models => (IReadOnlyCollection<IRace>)this.races;
+
+        public void Add(IRace model)
+        {
+            this.races.Add(model);
+        }
+
+        public bool Remove(IRace model)
+        {
+            return this.races.Remove(model);
+        }
+
+        public IRace FindByName(string name)
+        {
+            return this.races.FirstOrDefault(c => c.RaceName == name);
+        }
+
+    }
+}
