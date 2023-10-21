368729ea5aa4168ca15d404c0db7aa3d35885a73
Solenkoff <solenkoff@yahoo.com>
2023-10-21 11:36:55 +0300

    Added problem solution

---
+++ CSharp_OOP/Exams/CSharp_OOP_2022-04-09_Exam/P02_Formula1-Business_Logic/Formula1/Models/FormulaOneCar.cs
@@ -0,0 +1,70 @@
+﻿namespace Formula1.Models
+{
+    using Formula1.Models.Contracts;
+    using Formula1.Utilities;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public abstract class FormulaOneCar : IFormulaOneCar
+    {
+        private string model;
+        private int horsePower;
+        private double engineDisplacement;
+
+        protected FormulaOneCar(string model, int horsepower, double engineDisplacement)
+        {
+            this.Model = model;
+            this.Horsepower = horsepower;
+            this.EngineDisplacement = engineDisplacement;
+        }
+
+        public string Model
+        {
+            get => this.model;
+            private set
+            {
+                if(string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidF1CarModel, value));
+                }
+
+                this.model = value;
+            }
+        }
+
+        public int Horsepower
+        {
+            get => this.horsePower;
+            private set
+            {
+                if (value < 900 || value > 1050)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidF1HorsePower, value));
+                }
+
+                this.horsePower = value;
+            }
+        }
+
+        public double EngineDisplacement
+        {
+            get => this.engineDisplacement;
+            private set
+            {
+                if (value < 1.6 || value > 2.00)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidF1EngineDisplacement, value));
+                }
+
+                this.engineDisplacement = value;
+            }
+        }
+
+        public double RaceScoreCalculator(int laps)
+        {
+            return this.EngineDisplacement / (this.Horsepower * 1.0) * laps;
+
+        }
+    }
+}
