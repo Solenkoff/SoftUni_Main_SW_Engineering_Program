b99bd9111290a4a687ffcc186ce082b575b8b3ff
Solenkoff <solenkoff@yahoo.com>
2023-10-22 15:47:17 +0300

    Added problem solution

---
+++ CSharp_OOP/Exams/CSharp_OOP_2022-04-18_Exam_Retake/P01_Heroes-Structure/Repositories/HeroRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace Heroes.Repositories
+{
+    using Models.Contracts;
+    using Repositories.Contracts;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class HeroRepository : IRepository<IHero>
+    {
+
+        private readonly Dictionary<string, IHero> heroes;
+
+        public HeroRepository()
+        {
+            this.heroes = new Dictionary<string, IHero>();
+        }
+
+        public IReadOnlyCollection<IHero> Models => this.heroes.Values;
+
+
+        public void Add(IHero model)
+        {
+            this.heroes.Add(model.Name, model);
+        }
+      
+        public bool Remove(IHero model)
+        {
+            return heroes.Remove(model.Name);
+        }
+
+        public IHero FindByName(string name)
+        {
+            if (heroes.ContainsKey(name))
+            {
+                return heroes[name];
+            }
+
+            return null;
+        }
+
+    }
+}
