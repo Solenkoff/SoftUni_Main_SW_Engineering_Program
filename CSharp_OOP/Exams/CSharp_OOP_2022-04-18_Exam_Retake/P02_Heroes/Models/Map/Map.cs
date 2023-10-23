a2155c9893cd81cad6eecb040700f27ec50a78e3
Solenkoff <solenkoff@yahoo.com>
2023-10-23 10:53:09 +0300

    Added problem solution

---
+++ CSharp_OOP/Exams/CSharp_OOP_2022-04-18_Exam_Retake/P02_Heroes/Models/Map/Map.cs
@@ -0,0 +1,70 @@
+﻿namespace Heroes.Models.Map
+{
+    using Models.Contracts;
+    using Models.Heroes;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Map : IMap
+    {   
+
+        public string Fight(ICollection<IHero> players)
+        {
+            var knights = new List<Knight>();
+            var barbarians = new List<Barbarian>();
+
+            foreach (var player in players)
+            {
+                if (player.IsAlive)
+                {
+                    if (player is Knight knight)
+                    {
+                        knights.Add(knight);
+                    }
+                    else if (player is Barbarian barbarian)
+                    {
+                        barbarians.Add(barbarian);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Invalid player type!");
+                    }
+                }
+               
+            }
+     
+
+            while (true)
+            {
+                foreach (var knight in knights.Where(kn => kn.IsAlive))
+                {
+                    foreach (var barbarian in barbarians.Where(b => b.IsAlive))
+                    {
+                        barbarian.TakeDamage(knight.Weapon.DoDamage());
+                    }
+
+                    if(barbarians.All(b => b.IsAlive == false))
+                    {
+                        return $"The knights took {knights.Where(kn => kn.IsAlive == false).Count()} casualties but won the battle.";
+
+                    }
+                }
+
+                foreach (var barbarian in barbarians.Where(b => b.IsAlive))
+                {
+                    foreach (var knight in knights.Where(kn => kn.IsAlive))
+                    {
+                        knight.TakeDamage(barbarian.Weapon.DoDamage());
+                    }
+
+                    if (knights.All(kn => kn.IsAlive == false))
+                    {
+                        return $"The barbarians took {barbarians.Where(b => b.IsAlive == false).Count()} casualties but won the battle.";
+                    }
+                }
+            }
+         
+        }
+    }
+}
---
+++ CSharp_OOP/Exams/CSharp_OOP_2022-04-18_Exam_Retake/P02_Heroes/Repositories/HeroRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace Heroes.Repositories
+{
+    using Models.Contracts;
+    using Repositories.Contracts;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class HeroRepository : IRepository<IHero>
+    {
+
+        private readonly List<IHero> heroes;
+
+        public HeroRepository()
+        {
+            this.heroes = new List<IHero>();
+        }
+
+        public IReadOnlyCollection<IHero> Models => this.heroes.AsReadOnly();
+
+
+        public void Add(IHero model)
+        {
+            this.heroes.Add(model);
+        }
+      
+        public bool Remove(IHero model)
+        {
+            return heroes.Remove(model);
+        }
+
+        public IHero FindByName(string name)
+        {
+            return this.heroes.FirstOrDefault(h => h.Name == name);
+        }
+
+    }
+}
