e502aa78914f546ebee73060106345fd08f81d4e
Solenkoff <solenkoff@yahoo.com>
2023-11-15 13:31:21 +0200

    Added problem solution

---
+++ CSharp_DB__MS_SQL/L06_Table_Relations_Exercise/P01v2_One-To-One_Relationship.sql.sql
@@ -0,0 +1,26 @@
+CREATE TABLE [Passports](
+[PassportID] INT PRIMARY KEY IDENTITY(101,1)
+,[PassportNumber] NVARCHAR(8) NOT NULL
+);
+
+
+CREATE TABLE [Persons](
+[PersonID] INT PRIMARY KEY IDENTITY
+,[FirstName] NVARCHAR(50) NOT NULL
+,[Salary] DECIMAL(9,2) NOT NULL
+,[PassportID] INT FOREIGN KEY REFERENCES [Passports]([PassportID]) NOT NULL UNIQUE
+);
+
+
+INSERT INTO [Passports] VALUES
+('N34FG21B')
+,('K65LO4R7')
+,('ZE657QP2');
+
+
+INSERT INTO [Persons] VALUES
+('Roberto', 43300.00, 102)
+,('Tom', 56100.00, 103)
+,('Yana', 60200.00, 101);
+
+
