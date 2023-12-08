ad5fba13470cb7373205b81267930ee1f96c7950
Solenkoff <solenkoff@yahoo.com>
2023-12-08 11:16:31 +0200

    Added folder and solutions

---
+++ CSharp_DB__MS_SQL/L10_Subqueries_and_Joins_Exercise/P03_Sales_Employees.sql
@@ -0,0 +1,10 @@
+  SELECT e.[EmployeeID]
+       , e.[FirstName]
+       , e.[LastName]
+       , d.[Name] AS [DepartmentName]
+      FROM [Employees] AS e
+      JOIN [Departments] AS d ON e.[DepartmentID] = d.[DepartmentID]
+   WHERE e.[DepartmentID] = 3
+ORDER BY e.[EmployeeID] ASC
+
+
