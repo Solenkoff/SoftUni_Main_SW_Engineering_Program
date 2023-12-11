2278139ea437aadc21c5ce91b945777a4cf7ae16
Solenkoff <solenkoff@yahoo.com>
2023-12-11 13:36:17 +0200

    Added folder and solutions

---
+++ CSharp_DB__MS_SQL/L10_Subqueries_and_Joins_Exercise/P12_Highest_Peaks_in_Bulgaria.sql
@@ -0,0 +1,10 @@
+SELECT   c.[CountryCode]
+       , m.[MountainRange]
+       , p.[PeakName]
+       , p.[Elevation]
+      FROM [Peaks] AS p
+ LEFT JOIN [Mountains] AS m ON p.[MountainId] = m.[Id]
+ LEFT JOIN [MountainsCountries] AS mc ON m.[Id] = mc.[MountainId]
+ LEFT JOIN [Countries] AS c ON mc.[CountryCode] = c.[CountryCode]
+  WHERE mc.[CountryCode] = 'BG' AND p.[Elevation] > 2835
+ORDER BY p.[Elevation] DESC
