36acc612e3100465634b67cdde04b8da9fecb4e1
Solenkoff <solenkoff@yahoo.com>
2024-01-24 16:23:17 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L01_AdoDotNet/P07_Print_All_Minion_Names/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.SqlClient;
+
+const string GetAllMinions = @"SELECT Name FROM Minions";
+
+
+const string connectionString = "Server=.;Database=MinionsDB;User Id=sa;Password=MSSQL*Pass01;TrustServerCertificate=True;";
+
+using SqlConnection? sqlConnection = new SqlConnection(connectionString);
+
+try
+{
+    await sqlConnection.OpenAsync();
+
+    using SqlCommand cmdPrintMinions = new(GetAllMinions, sqlConnection);
+    using SqlDataReader reader = await cmdPrintMinions.ExecuteReaderAsync();
+    List<string> allMinions = new List<string>();
+
+    while (await reader.ReadAsync())
+    {
+        allMinions.Add($"{reader["Name"].ToString()}");
+    }
+
+    for (int first = 0, last = allMinions.Count - 1; first <= last; first++, last--)
+    {
+        await Console.Out.WriteLineAsync($"{allMinions[first]}");
+
+        if (last != first)
+        {
+            await Console.Out.WriteLineAsync($"{allMinions[last]}");
+        }
+    }
+
+}
+catch
+{
+    await Console.Out.WriteLineAsync("ERROR");
+}
+finally
+{
+    await sqlConnection.CloseAsync();
+}
