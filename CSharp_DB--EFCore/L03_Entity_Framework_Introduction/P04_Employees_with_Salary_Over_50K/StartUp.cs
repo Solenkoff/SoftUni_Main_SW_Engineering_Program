08d6dfa42277c433a8faf1424dfe3d56d2e6ab1d
Solenkoff <solenkoff@yahoo.com>
2024-01-29 16:55:33 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L03_Entity_Framework_Introduction/P04_Employees_with_Salary_Over_50K/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace P04_Employees_with_Salary_Over_50K
+{
+    using SoftUni.Data;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)   
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            string result = string.Join(Environment.NewLine,
+                employees.Select(e => $"{e.FirstName} - {e.Salary:f2}"));
+
+            return result;
+        }
+    }
+}
