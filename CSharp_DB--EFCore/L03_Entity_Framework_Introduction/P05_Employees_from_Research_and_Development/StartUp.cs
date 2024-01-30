95da8abb33350349c6e85f857009ad294571b13e
Solenkoff <solenkoff@yahoo.com>
2024-01-30 10:35:25 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L03_Entity_Framework_Introduction/P05_Employees_from_Research_and_Development/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace P05_Employees_from_Research_and_Development
+{
+    using SoftUni.Data;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)    
+        {
+            var employees = context.Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    DepartmentName = e.Department.Name,
+                    e.Salary
+                })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToList();
+
+            string result = string.Join(Environment.NewLine,
+                employees.Select(e => $"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:f2}"));
+
+            return result;
+        }
+    }
+}
