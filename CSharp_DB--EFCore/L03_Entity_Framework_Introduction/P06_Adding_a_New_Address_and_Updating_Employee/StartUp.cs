5212c4a88d66e57ae140a9e5eebac5b56ca89dce
Solenkoff <solenkoff@yahoo.com>
2024-01-31 11:32:47 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L03_Entity_Framework_Introduction/P06_Adding_a_New_Address_and_Updating_Employee/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace P06_Adding_a_New_Address_and_Updating_Employee
+{
+    using SoftUni.Data;
+    using SoftUni.Models;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(AddNewAddressToEmployee(context));
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)    
+        {
+            Address address = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            var employee = context.Employees.FirstOrDefault(e => e.LastName == "Nakov");
+
+            employee.Address = address;
+
+            context.SaveChanges();
+
+            var employees = context.Employees
+                .Select(e => new {
+                    e.Address.AddressText,
+                    e.AddressId
+                })
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .ToList();
+
+            return string.Join(Environment.NewLine, employees.Select(e => $"{e.AddressText}"));
+        }
+    }
+}
