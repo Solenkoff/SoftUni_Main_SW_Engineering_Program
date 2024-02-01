c600e1b0191a2caa2252f7328e0583823d957713
Solenkoff <solenkoff@yahoo.com>
2024-02-01 12:03:38 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L03_Entity_Framework_Introduction/P08_Addresses_by_Town/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace P08_Addresses_by_Town
+{
+    using SoftUni.Data;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(GetAddressesByTown(context));
+        }
+
+        public static string GetAddressesByTown(SoftUniContext context)    
+        {
+            var addresses = context.Addresses
+                .Select(a => new
+                {
+                    a.AddressText,
+                    TownName = a.Town.Name,
+                    EmployeeCount = a.Employees.Count
+                })
+                .OrderByDescending(e => e.EmployeeCount)
+                .ThenBy(e => e.TownName)
+                .ThenBy(e => e.AddressText)
+                .Take(10)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, addresses
+                         .Select(a => $"{a.AddressText}, {a.TownName} - {a.EmployeeCount} employees"));
+        }
+
+    }
+}
