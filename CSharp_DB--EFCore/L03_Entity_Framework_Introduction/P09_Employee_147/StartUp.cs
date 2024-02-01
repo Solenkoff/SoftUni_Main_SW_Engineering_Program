f16d02db9668c715cbedf187de18394d6a4af1aa
Solenkoff <solenkoff@yahoo.com>
2024-02-01 16:14:00 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L03_Entity_Framework_Introduction/P09_Employee_147/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace P09_Employee_147
+{
+    using SoftUni.Data;
+    using System.Text;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(GetEmployee147(context));
+        }
+
+        public static string GetEmployee147(SoftUniContext context)    
+        {
+            var employee147 = context.Employees
+                .Where(e => e.EmployeeId == 147)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    Projects = e.EmployeesProjects.Select(p => new
+                    {
+                        ProjectName = p.Project.Name
+                    })
+                   .OrderBy(p => p.ProjectName)
+                   .ToArray()
+                }).ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{employee147[0].FirstName} {employee147[0].LastName} - {employee147[0].JobTitle}");
+
+            foreach (var project in employee147[0].Projects)
+            {
+                sb.AppendLine($"{project.ProjectName}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
