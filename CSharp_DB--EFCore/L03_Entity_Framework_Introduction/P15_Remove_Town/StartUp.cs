dbb2001bb523320629f3ce6d5deb9f01724dd31d
Solenkoff <solenkoff@yahoo.com>
2024-02-06 16:32:04 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L03_Entity_Framework_Introduction/P15_Remove_Town/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace P15_Remove_Town
+{
+    using Microsoft.EntityFrameworkCore;
+    using SoftUni.Data;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(RemoveTown(context));
+        }
+
+        public static string RemoveTown(SoftUniContext context)        
+        {
+            var town = context.Towns
+                .Include(t => t.Addresses)
+                .FirstOrDefault(t => t.Name == "Seattle");
+
+            int result = 0;
+
+            if (town != null)
+            {
+                var employees = context.Employees
+                       .Where(e => e.AddressId.HasValue && e.Address.TownId == town.TownId);
+
+                foreach (var emp in employees)
+                {
+                    emp.AddressId = null;
+                }
+
+                context.Addresses.RemoveRange(town.Addresses);
+                context.Towns.Remove(town);
+
+                context.SaveChanges();
+
+                result = town.Addresses.Count;
+            }
+
+            return $"{result} addresses in Seattle were deleted";
+
+        }
+    }
+}
