8c59160e15956fd031967ddd089efc7dcf7a7474
Solenkoff <solenkoff@yahoo.com>
2024-02-08 11:23:49 +0200

    Added folder and solutions

---
+++ CSharp_DB--EFCore/L04_Entity_Relations/P01_StudentSystem/P01_StudentSystem/Data/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace P01_StudentSystem.Data
+{
+
+    public class Config
+    {
+          public const string ConnectionString =
+             @"Server=.;Database=StudentSystem;User Id=sa;Password=;TrustServerCertificate=True;";
+        //                       !!!                              ^--  To be inserted(password)... !!!
+    }
+}
