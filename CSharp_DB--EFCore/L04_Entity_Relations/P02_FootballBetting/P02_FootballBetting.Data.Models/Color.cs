1eb84b7b87022b4d99b981ed88bcaf996899e8ea
Solenkoff <solenkoff@yahoo.com>
2024-02-09 13:22:45 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L04_Entity_Relations/P02_FootballBetting/P02_FootballBetting.Data.Models/Color.cs
@@ -0,0 +1,26 @@
+﻿namespace P02_FootballBetting.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+
+    public class Color
+    {
+        public int ColorId { get; set; }
+        [Required]
+        [MaxLength(Constants.ColorNameMaxLength)]
+        public string Name { get; set; }
+
+        [InverseProperty(nameof(Team.PrimaryKitColor))]
+        public virtual ICollection<Team> PrimaryKitTeams { get; set; }
+
+        [InverseProperty(nameof(Team.SecondaryKitColor))]
+        public virtual ICollection<Team> SecondaryKitTeams { get; set; }
+    }
+}
