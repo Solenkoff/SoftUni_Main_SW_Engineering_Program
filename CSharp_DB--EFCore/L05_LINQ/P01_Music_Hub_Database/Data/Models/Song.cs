8a8525850a644f8782c7534ff7e0097876f1d6b5
Solenkoff <solenkoff@yahoo.com>
2024-02-10 11:23:56 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L05_LINQ/P01_Music_Hub_Database/Data/Models/Song.cs
@@ -0,0 +1,50 @@
+﻿namespace P01_Music_Hub_Database.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using P01_Music_Hub_Database.Data.Models.Enums;
+
+    public class Song
+    {
+        public Song()
+        {
+            SongPerformers = new HashSet<SongPerformer>();
+        }
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public int? AlbumId { get; set; }
+        [ForeignKey(nameof(AlbumId))]
+        public virtual Album? Album { get; set; }
+
+        [Required]
+        public int WriterId { get; set; }
+        [ForeignKey(nameof(WriterId))]
+        public virtual Writer Writer { get; set; } = null!;
+
+        public virtual ICollection<SongPerformer> SongPerformers { get; set; }
+    }
+}
---
+++ CSharp_DB--EFCore/L05_LINQ/P01_Music_Hub_Database/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace P01_Music_Hub_Database
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context =
+               new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+        }
+    }
+}
