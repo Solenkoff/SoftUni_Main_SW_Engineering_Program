3a05b7ed37269d76ae993e8f840b620042b0f8b1
Solenkoff <solenkoff@yahoo.com>
2024-02-10 13:30:22 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L05_LINQ/P03_Songs_Above_Duration/Data/Models/Producer.cs
@@ -0,0 +1,25 @@
+﻿namespace P03_Songs_Above_Duration.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Producer
+    {
+        public Producer()
+        {
+            Albums = new HashSet<Album>();
+        }
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; } = null!;
+        public string? Pseudonym { get; set; }
+        public string? PhoneNumber { get; set; }
+
+        public virtual ICollection<Album> Albums { get; set; }
+
+    }
+}
---
+++ CSharp_DB--EFCore/L05_LINQ/P03_Songs_Above_Duration/Data/Models/Writer.cs
@@ -0,0 +1,24 @@
+﻿namespace P03_Songs_Above_Duration.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Writer
+    {
+        public Writer()
+        {
+            Songs = new HashSet<Song>();
+        }
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+        public string? Pseudonym { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+
+    }
+}
