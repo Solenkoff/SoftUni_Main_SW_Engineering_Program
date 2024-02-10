c8cb0be7744083765f516422afff16f4e1e5f20e
Solenkoff <solenkoff@yahoo.com>
2024-02-10 11:23:56 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L05_LINQ/P01_Music_Hub_Database/Data/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace P01_Music_Hub_Database.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+          @"Server=.;Database=MusicHub;User Id=sa;Password=;TrustServerCertificate=True;";
+        //                       !!!                      ^--  To be inserted(password)... !!!
+
+    }
+}
---
+++ CSharp_DB--EFCore/L05_LINQ/P01_Music_Hub_Database/Data/Models/Enums/Genre.cs
@@ -0,0 +1,11 @@
+﻿namespace P01_Music_Hub_Database.Data.Models.Enums
+{
+    public enum Genre
+    {
+        Blues,
+        Rap,
+        PopMusic,
+        Rock,
+        Jazz
+    }
+}
