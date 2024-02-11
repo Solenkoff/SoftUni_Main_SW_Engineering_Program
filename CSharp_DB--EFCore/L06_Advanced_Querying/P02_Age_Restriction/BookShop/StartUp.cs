3f715206b77643b159e0d7056f400d0ceaebd736
Solenkoff <solenkoff@yahoo.com>
2024-02-11 11:22:46 +0200

    Added folder and solutions

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P02_Age_Restriction/BookShop/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string commandInput = Console.ReadLine();
+            Console.WriteLine(GetBooksByAgeRestriction(db, commandInput));       // P02
+
+        }
+
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)  // P02
+        {
+            if (!Enum.TryParse<AgeRestriction>(command, true, out var ageRestriction))
+            {
+                return $"{command} is not a valid age restriction";
+            }
+
+            var books = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b => new
+                {
+                    b.Title
+                })
+                .OrderBy(b => b.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books.Select(b => b.Title));
+        }
+
+    }
+}
+
+
