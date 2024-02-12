7bcbd2c58255be972c043276ef4912f61531704f
Solenkoff <solenkoff@yahoo.com>
2024-02-12 10:27:19 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P03_Golden_Books/BookShop/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetGoldenBooks(db));                               // P03
+
+        }
+
+
+        public static string GetGoldenBooks(BookShopContext context)         // P03
+        {
+            var books = context.Books
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .Select(b => new
+                {
+                    b.BookId,
+                    b.Title
+                })
+                .OrderBy(b => b.BookId)
+                .ToList();
+
+
+            return string.Join(Environment.NewLine, books.Select(b => b.Title));
+        }
+    }
+}
+
+
