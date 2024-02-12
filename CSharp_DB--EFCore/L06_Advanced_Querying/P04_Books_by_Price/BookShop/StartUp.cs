00f9cc07ed5c7f9f7853375ca584ace5f928cd3d
Solenkoff <solenkoff@yahoo.com>
2024-02-12 12:00:50 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P04_Books_by_Price/BookShop/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBooksByPrice(db));                              // P04
+
+        }
+
+
+        public static string GetBooksByPrice(BookShopContext context)            // P04
+        {
+            var books = context.Books
+                            .Where(b => b.Price > 40)
+                            .Select(b => new
+                            {
+                                b.Title,
+                                b.Price
+                            })
+                            .OrderByDescending(b => b.Price)
+                            .ToList();
+
+            return string.Join(Environment.NewLine, books.Select(b => $"{b.Title} - ${b.Price:f2}"));
+        }
+    }
+}
+
+
