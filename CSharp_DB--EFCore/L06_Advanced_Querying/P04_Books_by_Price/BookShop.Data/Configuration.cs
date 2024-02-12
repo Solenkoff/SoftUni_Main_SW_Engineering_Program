4315e15b51fe87258af89c69fc3e7bee89243093
Solenkoff <solenkoff@yahoo.com>
2024-02-12 12:00:50 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P04_Books_by_Price/BookShop.Data/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => @"Server=.;Database=BookShop;User Id=sa;Password=;TrustServerCertificate=True;";
+        //                            !!!                      ^--  To be inserted(password)... !!!
+
+    }
+}
