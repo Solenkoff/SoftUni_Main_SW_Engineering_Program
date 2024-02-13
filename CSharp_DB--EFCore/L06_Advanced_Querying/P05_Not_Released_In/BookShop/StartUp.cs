e8df75e2078617256cf3ab7805ee25b8f15cfd2e
Solenkoff <solenkoff@yahoo.com>
2024-02-13 11:05:25 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P05_Not_Released_In/BookShop/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            int yearInput = int.Parse(Console.ReadLine());                       // P05
+            Console.WriteLine(GetBooksNotReleasedIn(db, yearInput));              
+
+        }
+
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)       // P05
+        {
+            var books = context.Books
+                            .Where(b => b.ReleaseDate.HasValue && b.ReleaseDate.Value.Year != year)
+                            .Select(b => new
+                            {
+                                b.Title
+                            })
+                            .ToList();
+
+            return string.Join(Environment.NewLine, books.Select(b => b.Title));
+        }
+
+    }
+}
+
+
