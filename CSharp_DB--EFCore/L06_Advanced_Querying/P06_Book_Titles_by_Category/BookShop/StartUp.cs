794bf6c26a616d52a1adb616e988b84beb0cf412
Solenkoff <solenkoff@yahoo.com>
2024-02-13 14:14:53 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P06_Book_Titles_by_Category/BookShop/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();                                   // P06
+            Console.WriteLine(GetBooksByCategory(db, input));
+
+        }
+
+
+        public static string GetBooksByCategory(BookShopContext context, string input)     // P06
+        {
+            string[] categories = input
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.ToLower())
+                .ToArray();
+
+            var books = context.Books
+                .Select(b => new
+                {
+                    b.Title,
+                    b.BookCategories
+                })
+                .Where(b => b.BookCategories.All(bc => categories.Contains(bc.Category.Name.ToLower())))
+                .OrderBy(b => b.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books.Select(b => b.Title));
+        }
+
+    }
+}
+
+
