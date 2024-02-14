d22821a303e0abb11745769028c531cb1cf5e3d0
Solenkoff <solenkoff@yahoo.com>
2024-02-14 12:29:59 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P08_Author_Search/BookShop/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string endingString = Console.ReadLine();                            // P08
+            Console.WriteLine(GetAuthorNamesEndingIn(db, endingString));
+
+        }
+
+
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string input)   // P08
+        {
+            var authors = context.Authors
+                .Where(a => a.FirstName.EndsWith(input))
+                .Select(a => new
+                {
+                    AuthorFullName = a.FirstName + " " + a.LastName
+                })
+                .OrderBy(a => a.AuthorFullName)
+                .ToList();
+
+            return string.Join(Environment.NewLine, authors.Select(a => a.AuthorFullName));
+        }
+
+    }
+}
+
+
