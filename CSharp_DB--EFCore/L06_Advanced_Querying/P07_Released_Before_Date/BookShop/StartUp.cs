de9481f3a2abeba4114f832729130bf980fe083a
Solenkoff <solenkoff@yahoo.com>
2024-02-14 11:21:32 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P07_Released_Before_Date/BookShop/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string date = Console.ReadLine();                                    // P07
+            Console.WriteLine(GetBooksReleasedBefore(db, date));
+
+        }
+
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)   // P07
+        {
+            DateTime givenDate = DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            var books = context.Books
+                        .Where(b => b.ReleaseDate < givenDate)
+                        .Select(b => new
+                        {
+                            b.Title,
+                            b.EditionType,
+                            b.Price,
+                            b.ReleaseDate
+                        })
+                        .OrderByDescending(b => b.ReleaseDate)
+                        .ToList();
+
+            return string.Join(Environment.NewLine,
+                books.Select(b => $"{b.Title} - {b.EditionType} - ${b.Price:f2}"));
+        }
+
+    }
+}
+
+
