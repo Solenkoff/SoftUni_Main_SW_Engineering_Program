caf4769b81dd39f204164dfe1ac63dad2eaf06e5
Solenkoff <solenkoff@yahoo.com>
2024-02-14 16:46:50 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P10_Book_Search_by_Author/BookShop/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string subString = Console.ReadLine();                               // P10
+            Console.WriteLine(GetBooksByAuthor(db, subString));
+
+        }
+
+
+        public static string GetBooksByAuthor(BookShopContext context, string subString)    // P10
+        {
+            var books = context.Books
+                .Include(b => b.Author)
+                .Where(b => b.Author.LastName.ToLower().StartsWith(subString.ToLower()))
+                .Select(b => new
+                {
+                    b.Title,
+                    AuthorName = b.Author.FirstName + " " + b.Author.LastName
+                })
+                //.OrderBy(b => b.)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books.Select(b => $"{b.Title} ({b.AuthorName})"));
+        }
+
+    }
+}
+
+
