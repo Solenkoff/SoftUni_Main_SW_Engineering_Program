22bd1390deb856d9d277d80abf5bb93deefcec06
Solenkoff <solenkoff@yahoo.com>
2024-02-15 15:59:13 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P11_Count_Books/BookShop/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            int lengthCheck = int.Parse(Console.ReadLine());                     // P11
+            Console.WriteLine(CountBooks(db, lengthCheck)); 
+
+        }
+
+
+        public static int CountBooks(BookShopContext context, int lengthCheck)       // P11
+        {
+            return context.Books.Count(b => b.Title.Length > lengthCheck);
+        }
+
+    }
+}
+
+
