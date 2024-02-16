d496f0842125820754435a1a1311371a777d3673
Solenkoff <solenkoff@yahoo.com>
2024-02-16 13:39:57 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P12_Total_Book_Copies/BookShop/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(CountCopiesByAuthor(db));                          // P12
+
+        }
+
+
+        public static string CountCopiesByAuthor(BookShopContext context)          // P12
+        {
+            var result = context.Authors
+                .Include(a => a.Books)
+                .Select(a => new
+                {
+                    AuthorFullName = string.Join(" ", a.FirstName, a.LastName),
+                    CountOfBookCopies = a.Books.Sum(b => b.Copies)
+                })
+                .OrderByDescending(a => a.CountOfBookCopies)
+                .ToList();
+
+            return string.Join(Environment.NewLine, result.Select(a => $"{a.AuthorFullName} - {a.CountOfBookCopies}"));
+        }
+
+    }
+}
+
+
