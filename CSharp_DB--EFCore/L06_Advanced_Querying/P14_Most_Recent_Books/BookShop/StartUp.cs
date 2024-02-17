42c3c49dee22f30f87893fb48978b801474daab5
Solenkoff <solenkoff@yahoo.com>
2024-02-17 11:26:24 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P14_Most_Recent_Books/BookShop/StartUp.cs
@@ -0,0 +1,62 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetMostRecentBooks(db));                           // P14
+
+        }
+
+
+        public static string GetMostRecentBooks(BookShopContext context)         // P14
+        {
+            var categoriesWithBooks = context.Categories
+               //.Include(c => c.CategoryBooks)
+               //    .ThenInclude(cb => cb.Book)
+               .Select(c => new
+               {
+                   CategoryName = c.Name,
+                   MostRecentBooks = c.CategoryBooks
+                      .OrderByDescending(cb => cb.Book.ReleaseDate)
+                      .Take(3)
+                      .Select(cb => new
+                      {
+                          BookTitle = cb.Book.Title,
+                          BookReleaseYear = cb.Book.ReleaseDate.Value.Year
+                      })
+                      .AsEnumerable()
+               })
+               .OrderBy(c => c.CategoryName)
+               .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var category in categoriesWithBooks)
+            {
+                sb.AppendLine($"--{category.CategoryName}");
+
+                foreach (var book in category.MostRecentBooks)
+                {
+                    sb.AppendLine($"{book.BookTitle} ({book.BookReleaseYear})");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
+
+
