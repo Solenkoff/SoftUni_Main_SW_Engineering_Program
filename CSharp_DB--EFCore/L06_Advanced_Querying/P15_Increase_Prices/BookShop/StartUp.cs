7b196b4b272e42a673a3a11ef91f87da78fc42f9
Solenkoff <solenkoff@yahoo.com>
2024-02-18 10:32:02 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L06_Advanced_Querying/P15_Increase_Prices/BookShop/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            IncreasePrices(db);                                                  // P15
+
+        }
+
+
+        public static void IncreasePrices(BookShopContext context)        // P15
+        {
+            var books = context.Books
+                .Where(b => b.ReleaseDate.HasValue && b.ReleaseDate.Value.Year < 2010).ToList();
+
+            foreach (var book in books)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+
+    }
+}
+
+
