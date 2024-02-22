22c5d8c41cf41ae7890ef60cbc605a5fcc589df8
Solenkoff <solenkoff@yahoo.com>
2024-02-22 11:26:11 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L08_JSON_Processing/P02_Import_Products/ProductShop/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace ProductShop
+{
+    using Newtonsoft.Json;
+    using ProductShop.Data;
+    using ProductShop.Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            string productsJson = File.ReadAllText("../../../Datasets/products.json");      // P02
+            Console.WriteLine(ImportProducts(context, productsJson));
+
+        }
+
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)    // P02
+        {
+            var products = JsonConvert.DeserializeObject<Product[]>(inputJson);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Length}";
+        }
+
+    }
+}
