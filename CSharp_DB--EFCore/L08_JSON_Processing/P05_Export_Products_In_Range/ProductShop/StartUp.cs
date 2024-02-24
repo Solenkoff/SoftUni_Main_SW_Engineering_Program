dbc7103a35dc115bc9fb2a61331463bffebd6987
Solenkoff <solenkoff@yahoo.com>
2024-02-24 21:03:24 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L08_JSON_Processing/P05_Export_Products_In_Range/ProductShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace ProductShop
+{
+    using Newtonsoft.Json;
+    using ProductShop.Data;
+    using ProductShop.Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            Console.WriteLine(GetProductsInRange(context));                         // P05 
+
+        }
+
+
+        public static string GetProductsInRange(ProductShopContext context)        // P05
+        {
+            var productsInRange = context.Products
+                .Where(p => p.Price >= 500 && p.Price <= 1000)
+                .Select(p => new
+                {
+                    name = p.Name,
+                    price = p.Price,
+                    seller = p.Seller.FirstName + " " + p.Seller.LastName
+                })
+                .OrderBy(p => p.price)
+                .ToArray();
+
+            var json = JsonConvert.SerializeObject(productsInRange, Formatting.Indented);
+            return json;
+        }
+
+    }
+}
