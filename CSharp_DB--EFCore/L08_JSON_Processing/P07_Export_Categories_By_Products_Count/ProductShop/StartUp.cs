22f73d9405081f2309a932e473062e6e01c201a3
Solenkoff <solenkoff@yahoo.com>
2024-02-25 19:17:57 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L08_JSON_Processing/P07_Export_Categories_By_Products_Count/ProductShop/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace ProductShop
+{
+    using Newtonsoft.Json;
+    using ProductShop.Data;
+    using ProductShop.Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            Console.WriteLine(GetCategoriesByProductsCount(context));                       // P07
+
+        }
+
+        public static string GetCategoriesByProductsCount(ProductShopContext context)     // P07
+        {
+            var categories = context.Categories
+                .Select(c => new
+                {
+                    category = c.Name,
+                    productsCount = c.CategoriesProducts.Count,
+                    averagePrice = c.CategoriesProducts
+                                  .Average(cp => cp.Product.Price).ToString("f2"),
+                    totalRevenue = c.CategoriesProducts
+                                  .Sum(cp => cp.Product.Price).ToString("f2")
+                })
+                .OrderByDescending(c => c.productsCount)
+                .ToArray();
+
+            var json = JsonConvert.SerializeObject(categories, Formatting.Indented);
+            return json;
+        }
+
+    }
+}
