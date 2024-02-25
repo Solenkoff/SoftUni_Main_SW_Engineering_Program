462fd1a15d43c935136ce683db5e1605c4c8d59b
Solenkoff <solenkoff@yahoo.com>
2024-02-25 14:45:30 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L08_JSON_Processing/P06_Export_Sold_Products/ProductShop/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace ProductShop
+{
+    using Newtonsoft.Json;
+    using ProductShop.Data;
+    using ProductShop.Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            Console.WriteLine(GetSoldProducts(context));                                    // P06
+
+        }
+
+
+        public static string GetSoldProducts(ProductShopContext context)         // P06
+        {
+            var users = context.Users
+                .Where(u => u.ProductsSold.Count > 0 && u.ProductsSold.Any(p => p.BuyerId != null))
+                .Select(u => new
+                {
+                    firstName = u.FirstName,
+                    lastName = u.LastName,
+                    soldProducts = u.ProductsSold
+                        .Where(ps => ps.BuyerId != null)
+                        .Select(ps => new
+                        {
+                            name = ps.Name,
+                            price = ps.Price,
+                            buyerFirstName = ps.Buyer!.FirstName,
+                            buyerLastName = ps.Buyer.LastName
+                        })
+                        .ToArray()
+                })
+                .OrderBy(u => u.lastName)
+                .ThenBy(u => u.firstName)
+                .ToArray();
+
+            var json = JsonConvert.SerializeObject(users, Formatting.Indented);
+            return json;
+        }
+
+    }
+}
