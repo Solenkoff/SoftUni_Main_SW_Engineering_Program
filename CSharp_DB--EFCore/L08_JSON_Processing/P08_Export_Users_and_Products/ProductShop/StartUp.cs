8ba016844c8fa6fcad5c3eab1befbc12bbc8cac4
Solenkoff <solenkoff@yahoo.com>
2024-02-26 17:18:28 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L08_JSON_Processing/P08_Export_Users_and_Products/ProductShop/StartUp.cs
@@ -0,0 +1,65 @@
+﻿namespace ProductShop
+{
+    using Newtonsoft.Json;
+    using ProductShop.Data;
+    using ProductShop.Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            Console.WriteLine(GetUsersWithProducts(context));                               // P08
+
+        }
+
+
+        public static string GetUsersWithProducts(ProductShopContext context)        // P08
+        {
+            var users = context.Users
+                .Where(u => u.ProductsSold.Any(p => p.BuyerId != null))
+                .Select(u => new
+                {
+                    firstName = u.FirstName,
+                    lastName = u.LastName,
+                    age = u.Age,
+                    soldProducts = u.ProductsSold
+                        .Where(ps => ps.BuyerId != null)
+                        .Select(ps => new
+                        {
+                            name = ps.Name,
+                            price = ps.Price
+                        })
+                        .ToArray()
+                })
+                .OrderByDescending(u => u.soldProducts.Count())
+                .ToArray();
+
+            var result = new
+            {
+                usersCount = users.Count(),
+                users = users.Select(u => new
+                {
+                    u.firstName,
+                    u.lastName,
+                    u.age,
+                    soldProducts = new
+                    {
+                        count = u.soldProducts.Count(),
+                        products = u.soldProducts
+                    }
+                })
+            };
+
+            var json = JsonConvert.SerializeObject(result, new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore
+            });
+
+            return json;
+        }
+
+    }
+}
