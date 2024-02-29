7a0c25250f52ac1aca6ba661f879f694aa07284e
Solenkoff <solenkoff@yahoo.com>
2024-02-29 13:11:28 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L08_JSON_Processing/P15_Export_Cars_From_Make_Toyota/CarDealer/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using CarDealer.Data;
+    using CarDealer.DTOs.Import;
+    using CarDealer.Models;
+    using Castle.Core.Resource;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.IO;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetCarsFromMakeToyota(context));                         // P15
+
+        }
+
+
+        public static string GetCarsFromMakeToyota(CarDealerContext context)          // P15
+        {
+            var toyotaCars = context.Cars
+                .Where(c => c.Make == "Toyota")
+                .Select(c => new
+                {
+                    c.Id,
+                    c.Make,
+                    c.Model,
+                    c.TraveledDistance
+                })
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TraveledDistance)
+                .ToArray();
+
+            var toyotasJson = JsonConvert.SerializeObject(toyotaCars, Formatting.Indented);
+
+            return toyotasJson;
+        }
+
+    }
+}
