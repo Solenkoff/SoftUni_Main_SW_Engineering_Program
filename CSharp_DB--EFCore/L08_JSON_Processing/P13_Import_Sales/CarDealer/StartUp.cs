81af937bb41ea694d53d39878b6c1f1fe5f4b779
Solenkoff <solenkoff@yahoo.com>
2024-02-29 10:57:29 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L08_JSON_Processing/P13_Import_Sales/CarDealer/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using CarDealer.Data;
+    using CarDealer.DTOs.Import;
+    using CarDealer.Models;
+    using Castle.Core.Resource;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.IO;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string salesJson = File.ReadAllText("../../../Datasets/sales.json");           // P13
+            Console.WriteLine(ImportSales(context, salesJson));
+
+        }
+
+
+        public static string ImportSales(CarDealerContext context, string inputJson)    // P13
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+            IMapper mapper = new Mapper(config);
+
+            SaleDTO[] saleDTOs = JsonConvert.DeserializeObject<SaleDTO[]>(inputJson);
+            Sale[] sales = mapper.Map<Sale[]>(saleDTOs);
+
+            context.Sales.AddRange(sales);
+            context.SaveChanges();
+
+            return $"Successfully imported {sales.Count()}.";
+
+        }
+
+    }
+}
