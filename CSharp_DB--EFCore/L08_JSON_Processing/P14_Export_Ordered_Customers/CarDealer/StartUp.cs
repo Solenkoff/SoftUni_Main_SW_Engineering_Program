06a3f152903548cb90b11bef905be9e2e3072bfa
Solenkoff <solenkoff@yahoo.com>
2024-02-29 11:52:57 +0200

    Added problem solution

---
+++ CSharp_DB--EFCore/L08_JSON_Processing/P14_Export_Ordered_Customers/CarDealer/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using CarDealer.Data;
+    using CarDealer.DTOs.Import;
+    using CarDealer.Models;
+    using Castle.Core.Resource;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.IO;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetOrderedCustomers(context));                               // P14
+
+        }
+
+
+        public static string GetOrderedCustomers(CarDealerContext context)       // P14
+        {
+            var customers = context.Customers
+                .OrderBy(c => c.BirthDate)
+                .ThenBy(c => c.IsYoungDriver)
+                .Select(c => new
+                {
+                    c.Name,
+                    BirthDate = c.BirthDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    c.IsYoungDriver
+                })
+                .ToArray();
+
+            var customersJson = JsonConvert.SerializeObject(customers, Formatting.Indented);
+
+            return customersJson;
+        }
+
+    }
+}
