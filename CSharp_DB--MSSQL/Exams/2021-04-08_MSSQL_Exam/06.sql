ac4053061dd2edcd7dd18089f0459d9b416d1c79
Solenkoff <solenkoff@yahoo.com>
2024-03-02 19:25:27 +0200

    Add solution

---
+++ CSharp_DB--MSSQL/Exams/2021-04-08_MSSQL_Exam/06.sql
@@ -0,0 +1,7 @@
+SELECT r.[Description]
+     , c.[Name] AS [CategoryName]
+      FROM [Reports] AS r
+      JOIN [Categories] AS c ON r.[CategoryId] = c.[Id]
+     WHERE [CategoryId] IS NOT NULL
+ORDER BY r.[Description], [CategoryName]
+
