e03aac9add9fca8cab40bd303dcad0a01e809838
Solenkoff <solenkoff@yahoo.com>
2024-03-12 16:14:13 +0200

    Added problem solution

---
+++ JS_Advanced/L01_Syntax_Functions_and_Statements_Lab/P10_Aggregate_Elements.js
@@ -0,0 +1,19 @@
+function aggregateElements(arr) {
+    let sum = 0;
+    let inverseSum = 0;
+    let concat = '';
+
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i];
+        sum += element;
+        inverseSum += 1 / element;
+        concat += element;
+    }
+
+    console.log(sum);
+    console.log(inverseSum);
+    console.log(concat);
+}
+
+aggregateElements([1, 2, 3]);
+aggregateElements([2, 4, 8, 16]);
