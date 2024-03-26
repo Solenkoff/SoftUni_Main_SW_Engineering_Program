8bfd1de9aba88dd8e98c6c8606736706b44a846b
Solenkoff <solenkoff@yahoo.com>
2024-03-26 10:40:01 +0200

    Added folder and solutions

---
+++ JS_Advanced/L04_Arrays_and_Nested_Arrays_Exercise/P01_Print_an_Array_with_a_Given_Delimiter.js
@@ -0,0 +1,13 @@
+function printWithDelimiter(arr, delimiter){
+    console.log(arr.join(delimiter));
+}
+
+printWithDelimiter(
+['One', 'Two', 'Three', 'Four', 'Five'], 
+'-'
+);
+
+printWithDelimiter(
+['How about no?', 'I','will', 'not', 'do', 'it!'], 
+'_'
+);
