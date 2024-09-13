114a8497ca36607ad8e9cdd7016e1f4e0af562e4
Solenkoff <solenkoff@yahoo.com>
2024-09-13 17:07:45 +0300

    Added problem solution

---
+++ CSharp_DB--EFCore/L08_JSON_Processing/P16_Export_Local_Suppliers/CarDealer/DTOs/Import/SupplierDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace CarDealer.DTOs.Import
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SupplierDTO
+    { 
+        public string Name { get; set; }
+        public bool IsImporter { get; set; }
+    }
+}
