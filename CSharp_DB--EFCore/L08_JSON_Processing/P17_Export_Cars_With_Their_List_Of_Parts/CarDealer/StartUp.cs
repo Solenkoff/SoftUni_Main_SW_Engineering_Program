84bcc0625a55bab6257b89c86167feeffca989e6
Solenkoff <solenkoff@yahoo.com>
2024-09-14 13:11:38 +0300

    Added problem solution

---
+++ CSharp_DB--EFCore/L08_JSON_Processing/P17_Export_Cars_With_Their_List_Of_Parts/CarDealer/StartUp.cs
@@ -0,0 +1,61 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using CarDealer.Data;
+    using CarDealer.DTOs.Import;
+    using CarDealer.Models;
+    using Castle.Core.Resource;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.IO;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetCarsWithTheirListOfParts(context));                   // P17
+
+        }
+
+
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)     // P17
+        {
+            var cars = context.Cars
+                .Select(c => new
+                {
+                    c.Make,
+                    c.Model,
+                    c.TraveledDistance,
+                    Parts = c.PartsCars
+                        .Select(pc => new
+                        {
+                            Name = pc.Part.Name,
+                            Price = pc.Part.Price.ToString("f2")
+                        })
+                        .ToArray()
+                })
+                .ToArray();
+
+            var formattedCars = cars
+                .Select(c => new
+                {
+                    car = new
+                    {
+                        c.Make,
+                        c.Model,
+                        c.TraveledDistance
+                    },
+                    parts = c.Parts
+                })
+                .ToArray();
+
+            var carsJson = JsonConvert.SerializeObject(formattedCars, Formatting.Indented);
+
+            return carsJson;
+        }
+
+    }
+}
