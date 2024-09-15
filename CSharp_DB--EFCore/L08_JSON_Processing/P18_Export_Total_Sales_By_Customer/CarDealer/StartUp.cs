de6135f4aa268902541a1b23c31f84b4257838da
Solenkoff <solenkoff@yahoo.com>
2024-09-15 11:18:49 +0300

    Added problem solution

---
+++ CSharp_DB--EFCore/L08_JSON_Processing/P18_Export_Total_Sales_By_Customer/CarDealer/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using CarDealer.Data;
+    using CarDealer.DTOs.Import;
+    using CarDealer.Models;
+    using Castle.Core.Resource;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.IO;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetTotalSalesByCustomer(context));                       // P18
+
+        }
+
+
+        public static string GetTotalSalesByCustomer(CarDealerContext context)        // P18
+        {
+            var customerData = context.Customers
+                .Where(c => c.Sales.Count > 0)
+                .Select(c => new
+                {
+                    fullName = c.Name,
+                    boughtCars = c.Sales.Count,
+                    spentMoney = c.Sales.SelectMany(s => s.Car.PartsCars).Sum(pc => pc.Part.Price)
+                })
+                .OrderByDescending(c => c.spentMoney)
+                .ThenByDescending(c => c.boughtCars)
+                .ToArray();
+
+            var customerDataJson = JsonConvert.SerializeObject(customerData, Formatting.Indented);
+
+            return customerDataJson;
+        }
+
+    }
+}
