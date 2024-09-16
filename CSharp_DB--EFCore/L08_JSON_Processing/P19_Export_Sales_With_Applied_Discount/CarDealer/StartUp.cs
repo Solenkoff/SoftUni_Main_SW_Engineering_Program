f5dc3e53cca54f3c799dbcdd54ca473c6f497f26
Solenkoff <solenkoff@yahoo.com>
2024-09-16 12:23:22 +0300

    Added problem solution

---
+++ CSharp_DB--EFCore/L08_JSON_Processing/P19_Export_Sales_With_Applied_Discount/CarDealer/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using CarDealer.Data;
+    using CarDealer.DTOs.Import;
+    using CarDealer.Models;
+    using Castle.Core.Resource;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.IO;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetSalesWithAppliedDiscount(context));                    // P19
+
+        }
+
+
+        public static string GetSalesWithAppliedDiscount(CarDealerContext context)     // P19
+        {
+            var sales = context.Sales
+                .Select(s => new
+                {
+                    car = new
+                    {
+                        s.Car.Make,
+                        s.Car.Model,
+                        s.Car.TraveledDistance
+                    },
+                    customerName = s.Customer.Name,
+                    discount = s.Discount.ToString("f2"),
+                    price = s.Car.PartsCars.Sum(pc => pc.Part.Price).ToString("f2"),
+                    priceWithDiscount = (s.Car.PartsCars
+                            .Sum(pc => pc.Part.Price) * (100 - s.Discount) / 100).ToString("f2")
+                })
+                .Take(10)
+                .ToArray();
+
+            var salesJson = JsonConvert.SerializeObject(sales, Formatting.Indented);
+
+            return salesJson;
+        }
+
+    }
+}
