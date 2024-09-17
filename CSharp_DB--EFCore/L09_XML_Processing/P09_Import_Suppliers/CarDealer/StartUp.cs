dad8166f6393a8651f03484781f224dbf3253c88
Solenkoff <solenkoff@yahoo.com>
2024-09-17 19:38:39 +0300

    Added folder and solutions

---
+++ CSharp_DB--EFCore/L09_XML_Processing/P09_Import_Suppliers/CarDealer/StartUp.cs
@@ -0,0 +1,51 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using CarDealer.Data;
+    using CarDealer.DTOs.Export;
+    using CarDealer.DTOs.Import;
+    using CarDealer.Models;
+    using Microsoft.EntityFrameworkCore;
+    using System.Globalization;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+            CarDealerContext context = new CarDealerContext();
+
+            var inputSupplierXml = File.ReadAllText("../../../Datasets/suppliers.xml");    // P09
+            Console.WriteLine(ImportSuppliers(context, inputSupplierXml));
+
+        }
+
+        private static Mapper GetMapper()
+        {
+            var cfg = new MapperConfiguration(c => c.AddProfile<CarDealerProfile>());
+            return new Mapper(cfg);
+        }
+
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)     // P09
+        {
+            XmlSerializer xmlSerializer =
+                new XmlSerializer(typeof(ImportSupplierDto[]), new XmlRootAttribute("Suppliers"));
+
+            using var reader = new StringReader(inputXml);
+            ImportSupplierDto[] importSupplierDtos = (ImportSupplierDto[])xmlSerializer.Deserialize(reader);
+
+            var mapper = GetMapper();
+            Supplier[] suppliers = mapper.Map<Supplier[]>(importSupplierDtos);
+
+            context.AddRange(suppliers);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
+        }
+    }
+}
