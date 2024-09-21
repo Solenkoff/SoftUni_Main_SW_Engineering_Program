b654fc3b9c6a4108cfd13994652567e34dff4b47
Solenkoff <solenkoff@yahoo.com>
2024-09-21 17:30:41 +0300

    Added folder and solutions

---
+++ ASP.NET_Fundamentals/L04_ASP.NET_Core_Introduction_Exercise/WebShopDemo/WebShopDemo.Api/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿namespace WebShopDemo.Api.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using WebShopDemo.Core.Contracts;
+    using WebShopDemo.Core.Models;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService _productService)
+        {
+            productService = _productService;
+        }
+
+        /// <summary>
+        ///  Get All products
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(200, StatusCode = StatusCodes.Status200OK, Type = typeof(IEnumerable<ProductDto>))]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await productService.GetAll());
+        }
+    }
+}
---
+++ ASP.NET_Fundamentals/L04_ASP.NET_Core_Introduction_Exercise/WebShopDemo/WebShopDemo.Core/Contracts/IProductService.cs
@@ -0,0 +1,18 @@
+﻿namespace WebShopDemo.Core.Contracts
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using WebShopDemo.Core.Models;
+
+    /// <summary>
+    ///  Manipulates peoduct data
+    /// </summary>
+    public interface IProductService
+    {
+        /// <summary>
+        ///  Gets all products
+        /// </summary>
+        /// <returns>List of products</returns>
+        Task<IEnumerable<ProductDto>> GetAll();
+    }
+}
