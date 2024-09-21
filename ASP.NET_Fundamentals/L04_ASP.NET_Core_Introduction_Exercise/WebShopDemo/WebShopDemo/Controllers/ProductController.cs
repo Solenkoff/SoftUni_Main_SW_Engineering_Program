661c30edf27e97800ccd3fb07d42f789c6a10b06
Solenkoff <solenkoff@yahoo.com>
2024-09-21 17:30:41 +0300

    Added folder and solutions

---
+++ ASP.NET_Fundamentals/L04_ASP.NET_Core_Introduction_Exercise/WebShopDemo/WebShopDemo/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿namespace WebShopDemo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using WebShopDemo.Core.Contracts;
+
+    /// <summary>
+    ///  Web Shop Products
+    /// </summary>
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService _productService)
+        {
+            productService = _productService;
+        }
+
+        /// <summary>
+        ///  List all products
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> Index()
+        {
+            var products = await productService.GetAll();
+            ViewData["Title"] = "Products";
+
+            return View(products);
+        }
+    }
+}
---
+++ ASP.NET_Fundamentals/L04_ASP.NET_Core_Introduction_Exercise/WebShopDemo/WebShopDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebShopDemo.Core.Models;
+namespace WebShopDemo.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebShopDemo.Core.Models.ProductDto> ProductDto { get; set; }
+    }
+}
