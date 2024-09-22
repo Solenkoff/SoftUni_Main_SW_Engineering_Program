70ecfb16745eaa9c7a9b56f0c346129d87bd6e4c
Solenkoff <solenkoff@yahoo.com>
2024-09-22 17:25:49 +0300

    Added problem solution

---
+++ CSharp_DB--EFCore/L09_XML_Processing/P11_Import_Cars/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Export;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<ImportSupplierDto, Supplier>();
+            CreateMap<ImportPartDto, Part>();
+            CreateMap<ImportCarDto, Car>();
+            CreateMap<ImportCustomerDto,  Customer>();  
+            CreateMap<ImportSaleDto, Sale>();
+            CreateMap<Car, ExportCarDistanceDto>();
+            CreateMap<Customer, ExportSalesByCustomerDto>();
+            CreateMap<Sale, ExportAllSalesDto>();
+            CreateMap<Car, ExportBMWCarDto>();
+            CreateMap<Supplier, ExportLocalSuppliersDto>();
+            CreateMap<Car, ExportAllCarsWithPartsDto>();
+        }
+    }
+}
