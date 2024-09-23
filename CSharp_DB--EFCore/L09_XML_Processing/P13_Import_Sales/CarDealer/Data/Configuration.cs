a3ccf79e5983300df0da08cc34f164a58b7e711c
Solenkoff <solenkoff@yahoo.com>
2024-09-23 16:35:52 +0300

    Added problem solution

---
+++ CSharp_DB--EFCore/L09_XML_Processing/P13_Import_Sales/CarDealer/Data/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace CarDealer.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+            @"Server=.;Database=CarDealer;User Id=sa;Password=;TrustServerCertificate=True;Encrypt=False";
+        //                            !!!                    ^--  To be inserted(password)... !!!
+
+    }
+}
