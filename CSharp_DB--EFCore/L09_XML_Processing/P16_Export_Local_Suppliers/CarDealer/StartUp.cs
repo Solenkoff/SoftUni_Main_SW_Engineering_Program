d88e38bf18848845a50a7b881c14b24e4d698b82
Solenkoff <solenkoff@yahoo.com>
2024-09-25 15:31:47 +0300

    Added problem solution

---
+++ CSharp_DB--EFCore/L09_XML_Processing/P16_Export_Local_Suppliers/CarDealer/StartUp.cs
@@ -0,0 +1,63 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using CarDealer.Data;
+    using CarDealer.DTOs.Export;
+    using CarDealer.DTOs.Import;
+    using CarDealer.Models;
+    using Microsoft.EntityFrameworkCore;
+    using System.Globalization;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetLocalSuppliers(context));                                 // P16
+
+        }
+
+        private static Mapper GetMapper()
+        {
+            var cfg = new MapperConfiguration(c => c.AddProfile<CarDealerProfile>());
+            return new Mapper(cfg);
+        }
+
+
+        public static string GetLocalSuppliers(CarDealerContext context)               // P16
+        {
+            var mapper = GetMapper();
+
+            var nonImporters = context.Suppliers
+                .Where(s => !s.IsImporter)
+                .Select(s => new ExportLocalSuppliersDto
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    PartsCount = s.Parts.Count
+                })
+                .ToArray();
+
+            XmlSerializer xmlSerializer =
+            new XmlSerializer(typeof(ExportLocalSuppliersDto[]), new XmlRootAttribute("suppliers"));
+
+            var nsXml = new XmlSerializerNamespaces();
+            nsXml.Add(string.Empty, string.Empty);
+
+            StringBuilder sb = new StringBuilder();
+
+            using (StringWriter sw = new StringWriter(sb))
+            {
+                xmlSerializer.Serialize(sw, nonImporters, nsXml);
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+    }
+}
