772d52ceec4040a684f790b9d28da384f8519290
Solenkoff <solenkoff@yahoo.com>
2024-09-25 11:47:06 +0300

    Added problem solution

---
+++ CSharp_DB--EFCore/L09_XML_Processing/P15_Export_Cars_From_Make_BMW/CarDealer/StartUp.cs
@@ -0,0 +1,65 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using CarDealer.Data;
+    using CarDealer.DTOs.Export;
+    using CarDealer.DTOs.Import;
+    using CarDealer.Models;
+    using Microsoft.EntityFrameworkCore;
+    using System.Globalization;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+            CarDealerContext context = new CarDealerContext();
+
+            Console.WriteLine(GetCarsFromMakeBmw(context));                                // P15
+
+        }
+
+        private static Mapper GetMapper()
+        {
+            var cfg = new MapperConfiguration(c => c.AddProfile<CarDealerProfile>());
+            return new Mapper(cfg);
+        }
+
+
+        public static string GetCarsFromMakeBmw(CarDealerContext context)          // P15
+        {
+            var mapper = GetMapper();
+
+            var carsBMW = context.Cars
+                .Where(c => c.Make == "BMW")
+                .Select(c => new ExportBMWCarDto
+                {
+                    Id = c.Id,
+                    Model = c.Model,
+                    TraveledDistance = c.TraveledDistance,
+                })
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TraveledDistance)
+                .ToArray();
+
+            XmlSerializer xmlSerializer =
+           new XmlSerializer(typeof(ExportBMWCarDto[]), new XmlRootAttribute("cars"));
+
+            var nsXml = new XmlSerializerNamespaces();
+            nsXml.Add(string.Empty, string.Empty);
+
+            StringBuilder sb = new StringBuilder();
+
+            using (StringWriter sw = new StringWriter(sb))
+            {
+                xmlSerializer.Serialize(sw, carsBMW, nsXml);
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+    }
+}
