57885499c370689fa03f73df4828b2f03f1be155
Solenkoff <solenkoff@yahoo.com>
2024-10-30 17:19:20 +0200

    Added folder with solutions

---
+++ CSharp_DB--EFCore/Exams/2021-04-04_EFCore_Exam/P01_Model Defition/Common/GlobalConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace TeisterMask.Common
+{
+    public class GlobalConstants
+    {
+
+        // Username  
+        public const int EMPLOYEE_USERNAME_MAX_LENGHT = 40;
+
+        // Phone
+        public const int EMPLOYEE_PHONE_MAX_LENGHT = 12;
+
+        // Project
+        public const int PROJECT_NAME_MAX_LENGHT = 40;
+
+        // Task
+        public const int TASK_NAME_MAX_LENGHT = 40;
+
+
+
+
+    }
+}
