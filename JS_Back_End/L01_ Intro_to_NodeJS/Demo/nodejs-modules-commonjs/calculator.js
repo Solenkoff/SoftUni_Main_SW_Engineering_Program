a8f291e01c05176d36eeef54d2356a3389f0fb49
Solenkoff <solenkoff@yahoo.com>
2025-01-08 13:38:58 +0200

    Add course demo file

---
+++ JS_Back_End/L01_ Intro_to_NodeJS/Demo/nodejs-modules-commonjs/calculator.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log('Something');
+
+// Default export
+module.exports = {
+    add,
+    multiply,
+    subtract,
+}
