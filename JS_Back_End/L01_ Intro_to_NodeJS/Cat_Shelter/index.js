333782e7dd901ebfd8d8662868b562b5c862bd76
Solenkoff <solenkoff@yahoo.com>
2025-01-09 12:12:15 +0200

    Add addBreed page and set simple routing

--- JS_Back_End/L01_ Intro_to_NodeJS/Cat_Shelter/index.js
+++ JS_Back_End/L01_ Intro_to_NodeJS/Cat_Shelter/index.js
@@ -2,6 +2,8 @@ import http from 'http';
 
 import siteCss from './content/styles/site.css.js';
 import homePage from './views/home/index.html.js';
+import addBreedPage from './views/addBreed.html.js';
+
 
 const server = http.createServer((req, res) => {
 
@@ -15,12 +17,23 @@ const server = http.createServer((req, res) => {
 
         return res.end();
     }
-    
+
     res.writeHead(200, {
         'content-type': 'text/html',
     })
 
-    res.write(homePage);
+    switch (req.url) {
+        case '/':
+            res.write(homePage);
+            break;
+        case '/cats/add-breed':
+            res.write(addBreedPage);
+            break;
+        default:
+            res.write('Page Not Found!');
+            break;
+    }
+
     res.end();
 });
 
