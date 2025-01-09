cd44293a25362bd0996f6f0788fe555685961d2b
Solenkoff <solenkoff@yahoo.com>
2025-01-09 12:06:57 +0200

    Set route to assets/css with fixing paths

--- JS_Back_End/L01_ Intro_to_NodeJS/Cat_Shelter/index.js
+++ JS_Back_End/L01_ Intro_to_NodeJS/Cat_Shelter/index.js
@@ -1,9 +1,21 @@
 import http from 'http';
 
+import siteCss from './content/styles/site.css.js';
 import homePage from './views/home/index.html.js';
 
 const server = http.createServer((req, res) => {
 
+    // Load assets
+    if (req.url === '/styles/site.css') {
+        res.writeHead(200, {
+            'content-type': 'text/css',
+        });
+
+        res.write(siteCss);
+
+        return res.end();
+    }
+    
     res.writeHead(200, {
         'content-type': 'text/html',
     })
