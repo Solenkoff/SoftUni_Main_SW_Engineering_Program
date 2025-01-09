7000f80f51fba9a09de4996b79ff22f26e6b4ea1
Solenkoff <solenkoff@yahoo.com>
2025-01-09 11:24:30 +0200

    Setting http server

---
+++ JS_Back_End/L01_ Intro_to_NodeJS/Cat_Shelter/index.js
@@ -0,0 +1,10 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.write('It works!');
+
+    res.end();
+});
+
+server.listen(5000);
+console.log('Server is listening on http://localhost:5000...');
