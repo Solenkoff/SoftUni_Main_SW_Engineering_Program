ac2d3293e4e1bc55ae6e0cce195fb4bfa72282e2
Solenkoff <solenkoff@yahoo.com>
2025-01-10 11:57:39 +0200

    Dynamic cats rendering

--- JS_Back_End/L01_ Intro_to_NodeJS/Cat_Shelter/index.js
+++ JS_Back_End/L01_ Intro_to_NodeJS/Cat_Shelter/index.js
@@ -5,6 +5,37 @@ import homePage from './views/home/index.html.js';
 import addBreedPage from './views/addBreed.html.js';
 import addCatPage from './views/addCat.html.js';
 
+const cats = [
+    {
+        id: 1,
+        imageUrl: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=',
+        name: 'Pretty Kitty',
+        breed: 'Bombay Cat',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+    },
+    {
+        id: 2,
+        imageUrl: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=',
+        name: 'Navcho',
+        breed: 'Persian Cat',
+        description: 'A talkative and affectionate cat with striking yellow eyes.',
+    },
+    {
+        id: 3,
+        imageUrl: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=',
+        name: 'Sisa',
+        breed: 'Siamese Cat',
+        description: 'Loves to cuddle and nap. Requires regular grooming for its luxurious coat.',
+    },
+    {
+        id: 4,
+        imageUrl: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=',
+        name: 'Garry',
+        breed: 'Bombay Cat',
+        description: 'Mysterious and elegant. Often found lounging in sunny spots.',
+    },
+];
+
 
 const server = http.createServer((req, res) => {
 
@@ -25,13 +56,13 @@ const server = http.createServer((req, res) => {
 
     switch (req.url) {
         case '/':
-            res.write(homePage);
+            res.write(homePage(cats));
             break;
         case '/cats/add-breed':
-            res.write(addBreedPage);
+            res.write(addBreedPage());
             break;
         case '/cats/add-cat':
-            res.write(addCatPage);
+            res.write(addCatPage());
             break;
         default:
             res.write('Page Not Found!');
