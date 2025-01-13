4be3a08d27277843fb341f70b70b027a7d667024
Solenkoff <solenkoff@yahoo.com>
2025-01-13 14:25:05 +0200

    Add lab demo

---
+++ JS_Back_End/L02_NodeJS_Streams_and_Utilities/Demo/events/audit-system.js
@@ -0,0 +1,14 @@
+import messageBroker from "./message-broker.js"
+
+messageBroker.on('userCreated', userCreated);
+messageBroker.on('userDeleted', userDeleted);
+
+function userCreated(username) {
+    // TODO: actual audit
+
+    console.log(`Audit System: ${username}`);
+}
+
+function userDeleted(username)  {
+    console.log(`Audit System (user deleted): ${username}`);
+}
---
+++ JS_Back_End/L02_NodeJS_Streams_and_Utilities/Demo/events/backoffice-system.js
@@ -0,0 +1,27 @@
+import http from 'http';
+
+import messageBroker from './message-broker.js';
+import './audit-system.js';
+import './reporting-service.js';
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Home page');
+    } else if (req.url === '/create-user') {
+        // TODO: actual user creation
+
+        messageBroker.emit('userCreated', 'Pesho');
+
+        res.write('User created');
+    } else if (req.url === '/delete-user') {
+        messageBroker.emit('userDeleted', 'Ivan');
+    } else {
+        res.write('Not Found');
+    }
+
+    res.end();
+});
+
+
+server.listen(5000);
+console.log('Server is listening on http://localhost:5000...');
