bc6076f7d64881b940b369107a047095f6a4fddb
Solenkoff <solenkoff@yahoo.com>
2025-01-16 15:24:15 +0200

    Add and set up Handlebars

--- JS_Back_End/L04_Workshop_Part_01-ExpressJS_and_Templating/Movie_Magic_Part_01/src/index.js
+++ JS_Back_End/L04_Workshop_Part_01-ExpressJS_and_Templating/Movie_Magic_Part_01/src/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
+
 
 const app = express();
 
-app.get('/', ( req, res ) => {
-    res.send('It works!');
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+
+app.get ('/', ( req, res ) => {
+    res.render('home', { layoutal: false});
 });
+
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
