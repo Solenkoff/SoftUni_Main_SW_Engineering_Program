a6be92e007f05436452233f7b7b368124c5919c7
Solenkoff <solenkoff@yahoo.com>
2025-01-16 16:20:45 +0200

    Add main layout

--- JS_Back_End/L04_Workshop_Part_01-ExpressJS_and_Templating/Movie_Magic_Part_01/src/index.js
+++ JS_Back_End/L04_Workshop_Part_01-ExpressJS_and_Templating/Movie_Magic_Part_01/src/index.js
@@ -3,6 +3,7 @@ import handlebars from 'express-handlebars';
 
 
 const app = express();
+const port = 5000;
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
@@ -10,12 +11,13 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
-app.use('/static',express.static('src/public'));
- 
+app.use('/static', express.static('src/public'));
 
-app.get ('/', ( req, res ) => {
-    res.render('home', { layout: false});
+
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
 
-app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
+
+app.listen(port, () => console.log('Server is listening on http://localhost:5000...'));
