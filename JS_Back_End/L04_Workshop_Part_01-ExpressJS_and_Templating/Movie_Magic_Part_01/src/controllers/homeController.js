1fb4ee498ffee33b3ffe7a9acd18fd89b9ac6b71
Solenkoff <solenkoff@yahoo.com>
2025-01-19 16:22:47 +0200

    Add getAll to movieService

--- JS_Back_End/L04_Workshop_Part_01-ExpressJS_and_Templating/Movie_Magic_Part_01/src/controllers/homeController.js
+++ JS_Back_End/L04_Workshop_Part_01-ExpressJS_and_Templating/Movie_Magic_Part_01/src/controllers/homeController.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
-import movies from '../movies.js';
+import movieService from '../services/movieService.js';
 
 const router = Router();
 
 
 router.get('/', (req, res) => {
+    const movies = movieService.getAll();
     res.render('home', { movies });
 });
 
