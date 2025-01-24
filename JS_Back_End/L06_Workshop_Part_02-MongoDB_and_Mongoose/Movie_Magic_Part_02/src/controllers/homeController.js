3915db77e5afd84c44174674eca164ccc5e1ca45
Solenkoff <solenkoff@yahoo.com>
2025-01-24 10:56:20 +0200

    Refactor Search functionality to work with DB

--- JS_Back_End/L06_Workshop_Part_02-MongoDB_and_Mongoose/Movie_Magic_Part_02/src/controllers/homeController.js
+++ JS_Back_End/L06_Workshop_Part_02-MongoDB_and_Mongoose/Movie_Magic_Part_02/src/controllers/homeController.js
@@ -5,13 +5,13 @@ import movieService from '../services/movieService.js';
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const movies = await movieService.getAll();
-
     // #1 Convert documents to plain object
     // const plainMovies = movies.map(m => m.toObject());  
     // #2 with <lean> in movieService -> returns [{},{},..]
     // #3  with <lean> here (you decide where to lean -> turn it into [{},{},..])
     // * #4  as it is with  runtimeOptions: { allowProtoPropertiesByDefault: true } in index.js when setting up engine
+    
+    const movies = await movieService.getAll();
 
     res.render('home', { movies });
 });
