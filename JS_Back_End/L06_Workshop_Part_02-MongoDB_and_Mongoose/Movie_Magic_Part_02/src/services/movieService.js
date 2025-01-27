01d038dddca7658693389c20213f8f1cdd2785a2
Solenkoff <solenkoff@yahoo.com>
2025-01-27 11:16:51 +0200

    Modify update cast query

--- JS_Back_End/L06_Workshop_Part_02-MongoDB_and_Mongoose/Movie_Magic_Part_02/src/services/movieService.js
+++ JS_Back_End/L06_Workshop_Part_02-MongoDB_and_Mongoose/Movie_Magic_Part_02/src/services/movieService.js
@@ -2,55 +2,60 @@ import Movie from '../models/Movie.js';
 
 const movieService = {
 
-    getAll( filter = {} ){
+    getAll(filter = {}) {
         //  #2 from homeController -> <lean>
         // let result = await  Movie.find({}).lean();  // with async
 
-        let query =  Movie.find({}); 
+        let query = Movie.find({});
 
-        if(filter.search){
+        if (filter.search) {
             // TODO  fix partial search to be case sensitive
-            query = query.where({title: filter.search});
+            query = query.where({ title: filter.search });
         }
 
-        if(filter.genre){
+        if (filter.genre) {
             // TODO  fix partial search to be case sensitive
-            query = query.where({genre: filter.genre});
+            query = query.where({ genre: filter.genre });
         }
 
-        if(filter.year){
+        if (filter.year) {
             // TODO  fix partial search to be case sensitive
-            query = query.where({year: Number(filter.year)});
+            query = query.where({ year: Number(filter.year) });
         }
 
         return query;
     },
-    getOne(movieId){
+    getOne(movieId) {
         //  TODO: If no such movie...
         const result = Movie.findById(movieId);
 
         return result;
     },
-    getOneWithCasts(movieId){
+    getOneWithCasts(movieId) {
         return this.getOne(movieId).populate('casts');
     },
-    createMovie(movieData){
+    createMovie(movieData) {
         const newMoviePromise = Movie.create({
             ...movieData,
             year: Number(movieData.year),
             rating: Number(movieData.rating),
         });
 
-        return newMoviePromise; 
+        return newMoviePromise;
     },
-    async attachCast(movieId, castId){
-        //  #1  Attach
-        const movie = await Movie.findById(movieId);
-        movie.casts.push(castId); 
+    async attachCast(movieId, castId) {
+        // TODO: Check if castId has not been added already
 
-        await movie.save();
+        //  #1  Attach   ( Allows additional checks )
+        // const movie = await Movie.findById(movieId);
+        // movie.casts.push(castId); 
 
-        return movie;
+        // await movie.save();
+        // return movie;
+
+        //*  #2  Better Attach
+        return Movie.findByIdAndUpdate(movieId, { $push: { casts: castId } });
+
     }
 }
 
