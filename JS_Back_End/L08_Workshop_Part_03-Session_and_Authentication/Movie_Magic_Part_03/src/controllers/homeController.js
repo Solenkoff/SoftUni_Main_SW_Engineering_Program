d399d554e5d305655855d362cbe5eaed913ea077
Solenkoff <solenkoff@gmail.com>
2025-01-31 10:51:14 +0200

    Add new resources

---
+++ JS_Back_End/L08_Workshop_Part_03-Session_and_Authentication/Movie_Magic_Part_03/src/controllers/homeController.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+
+import movieService from '../services/movieService.js';
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    // #1 Convert documents to plain object
+    // const plainMovies = movies.map(m => m.toObject());  
+    // #2 with <lean> in movieService -> returns [{},{},..]
+    // #3  with <lean> here (you decide where to lean -> turn it into [{},{},..])
+    // * #4  as it is with  runtimeOptions: { allowProtoPropertiesByDefault: true } in index.js when setting up engine
+    
+    const movies = await movieService.getAll();
+
+    res.render('home', { movies });
+});
+
+router.get('/about', (req, res) => { 
+    res.render('about');
+});
+
+
+export default router;
---
+++ JS_Back_End/L08_Workshop_Part_03-Session_and_Authentication/Movie_Magic_Part_03/src/services/castService.js
@@ -0,0 +1,18 @@
+import Cast from "../models/Cast.js";
+
+export default {
+    getAll(filter = {}) {
+        let query = Cast.find({});
+        // return Cast.find({ _id: { $in: ids } });  -> REturning those casts that are present in the movie(ids)
+
+        if(filter.exclude){
+            //query = query.find({_id: {$nin: filter.exclude}});   //  with mongodb
+            query = query.nin('_id', filter.exclude);              //  with mongoose
+        }
+
+        return query;
+    },
+    create(castData) {
+        return Cast.create(castData);
+    },
+} 
