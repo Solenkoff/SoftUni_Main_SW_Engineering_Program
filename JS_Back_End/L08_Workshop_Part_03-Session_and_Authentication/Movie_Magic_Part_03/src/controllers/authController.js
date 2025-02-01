3d160a15f219909d79bb4cfa4adae983c9ad9a5f
Solenkoff <solenkoff@gmail.com>
2025-02-01 13:27:39 +0200

    Add login functionality with jwt generation

--- JS_Back_End/L08_Workshop_Part_03-Session_and_Authentication/Movie_Magic_Part_03/src/controllers/authController.js
+++ JS_Back_End/L08_Workshop_Part_03-Session_and_Authentication/Movie_Magic_Part_03/src/controllers/authController.js
@@ -8,7 +8,7 @@ authController.get('/register', (req, res) => {
     res.render('auth/register');
 })
 
-authController.post('/register', async (req, res ) => {
+authController.post('/register', async (req, res) => {
     const userData = req.body;
 
     await authService.register(userData);
@@ -19,7 +19,21 @@ authController.post('/register', async (req, res ) => {
 authController.get('/login', (req, res) => {
     res.render('auth/login');
 
-})
+});
+
+authController.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const token = await authService.login(email, password);
+        console.log(token);
+    } catch (err) {
+        console.log(err.message);
+        return res.redirect('/404');
+    }
+
+    res.redirect('/');
+});
 
 
 export default authController;
