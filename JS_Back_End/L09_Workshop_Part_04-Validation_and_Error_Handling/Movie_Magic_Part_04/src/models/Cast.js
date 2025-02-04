fb64ec4539848bfd2234cd7296506cf152c464f6
Solenkoff <solenkoff@gmail.com>
2025-02-04 13:50:59 +0200

    Add cast validations

--- JS_Back_End/L09_Workshop_Part_04-Validation_and_Error_Handling/Movie_Magic_Part_04/src/models/Cast.js
+++ JS_Back_End/L09_Workshop_Part_04-Validation_and_Error_Handling/Movie_Magic_Part_04/src/models/Cast.js
@@ -1,11 +1,33 @@
 import { Schema, model } from 'mongoose';
 
 const castSchema = new Schema({
-    name: String,
-    age: Number,
-    born: String,
-    imageUrl: String,
-});
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+        minLength: [5, 'Name should be at least 5 characters long!'],
+        match: [/^[a-zA-Z0-9]+$/, 'Name should be alphanumeric, digits and whitespaces only!'],
+    },
+    age: {
+        type: Number,
+        min: 1,
+        max: 120,
+    },
+    born: {
+        type: String,
+        minLength: 10,
+        match: /^[a-zA-Z0-9]+$/,
+    },
+    imageUrl: {
+        type: String,
+        validate: {         //  deeper validation for  ->   match: /^https?:\/\//
+            validator: function(valueToValidate) {
+                return /^https?:\/\//.test(valueToValidate);     
+            },
+            message: (props) => `${props.value} is invalid imageUrl!`
+        }
+        
+    },
+});    
 
 const Cast = model('Cast', castSchema);
 
