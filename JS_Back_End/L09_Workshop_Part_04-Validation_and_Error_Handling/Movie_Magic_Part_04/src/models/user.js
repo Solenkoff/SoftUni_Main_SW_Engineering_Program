d31588c395c338c5035a0f5cbc1a89fa40ca2d14
Solenkoff <solenkoff@gmail.com>
2025-02-04 13:51:57 +0200

    Add user validations

--- JS_Back_End/L09_Workshop_Part_04-Validation_and_Error_Handling/Movie_Magic_Part_04/src/models/user.js
+++ JS_Back_End/L09_Workshop_Part_04-Validation_and_Error_Handling/Movie_Magic_Part_04/src/models/user.js
@@ -2,12 +2,28 @@ import { Schema, model } from "mongoose";
 import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
-    email: String,
-    password: String,
+    email: {
+        type: String,
+        unique: true,
+        minLength: 10,
+        match: /\@[a-zA-Z0-9]+.[a-zA-Z0-9]+$/,
+    },
+    password:  {
+        type: String,
+        minLength: 6,
+        match: /^\w+$/,
+    },
 });
 
+userSchema.virtual('rePassword')
+    .set(function(rePassword) {
+        if(rePassword !== this.password){
+            throw new Error('Password missmatch!');
+        }
+    })
+  //.get()    getter if we need the rePassword somewhere...
+
 userSchema.pre('save', async function(){
-    // TODO: fix update user bug
     this.password = await bcrypt.hash(this.password, 10);  
 });
 
