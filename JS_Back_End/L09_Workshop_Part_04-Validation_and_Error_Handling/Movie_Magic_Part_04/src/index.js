78cc4d0721fc1cf40082ac5c47da9c7e880eb6bf
Solenkoff <solenkoff@gmail.com>
2025-02-05 15:23:40 +0200

    Add getErrorMessage util

--- JS_Back_End/L09_Workshop_Part_04-Validation_and_Error_Handling/Movie_Magic_Part_04/src/index.js
+++ JS_Back_End/L09_Workshop_Part_04-Validation_and_Error_Handling/Movie_Magic_Part_04/src/index.js
@@ -1,26 +1,28 @@
 import express from 'express';
+import expressSession from 'express-session';
 import handlebars from 'express-handlebars';
 import mongoose from 'mongoose';
-import cookieParser  from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 
 import routes from './routes.js';
 import showRatingHelper from './helpers/rating-helper.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { tempData } from '../middlewares/tempDataMiddleware.js';
 
 const app = express();
-const port = 5000;  
+const port = 5000;
 
 //  db configuration
- try {
+try {
     const localUri = 'mongodb://127.0.0.1:27017/magic-movies';
     await mongoose.connect(process.env.DATABASE_URI ?? localUri);
 
     console.log('DB connected successfully!');
- } catch (err) {
+} catch (err) {
     console.log('Cannot connect to DB!');
     console.log(err.message);
- }
+}
 
 
 //  handlebars configuration
@@ -38,8 +40,18 @@ app.set('views', './src/views');
 
 //  express configuration
 app.use('/static', express.static('src/public'));
-app.use(express.urlencoded({extended: false}));  
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressSession({
+    secret: 'DrugMnooTaenSecretTuka',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { 
+        secure: false, 
+        httpOnly: true 
+    }
+}));
+app.use(tempData);
 app.use(authMiddleware());  //  Get Express to parse form data
 
 //  set up routes
--- JS_Back_End/L09_Workshop_Part_04-Validation_and_Error_Handling/Movie_Magic_Part_04/src/utils/errorUtils.js
+++ JS_Back_End/L09_Workshop_Part_04-Validation_and_Error_Handling/Movie_Magic_Part_04/src/utils/errorUtils.js
@@ -1,7 +1,7 @@
 export const getErrorMessage = (err) => {
     switch (err.name) {
         case 'ValidationError':
-            return Object.values(err.errors).at(0).message;
+            return Object.values(err.errors)[0].message;
         default:
             return err.message;
     }
