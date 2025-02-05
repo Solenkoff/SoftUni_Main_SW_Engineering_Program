f7f3a5001a48b821ef0a7a93187b73090b31e63c
Solenkoff <solenkoff@gmail.com>
2025-02-05 15:23:40 +0200

    Add getErrorMessage util

--- JS_Back_End/L09_Workshop_Part_04-Validation_and_Error_Handling/Movie_Magic_Part_04/src/controllers/authController.js
+++ JS_Back_End/L09_Workshop_Part_04-Validation_and_Error_Handling/Movie_Magic_Part_04/src/controllers/authController.js
@@ -42,8 +42,7 @@ authController.post('/login', async (req, res) => {
         res.cookie('auth', token, { httpOnly: true });
         res.redirect('/');
     } catch (err) {
-        console.log(err.message);
-        res.redirect('/404');
+        return res.render('auth/login', { error: getErrorMessage(err) });
     }
 });
 
