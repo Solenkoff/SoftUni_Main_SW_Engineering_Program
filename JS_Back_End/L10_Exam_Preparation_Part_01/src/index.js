e5d8d5b006542954cff9fdf8b1cf1bb591257b8c
Solenkoff <solenkoff@gmail.com>
2025-02-07 14:00:42 +0200

    Install handlebars and set as view engine

--- JS_Back_End/L10_Exam_Preparation_Part_01/src/index.js
+++ JS_Back_End/L10_Exam_Preparation_Part_01/src/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import handlbars from 'express-handlebars';
 import routes from './routes.js';
 const app = express();
+} 
+
+//  Handlebars setup
+app.engine('hbs', handlbars.engine({
+    extname: 'hbs',
+}))
+
+app.set('view engine', 'hbs');
+//  may use   |  import path from 'path';
+//            |  app.set('views', path.resolve('./src/views'));  
+app.set('views', './src/views');  
+
 //  Express setup
 app.use(express.static('src/public'));   //  from root dir
 app.use(express.urlencoded({extended: false}));
+app.use(routes);
+
+
+
 app.listen(port, () => console.log('Server is listening on http://localhost:3000...'));     
