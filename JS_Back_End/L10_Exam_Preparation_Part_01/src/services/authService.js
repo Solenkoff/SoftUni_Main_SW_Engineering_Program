29508ad7ec5d6bbfcafc8b7d681d98f90d3971d8
Solenkoff <solenkoff@gmail.com>
2025-02-07 15:31:27 +0200

    Add auth service with register functionality

---
+++ JS_Back_End/L10_Exam_Preparation_Part_01/src/services/authService.js
@@ -0,0 +1,23 @@
+import User from '../models/User.js';
+
+ 
+export const register = async (userData) => {
+    if(userData.password !== userData.confirmPassword){
+        throw new Error('Password missmatch!');
+    }
+       
+    const user = await User.findOne({email: userData.email}).select({_id: true});
+    if(user){
+        throw new Error('User already exists!');
+    }
+
+    return User.create(userData); 
+};
+
+
+ 
+const authService = {
+    register
+};
+
+export default authService; 
