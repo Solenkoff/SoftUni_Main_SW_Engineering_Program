9013cead34fd6ed975318ba4ff52b59acd0b9bd1
Solenkoff <solenkoff@gmail.com>
2025-02-07 14:30:54 +0200

    Install mongoose and set up DB

--- JS_Back_End/L10_Exam_Preparation_Part_01/src/index.js
+++ JS_Back_End/L10_Exam_Preparation_Part_01/src/index.js
@@ -1,7 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import handlbars from 'express-handlebars';
+
+
 import routes from './routes.js';
+    
+
 const app = express();
+const port = 3000;
+
+
+//  Db setup
+try {
+    //  TODO: Change DB name
+    const url = 'mongodb://localhost:27017/techstore';   
+    await mongoose.connect(url);
+
+    console.log('DB Connected!');
+} catch (err) {
+    console.error('Cannot connect to DB!');
+    console.log(err.message);
 } 
 
 //  Handlebars setup
