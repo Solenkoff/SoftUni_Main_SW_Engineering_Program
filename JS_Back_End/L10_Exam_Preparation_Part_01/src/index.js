a17a3c4634ecbc458acfd1b005fabbed193c22b3
Solenkoff <solenkoff@gmail.com>
2025-02-07 12:09:45 +0200

    Setup express

---
+++ JS_Back_End/L10_Exam_Preparation_Part_01/src/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+//  Express setup
+app.use(express.static('src/public'));   //  from root dir
+app.use(express.urlencoded({extended: false}));
+app.listen(port, () => console.log('Server is listening on http://localhost:3000...'));     
