17c09311677516f2a9f6dd1b726a6f3bf3ba8ae8
Solenkoff <solenkoff@gmail.com>
2025-02-10 10:20:05 +0200

    Install and config express-session

--- JS_Back_End/L10_Exam_Preparation_Part_01/src/index.js
+++ JS_Back_End/L10_Exam_Preparation_Part_01/src/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import handlbars from 'express-handlebars';
 import cookieParser from 'cookie-parser';
+import expressSession from 'express-session';
 
 import routes from './routes.js';
 import { auth } from './middlewares/authMiddleware.js';
 import { setTitle } from './utils/pageTitleUtil.js';
-    
+
 
 const app = express();
 const port = 3000;
@@ -15,20 +16,20 @@ const port = 3000;
 //  Db setup
 try {
     //  TODO: Change DB name
-    const url = 'mongodb://localhost:27017/techstore';   
+    const url = 'mongodb://localhost:27017/techstore';
     await mongoose.connect(url);
 
     console.log('DB Connected!');
 } catch (err) {
     console.error('Cannot connect to DB!');
     console.log(err.message);
-} 
+}
 
 //  Handlebars setup
 app.engine('hbs', handlbars.engine({
     extname: 'hbs',
     runtimeOptions: {
-        allowProtoPropertiesByDefault: true,  
+        allowProtoPropertiesByDefault: true,
     },
     helpers: {
         setTitle
@@ -38,12 +39,22 @@ app.engine('hbs', handlbars.engine({
 app.set('view engine', 'hbs');
 //  may use   |  import path from 'path';
 //            |  app.set('views', path.resolve('./src/views'));  
-app.set('views', './src/views');  
+app.set('views', './src/views');
 
 //  Express setup
 app.use(express.static('src/public'));   //  from root dir
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressSession({
+    secret: '*slnlsnLSlLlnsl***03jpjSIDj90034j4twSJ',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { 
+        secure: false, 
+        httpOnly: true 
+    }
+}));
+
 app.use(auth);
 app.use(routes);
 
