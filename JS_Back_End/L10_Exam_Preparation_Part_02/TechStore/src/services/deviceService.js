10af3cc183217bb1a74d64b53caae1a275d13588
Solenkoff <solenkoff@gmail.com>
2025-02-12 17:42:17 +0200

    Add device details functionality

--- JS_Back_End/L10_Exam_Preparation_Part_02/TechStore/src/services/deviceService.js
+++ JS_Back_End/L10_Exam_Preparation_Part_02/TechStore/src/services/deviceService.js
@@ -5,12 +5,15 @@ export const getAll = () => Device.find({});
 //export const getLatest = () => Device.find({}).sort({_id: 'desc'}).limit(3);
 export const getLatest = () => getAll().sort({ createdAt: 'desc' }).limit(3);
 
+export const getOne = (deviceId) => Device.findById(deviceId);
+
 export const create = (deviceData, userId) => Device.create({ ...deviceData, owner: userId });
 
 
 const deviceService = {
     getAll,
     getLatest,
+    getOne,
     create,
 
 };
