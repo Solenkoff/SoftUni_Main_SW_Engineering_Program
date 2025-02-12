3e8d1c4ec11c6c6ebba519e8d024016dff7678a4
Solenkoff <solenkoff@gmail.com>
2025-02-12 17:42:17 +0200

    Add device details functionality

--- JS_Back_End/L10_Exam_Preparation_Part_02/TechStore/src/controllers/deviceController.js
+++ JS_Back_End/L10_Exam_Preparation_Part_02/TechStore/src/controllers/deviceController.js
@@ -28,6 +28,15 @@ deviceController.post('/create', isAuth, async (req, res) => {   // Check if log
     } catch (err) {                                         // Catch error and return response with kept data and error message
         res.render('devices/create', { device: deviceData, error: getErrorMessage(err) });
     }
+});
+
+deviceController.get('/:deviceId/details', async (req, res) => {
+    const deviceId = req.params.deviceId;
+    const device = await deviceService.getOne(deviceId);
+    const isOwner = device.owner.equals(req.user?.id); 
+    //const isOwner = req.user && req.user.id === device.owner.toString(); 
+    res.render('devices/details', { device, isOwner, hasPrefered }); 
+}); 
 })
 
 export default deviceController; 
