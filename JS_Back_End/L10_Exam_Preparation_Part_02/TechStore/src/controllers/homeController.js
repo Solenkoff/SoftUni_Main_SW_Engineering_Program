1f78b3ab885a6c1eb2f125029cdce5138ec90343
Solenkoff <solenkoff@gmail.com>
2025-02-12 16:42:29 +0200

    Add home page with view

--- JS_Back_End/L10_Exam_Preparation_Part_02/TechStore/src/controllers/homeController.js
+++ JS_Back_End/L10_Exam_Preparation_Part_02/TechStore/src/controllers/homeController.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import deviceService from "../services/deviceService.js";
 
 const homeController = Router();
 
 
-homeController.get('/', (req, res) => {
-    res.render('home');
+homeController.get('/', async (req, res) => {
+
+    const latestDevice = await deviceService.getLatest();
+    res.render('home', { devices: latestDevice });
 })
 
-export default homeController;
+export default homeController; 
