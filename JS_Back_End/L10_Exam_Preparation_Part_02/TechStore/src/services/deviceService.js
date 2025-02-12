c5ad7d5ca46c51ea1a573392f5f95cdc2f9ec624
Solenkoff <solenkoff@gmail.com>
2025-02-12 17:08:20 +0200

    Add catalog functionality

--- JS_Back_End/L10_Exam_Preparation_Part_02/TechStore/src/services/deviceService.js
+++ JS_Back_End/L10_Exam_Preparation_Part_02/TechStore/src/services/deviceService.js
@@ -1,5 +1,7 @@
 import Device from '../models/Device.js';
 
+
+export const getAll = () => Device.find({});
 //export const getLatest = () => Device.find({}).sort({_id: 'desc'}).limit(3);
 export const getLatest = () => getAll().sort({ createdAt: 'desc' }).limit(3);
 
@@ -7,6 +9,7 @@ export const create = (deviceData, userId) => Device.create({ ...deviceData, own
 
 
 const deviceService = {
+    getAll,
     getLatest,
     create,
 
