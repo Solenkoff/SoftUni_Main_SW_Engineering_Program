d0da60facc8510a0be5e3034c10445a2ebb27d5c
Solenkoff <solenkoff@gmail.com>
2025-02-12 16:42:29 +0200

    Add home page with view

--- JS_Back_End/L10_Exam_Preparation_Part_02/TechStore/src/services/deviceService.js
+++ JS_Back_End/L10_Exam_Preparation_Part_02/TechStore/src/services/deviceService.js
@@ -1,9 +1,13 @@
 import Device from '../models/Device.js';
 
+//export const getLatest = () => Device.find({}).sort({_id: 'desc'}).limit(3);
+export const getLatest = () => getAll().sort({ createdAt: 'desc' }).limit(3);
+
 export const create = (deviceData, userId) => Device.create({ ...deviceData, owner: userId });
 
 
 const deviceService = {
+    getLatest,
     create,
 
 };
