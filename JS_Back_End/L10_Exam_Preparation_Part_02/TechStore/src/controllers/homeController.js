e9a501af2d801febaf6720c2da272ed4248da19e
Solenkoff <solenkoff@gmail.com>
2025-02-13 19:01:29 +0200

    Add profile page

--- JS_Back_End/L10_Exam_Preparation_Part_02/TechStore/src/controllers/homeController.js
+++ JS_Back_End/L10_Exam_Preparation_Part_02/TechStore/src/controllers/homeController.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import deviceService from "../services/deviceService.js";
+import { isAuth } from "../middlewares/authMiddleware.js";
 
 const homeController = Router();
 
@@ -13,6 +14,13 @@ homeController.get('/', async (req, res) => {
 
 homeController.get('/about', async (req, res) => {
     res.render('about');
+});
+
+homeController.get('/profile', isAuth, async (req, res) => {
+    res.render('profile', {
+        ownDevices,
+        preferredDevices
+    });  
 });
 
 
