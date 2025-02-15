9ac3d0fc04f3d324078ca9f024fec32327950de7
Solenkoff <solenkoff@gmail.com>
2025-02-15 13:31:12 +0200

    Add create recipe functionality

--- JS_Back_End/L10_Exam_Preparation_Part_02/Home_Cooking_Recipes/src/controllers/recipeController.js
+++ JS_Back_End/L10_Exam_Preparation_Part_02/Home_Cooking_Recipes/src/controllers/recipeController.js
@@ -1,11 +1,28 @@
 import { Router } from 'express';
 
-import recipeService from '../services/disasterService.js';
+import recipeService from '../services/recipeService.js';
 import { isAuth } from '../middlewares/authMiddleware.js';
 import { getErrorMessage } from '../utils/errorUtil.js';
 
 const recipeController = Router();
 
+
+recipeController.get('/create', isAuth, (req, res) => {
+    res.render('recipes/create');
+
+});
+
+recipeController.post('/create', isAuth, async (req, res) => {   
+    const recipeData = req.body;     
+    const userId = req.user.id;      
+
+    try {
+        await recipeService.create(recipeData, userId);     
+        res.redirect('/recipes/catalog');                           
+    } catch (err) {                                         
+        res.render('recipes/create', { recipe: recipeData, error: getErrorMessage(err) });
+    }
+});
 
 
 export default recipeController;   
--- JS_Back_End/L10_Exam_Preparation_Part_02/Home_Cooking_Recipes/src/services/recipeService.js
+++ JS_Back_End/L10_Exam_Preparation_Part_02/Home_Cooking_Recipes/src/services/recipeService.js
@@ -1,6 +1,7 @@
-import Recipe from "../models/Model";
+import Recipe from "../models/Recipe.js";
 
 
+export const create = (recipeData, userId) => Recipe.create({ ...recipeData, owner: userId });
 
 
 const recipeService = {
