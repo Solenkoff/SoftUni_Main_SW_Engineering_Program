4094687571cce333c9e564f9d6ce3ac785c31b16
Solenkoff <solenkoff@gmail.com>
2025-02-15 15:05:56 +0200

    Add recipe details page

--- JS_Back_End/L10_Exam_Preparation_Part_02/Home_Cooking_Recipes/src/services/recipeService.js
+++ JS_Back_End/L10_Exam_Preparation_Part_02/Home_Cooking_Recipes/src/services/recipeService.js
@@ -9,12 +9,15 @@ export const getAll = (filter = {}) => {
 
 export const getLatest = () => getAll().sort({ createdAt: 'desc' }).limit(3);
 
+export const getOne = (recipeId) => Recipe.findById(recipeId);
+
 export const create = (recipeData, userId) => Recipe.create({ ...recipeData, owner: userId });
 
 
 const recipeService = {
     getAll,
     getLatest,
+    getOne,
     create,
    
 };
