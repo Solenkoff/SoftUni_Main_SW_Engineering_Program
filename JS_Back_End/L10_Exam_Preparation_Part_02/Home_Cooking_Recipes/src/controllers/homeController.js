3531bbb6f63581c95c8802db2415402deb84ef54
Solenkoff <solenkoff@gmail.com>
2025-02-15 14:36:51 +0200

    Add home page with last three posts

--- JS_Back_End/L10_Exam_Preparation_Part_02/Home_Cooking_Recipes/src/controllers/homeController.js
+++ JS_Back_End/L10_Exam_Preparation_Part_02/Home_Cooking_Recipes/src/controllers/homeController.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import recipeService from "../services/recipeService.js";
 
 const homeController = Router();
 
 
 homeController.get('/', async (req, res) => {
-    res.render('home');
+    const latestResipes = await recipeService.getLatest();
+    res.render('home', { recipes: latestResipes });
 });
 
 
--- JS_Back_End/L10_Exam_Preparation_Part_02/Home_Cooking_Recipes/src/services/recipeService.js
+++ JS_Back_End/L10_Exam_Preparation_Part_02/Home_Cooking_Recipes/src/services/recipeService.js
@@ -7,11 +7,14 @@ export const getAll = (filter = {}) => {
     return query;
 };
 
+export const getLatest = () => getAll().sort({ createdAt: 'desc' }).limit(3);
+
 export const create = (recipeData, userId) => Recipe.create({ ...recipeData, owner: userId });
 
 
 const recipeService = {
     getAll,
+    getLatest,
     create,
    
 };
