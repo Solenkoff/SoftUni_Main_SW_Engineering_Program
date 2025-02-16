fedb5484e59da949b42f32a733bbab1faafe1df4
Solenkoff <solenkoff@gmail.com>
2025-02-16 13:44:32 +0200

    Add validations and sanitize code

--- JS_Back_End/Exams/2025-02-16_Exam_JS_Back_End/PowerOfNature/src/models/Disaster.js
+++ JS_Back_End/Exams/2025-02-16_Exam_JS_Back_End/PowerOfNature/src/models/Disaster.js
@@ -4,6 +4,7 @@ const disasterSchema = new Schema({
     name: {
         type: String,
         required: true,
+        minLength: [2, 'The Name should be at least 2 characters long']
     },
     typeDisaster: {
         type: String,
@@ -21,18 +22,23 @@ const disasterSchema = new Schema({
     eventYear: {
         type: Number,
         required: true,
+        min: [0, 'The Year should be between 0 and 2024'],
+        max: [2024, 'The Year should be between 0 and 2024']
     },
     location: {
         type: String,
         required: true,
+        minLength: [3, 'The Location should be at least 3 characters long']
     },
     image: {
         type: String,
         required: true,
+        validate: [ /^https?:\/\//, 'The Disaster Image should start with http:// or https://' ]
     },
     description: {
         type: String,
         required: true,
+        minLength: [10, 'The Description should be a minimum of 10 characters long']
     },
     interestedList : [{
         type: Types.ObjectId,
