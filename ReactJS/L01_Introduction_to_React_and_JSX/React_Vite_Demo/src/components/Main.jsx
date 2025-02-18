b398d1b8bf1210c9e037de2ade3b4e90f371f088
Solenkoff <solenkoff@gmail.com>
2025-02-18 16:38:03 +0200

    Add lab demo

---
+++ ReactJS/L01_Introduction_to_React_and_JSX/React_Vite_Demo/src/components/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+    return (
+        <>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nam ex suscipit animi? In aperiam fugiat excepturi autem, consequatur reprehenderit.</p>
+        </>
+    )
+};
+
+export default Main;
