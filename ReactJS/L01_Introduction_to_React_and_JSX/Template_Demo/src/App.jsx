2db7a6aa42a0ca2c2e1e431ebb204d555dfd022d
Solenkoff <solenkoff@gmail.com>
2025-02-18 18:22:37 +0200

    Add lab demo

---
+++ ReactJS/L01_Introduction_to_React_and_JSX/Template_Demo/src/App.jsx
@@ -0,0 +1,34 @@
+import AboutSection from "./AboutSection";
+import CategorySection from "./CategorySection";
+import ClientSection from "./ClientSection";
+import ExperienceSection from "./ExperienceSection";
+import FooterSection from "./FooterSection";
+import FreelanceSection from "./FreelanceSection";
+import HeroArea from "./HeroArea";
+import InfoSection from "./InfoSection";
+
+function App() {
+
+    return (
+        <>
+            <HeroArea />
+
+            <ExperienceSection />
+
+            <CategorySection />
+            
+            <AboutSection />
+
+            <FreelanceSection />
+           
+            <ClientSection />
+         
+            <InfoSection />
+
+            <FooterSection />
+
+        </>
+    )
+}
+
+export default App;  
---
+++ ReactJS/L01_Introduction_to_React_and_JSX/Template_Demo/src/CategoryBox.jsx
@@ -0,0 +1,18 @@
+export default function CategoryBox() {
+    return (
+        <>
+
+            <div className="box">
+                <div className="img-box">
+                    <img src="images/c1.png" alt="" />
+                </div>
+                <div className="detail-box">
+                    <h5>
+                        Design & Arts
+                    </h5>
+                </div>
+            </div>
+
+        </>
+    )
+}
