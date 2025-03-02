fff767badf33544eba484d2e77f27b2c6a33d3e2
Solenkoff <solenkoff@gmail.com>
2025-03-02 10:45:59 +0200

    Format iso date

---
+++ ReactJS/L04_Workshop_Components/UsersList/client/src/utils/datetimeUtils.js
@@ -0,0 +1,7 @@
+export const fromIsoDate = (isoDate) => {
+    const date = new Date(isoDate);
+
+    const formattedDate = date.toLocaleString('en-Us', {year: 'numeric', month: 'long', day: 'numeric'});
+
+    return formattedDate;
+}
