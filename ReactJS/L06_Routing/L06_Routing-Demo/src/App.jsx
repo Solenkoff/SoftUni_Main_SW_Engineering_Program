d459805e2407ee6a96d1049d1318454378536e67
Solenkoff <solenkoff@gmail.com>
2025-03-07 10:25:21 +0200

    Initialize and set up demo project

---
+++ ReactJS/L06_Routing/L06_Routing-Demo/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router';
+
+import './App.css'
+
+function App() {
+   
+
+    return (   
+        
+            
+        </div>
+    )
+}
+
+export default App
