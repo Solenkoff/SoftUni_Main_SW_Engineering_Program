dd84860cc3521500f3628d8e22e9ec541079b9a0
Solenkoff <solenkoff@gmail.com>
2025-03-07 14:06:32 +0200

    Add Catalog page

--- ReactJS/L06_Routing/L06_Routing-Demo/src/App.jsx
+++ ReactJS/L06_Routing/L06_Routing-Demo/src/App.jsx
@@ -7,15 +7,27 @@ import Home from './components/Home'
 import Contacts from './components/Contacts';
 import Pricing from './components/Pricing';
 import NotFound from './components/404';
+import Catalog from './components/Catalog';
+
+   
+
+
 function App() {
    
 
     return (   
+        <div className="bg-white">
             <Header />
+
+            <Routes>
                   <Route path="/" element={<Home />} />
+                  <Route path="/catalog" element={<Catalog />} />
                   <Route path="/pricing" element={<Pricing />} />
                   <Route path="/contacts" element={<Contacts />} />
                   <Route path="/*" element={<NotFound />} />
+
+
+            </Routes>
         
             
         </div>
