67d667fe474c007486ce97c876c5c784c21b5dc8
Solenkoff <solenkoff@gmail.com>
2025-03-07 11:08:11 +0200

    Add Home page

--- ReactJS/L06_Routing/L06_Routing-Demo/src/App.jsx
+++ ReactJS/L06_Routing/L06_Routing-Demo/src/App.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router';
 import './App.css'
 
 import Header from './components/Header'
+import Home from './components/Home'
 function App() {
    
 
     return (   
             <Header />
+                  <Route path="/" element={<Home />} />
         
             
         </div>
