1eb310fa9acfbcbf5cd25932fccf5817f469e96f
Solenkoff <solenkoff@gmail.com>
2025-03-08 15:34:29 +0200

    Add Individual and Business pricing with nested routing

--- ReactJS/L06_Routing/L06_Routing-Demo/src/App.jsx
+++ ReactJS/L06_Routing/L06_Routing-Demo/src/App.jsx
@@ -9,23 +9,28 @@ import Pricing from './components/Pricing';
 import NotFound from './components/404';
 import Catalog from './components/Catalog';
 import ProductDetails from './components/ProductDetails';
+import IndividualPricing from './components/IndividualPricing';
+import BusinessPricing from './components/BusinessPricing';
 
-   
+
 function App() {
-   
-    return (   
+
+    return (
         <div className="bg-white">
             <Header />
 
             <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/catalog" element={<Catalog />} />
-                  <Route path="/catalog/:productId" element={<ProductDetails />} />
-                  <Route path="/pricing" element={<Pricing />} />
-                  <Route path="/contacts" element={<Contacts />} />
-                  <Route path="/*" element={<NotFound />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/catalog" element={<Catalog />} />
+                <Route path="/catalog/:productId" element={<ProductDetails />} />
+                <Route path="/pricing" element={<Pricing />} >
+                    <Route index element={<IndividualPricing />} />
+                    <Route path='business' element={<BusinessPricing />} />
+                </Route>
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/*" element={<NotFound />} />
             </Routes>
-        
+
         </div>
     )
 }
