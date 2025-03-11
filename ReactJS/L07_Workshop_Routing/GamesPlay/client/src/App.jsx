ededd5ca645ac9d5d6bed25f7ebfdc5dc2b0c978
Solenkoff <solenkoff@gmail.com>
2025-03-11 12:17:32 +0200

    Add all main pages and routes

--- ReactJS/L07_Workshop_Routing/GamesPlay/client/src/App.jsx
+++ ReactJS/L07_Workshop_Routing/GamesPlay/client/src/App.jsx
@@ -2,6 +2,11 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
+import Login from './components/login/Login'
+import Register from './components/register/Register'
+import GameCatalog from './components/game-catalog/GameCatalog'
+import GameCreate from './components/game-create/GameCreate' 
+
 
 function App() {
 
@@ -12,10 +17,16 @@ function App() {
             <main id="main-content">
                 <Routes>
                     <Route index element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/games' element={<GameCatalog />} />
+                    <Route path='/games/create' element={<GameCreate />} />
+                    {/* <Route path='/games/:gameId/details' element={<GameDetails />} /> */}
+                    {/* <Route path='/games/:gameId/edit' element={<GameEdit />} /> */}
                 </Routes>
                 
             </main>
-        </div>
+        </div> 
     )
 }
 
