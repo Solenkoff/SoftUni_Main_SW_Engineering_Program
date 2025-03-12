0d3b6aa504e546d28bedc4c89744442548b3187b
Solenkoff <solenkoff@gmail.com>
2025-03-12 13:48:57 +0200

    Delete game functionality

--- ReactJS/L07_Workshop_Routing/GamesPlay/client/src/components/game-details/GameDetails.jsx
+++ ReactJS/L07_Workshop_Routing/GamesPlay/client/src/components/game-details/GameDetails.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 
 import gameService from "../../services/gameService";
 
 export default function GameDetails() {
+    const navigate = useNavigate();
     const [game, setGame] = useState({});
     const { gameId } = useParams();
 
@@ -14,9 +15,21 @@ export default function GameDetails() {
             })
     }, [gameId]);
 
+    const gameDeleteClickHandler = async () => {
+        const hasConfirmed = confirm(`Are you sure you want to delete ${game.title} game?`);
+
+        if(!hasConfirmed){
+            return;
+        }
+         
+        const result = await gameService.remove(gameId);
+        
+        navigate('/games');
+    }
+
     return (
         <section id="game-details">
-            <h1>Game Details</h1>
+            <h1>Game Details</h1>    
             <div className="info-section">
 
                 <div className="game-header">
@@ -46,8 +59,13 @@ export default function GameDetails() {
 
                 {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
                 <div className="buttons">
-                    <a href="#" className="button">Edit</a>
-                    <a href="#" className="button">Delete</a>
+                    <Link to={`/games/${gameId}/edit`} className="button">Edit</Link>
+                    <button 
+                        onClick={gameDeleteClickHandler} 
+                        className="button"
+                    >
+                        Delete
+                    </button>
                 </div>
             </div>
 
