b25f400afe8ae97da38b0d4debba2d2fe1090ab7
Solenkoff <solenkoff@gmail.com>
2025-03-13 11:07:13 +0200

    Add login handler

--- ReactJS/L07_Workshop_Routing/GamesPlay/client/src/App.jsx
+++ ReactJS/L07_Workshop_Routing/GamesPlay/client/src/App.jsx
@@ -8,23 +8,29 @@ import GameCatalog from './components/game-catalog/GameCatalog'
 import GameCreate from './components/game-create/GameCreate' 
 import GameDetails from './components/game-details/GameDetails'
 import GameEdit from './components/game-edit/GameEdit'
+import { useState } from 'react'
 
 
 function App() {
+    const [email, setEmail] = useState('');
+
+    const userLoginHandler = (email) => {
+        setEmail(email); 
+    }
 
     return (
         <div id="box">
             <Header />
 
-            <main id="main-content">
+            <main id="main-content">    
                 <Routes>
                     <Route index element={<Home />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/register' element={<Register />} />
                     <Route path='/games' element={<GameCatalog />} />
                     <Route path='/games/create' element={<GameCreate />} />
                     <Route path='/games/:gameId/details' element={<GameDetails />} />
                     <Route path='/games/:gameId/edit' element={<GameEdit />} />
+                    <Route path='/login' element={<Login onLogin={userLoginHandler}/>} />
+                    <Route path='/register' element={<Register />} />
                 </Routes>
                 
             </main>
