7795413e198f5589b76f17d623ae3de7ed36cd8b
Solenkoff <solenkoff@gmail.com>
2025-03-14 16:34:49 +0200

    Add useFetch custom hook

--- ReactJS/L08_React_Hooks/L08_React_Hooks-Demo/client/src/App.jsx
+++ ReactJS/L08_React_Hooks/L08_React_Hooks-Demo/client/src/App.jsx
@@ -1,13 +1,24 @@
+import useFetch from "./hooks/useFetch"
+import { Spin } from 'antd';
+
+
 import Chat from "./components/Chat"
 
-function App() {
+const url = 'http://localhost:3030/jsonstore/messages';
 
+function App() {
+    const [pending, messages] = useFetch(url, []);
+
+    console.log(messages);
+    
     return (
         <>
-            <Chat />
+            {pending
+                ? <Spin />
+                : <Chat messages={messages} />
+            }
         </>
-    )
+    ) 
 }
 
 export default App
- 
