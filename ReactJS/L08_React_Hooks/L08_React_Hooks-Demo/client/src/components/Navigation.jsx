b6b0a13ee2d8f621fdd26aec7dba563021b317da
Solenkoff <solenkoff@gmail.com>
2025-03-15 16:48:52 +0200

    Fix navigation

--- ReactJS/L08_React_Hooks/L08_React_Hooks-Demo/client/src/components/Navigation.jsx
+++ ReactJS/L08_React_Hooks/L08_React_Hooks-Demo/client/src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router';
 
-import { HomeOutlined, MessageOutlined } from '@ant-design/icons';
+import { HomeOutlined, MessageOutlined, SendOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
 
 const items = [
@@ -14,15 +14,24 @@ const items = [
         label: <Link to="/chat">Chat</Link>,
         key: 'chat',
         icon: <MessageOutlined />,
+    },
+    {
+        label: <Link to="/send">Send</Link>,
+        key: 'send',
+        icon: <SendOutlined />,
     }
 ];
 
 export default function Navigation() {
     const [current, setCurrent] = useState('mail');
     const onClick = (e) => {
-        console.log('click ', e);
         setCurrent(e.key);
     };
 
-    return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+    return <Menu
+        onClick={onClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+        items={items}
+    />;
 };
