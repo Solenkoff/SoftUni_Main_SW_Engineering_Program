dc3f9ae92a5528435ef720ca64ef20a1052ff2cc
Solenkoff <solenkoff@gmail.com>
2025-03-15 19:30:38 +0200

    Add UserContext  and refactor code to change prop drilling with context

--- ReactJS/L08_React_Hooks/L08_React_Hooks-Demo/client/src/App.jsx
+++ ReactJS/L08_React_Hooks/L08_React_Hooks-Demo/client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
+import { useState } from "react";
+
+import { UserContext } from "./contexts/UserContext";
 
 import Navigation from "./components/Navigation";
 import ChatPage from "./components/ChatPage";
 import HomePage from "./components/HomePage";
 import Send from "./components/Send";
 import Login from "./components/Login";
-import { useState } from "react";
 
 function App() {
     const [user, setUser] = useState('');
@@ -15,18 +17,17 @@ function App() {
     }
    
     return (
-        <>
+        <UserContext.Provider value={{userLoginHandler, user}}>
             <Navigation />
 
             <Routes>
                 <Route index element={<HomePage />} />
                 <Route path="/chat" element={<ChatPage/>} />
-                <Route path="/send" element={<Send user={user} />} />
-                <Route path="/login" element={<Login onLogin={userLoginHandler} />} />
+                <Route path="/send" element={<Send />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
 
-
-        </>
+        </UserContext.Provider>
     )
 }
 
