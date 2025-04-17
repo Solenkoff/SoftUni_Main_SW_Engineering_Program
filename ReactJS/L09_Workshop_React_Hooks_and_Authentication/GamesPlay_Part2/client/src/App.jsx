09c84109fedb3a648fc73c22e08a4a2dd49d1792
Solenkoff <solenkoff@gmail.com>
2025-04-17 12:14:16 +0300

    Add UserContext with UserAuth hook and adapting the requester

---
+++ ReactJS/L09_Workshop_React_Hooks_and_Authentication/GamesPlay_Part2/client/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router'
+
+import { UserContext } from './contexts/UserContext'
+
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import Login from './components/login/Login'
+import Register from './components/register/Register'
+import GameCatalog from './components/game-catalog/GameCatalog'
+import GameCreate from './components/game-create/GameCreate'
+import GameDetails from './components/game-details/GameDetails'
+import GameEdit from './components/game-edit/GameEdit'
+import './App.css'
+import Logout from './components/logout/Logout'
+
+function App() {
+    const [authData, setAuthData] = useState({});
+
+    const userLoginHandler = (resultData) => {
+        setAuthData(resultData);
+    };
+
+    const userLogoutHandler = () => {
+        setAuthData({});
+    };
+
+    return (
+        <UserContext.Provider value={{ ...authData, userLoginHandler, userLogoutHandler }}>
+            <div id="box">
+                <Header />
+
+                <main id="main-content">
+                    <Routes>
+                        <Route index element={<Home />} />
+                        <Route path="/games" element={<GameCatalog />} />
+                        <Route path="/games/create" element={<GameCreate />} />
+                        <Route path="/games/:gameId/details" element={<GameDetails />} />
+                        <Route path="/games/:gameId/edit" element={<GameEdit />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/logout" element={<Logout />} />
+                    </Routes>
+                </main>
+            </div>
+        </UserContext.Provider>
+    )
+}
+
+export default App
