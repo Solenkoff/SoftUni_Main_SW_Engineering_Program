66e4c03e1ee6b42efd6c2e7f8b57845fa3c1259a
Solenkoff <solenkoff@gmail.com>
2025-04-20 16:49:51 +0300

    Loading Workshop until now

---
+++ ReactJS/L11_Class_Components/GamesPlay_Part4/client/src/api/commentApi.js
@@ -0,0 +1,60 @@
+import { useEffect, useReducer } from "react";
+import useAuth from "../hooks/useAuth";
+import request from "../utils/request";
+
+const baseUrl = 'http://localhost:3030/data/comments';
+
+function commentsReducer(state, action) {
+    switch (action.type) {
+        case 'ADD_COMMENT': 
+            return [...state, action.payload]
+        case 'GET_ALL':
+           return action.payload; 
+        default:
+            return state;
+    }
+};
+
+export const useComments = (gameId) => {
+    const { accessToken } = useAuth();
+    // const [comments, setComments] = useState([]);
+    const [comments, dispatch] = useReducer(commentsReducer, [])
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams({
+            where: `gameId="${gameId}"`,
+            load: `author=_ownerId:users`,
+        });
+
+        const options = {
+            headers: {
+                'X-Authorization': accessToken,
+            }
+        }
+
+        request.get(`${baseUrl}?${searchParams.toString()}`, null, options)
+            .then(result => dispatch({type: 'GET_ALL', payload: result}))
+    }, [gameId, accessToken]); // TODO Fix this
+
+    return {
+        comments,
+        addComment: (commentData) => dispatch({type: 'ADD_COMMENT', payload: commentData})
+    }
+}
+
+export const useCreateComment = () => {
+    const { request } = useAuth();
+
+    const create = (gameId, comment) => {
+        const commentData = {
+            gameId,
+            comment,
+        };
+
+        return request.post(baseUrl, commentData);
+    }
+
+    return {
+        create,
+    }
+}
