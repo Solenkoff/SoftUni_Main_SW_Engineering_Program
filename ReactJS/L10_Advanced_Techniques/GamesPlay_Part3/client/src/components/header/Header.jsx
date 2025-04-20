6fd779459dc10a17e941fc6875f4b2e0b36cfe43
Solenkoff <solenkoff@gmail.com>
2025-04-20 11:15:19 +0300

    Implement userContext and useAuth

--- ReactJS/L10_Advanced_Techniques/GamesPlay_Part3/client/src/components/header/Header.jsx
+++ ReactJS/L10_Advanced_Techniques/GamesPlay_Part3/client/src/components/header/Header.jsx
@@ -1,21 +1,21 @@
-import { useContext } from "react";
 import { Link } from "react-router";
-import { UserContext } from "../../contexts/UserContext";
+import useAuth from "../../hooks/useAuth";
 
 export default function Header() {
-    const { email } = useContext(UserContext);
-
+    const { email, isAuthenticated } = useAuth();
+    
     return (
         <header>
             <h1><Link className="home" to="/">GamesPlay</Link></h1>
             <nav>
                 <Link to="/games">All games</Link>
                 {/* <!-- Logged-in users --> */}
-                {email
+                {isAuthenticated
                     ? (
                         <div id="user">
                             <Link to="/games/create">Create Game</Link>
                             <Link to="/logout">Logout</Link>
+                            <Link to="/admin">Admin</Link>
                             {email}
                         </div>
                     )
