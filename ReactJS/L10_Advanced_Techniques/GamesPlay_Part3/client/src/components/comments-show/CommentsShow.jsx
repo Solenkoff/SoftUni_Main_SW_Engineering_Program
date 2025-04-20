9c8a647d4a3be216901fd2ca7febe846fde09f7a
Solenkoff <solenkoff@gmail.com>
2025-04-20 12:07:41 +0300

    Update commentApi and implement logic

--- ReactJS/L10_Advanced_Techniques/GamesPlay_Part3/client/src/components/comments-show/CommentsShow.jsx
+++ ReactJS/L10_Advanced_Techniques/GamesPlay_Part3/client/src/components/comments-show/CommentsShow.jsx
@@ -1,4 +1,4 @@
-export default function CommentsShow({
+export default function CommentsView({
     comments,
 }) {
     return (
@@ -6,9 +6,9 @@ export default function CommentsShow({
             <h2>Comments:</h2>
             <ul>
                 {comments.length > 0
-                    ? comments.map(({ _id, email, comment }) => (
-                        <li key={_id} className="comment">
-                            <p>{email}: {comment}</p>
+                    ? comments.map(({ _id, comment, pending, author }) => (
+                        <li key={_id} className="comment" style={{ backgroundColor: pending ? 'lightgray' : '' }} >
+                            <p>{author.email}: {comment}</p>
                         </li>
                     ))
                     : <p className="no-comment">No comments.</p>
