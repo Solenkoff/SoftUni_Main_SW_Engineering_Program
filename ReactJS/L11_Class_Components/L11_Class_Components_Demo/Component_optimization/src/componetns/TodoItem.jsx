8bbf0d67c2c6f8f47be1ee77a4e4dfc96fd64170
Solenkoff <solenkoff@gmail.com>
2025-04-20 14:39:48 +0300

    Add Class Components Demo

---
+++ ReactJS/L11_Class_Components/L11_Class_Components_Demo/Component_optimization/src/componetns/TodoItem.jsx
@@ -0,0 +1,18 @@
+import { memo } from "react";
+
+function TodoItem({
+    id,
+    text,
+    isCompleted,
+    onToggle
+}) {
+    console.log(`${text} -> re-redners`);
+    
+    return (
+        <li onClick={() => onToggle(id)} style={isCompleted ? { textDecoration: 'line-through' } : {}}>
+            {text}
+        </li>
+    );
+}
+
+export default memo(TodoItem);
