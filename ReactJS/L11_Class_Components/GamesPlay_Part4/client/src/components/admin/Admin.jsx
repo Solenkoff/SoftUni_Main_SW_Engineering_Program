9b96c08ad4b16526e000562a3e38e9e94292bc46
Solenkoff <solenkoff@gmail.com>
2025-04-22 15:20:51 +0300

    Refactor aAdmin and add styles

--- ReactJS/L11_Class_Components/GamesPlay_Part4/client/src/components/admin/Admin.jsx
+++ ReactJS/L11_Class_Components/GamesPlay_Part4/client/src/components/admin/Admin.jsx
@@ -1,8 +1,25 @@
-export default function Admin() {
-    return (
-        <>
-            <h1>Admin Section</h1>
-            <p>This will be code splitted</p>
-        </>
-    );
+import { Component } from 'react';
+import AdminComments from './admin-comments/AdminComments';
+
+import styles from './Admin.module.css';
+import { UserContext } from '../../contexts/UserContext';
+import ErrorBoundary from '../error-boundary/ErrorBoundary';
+
+export default class Admin extends Component {
+    render() {
+        return (
+            <ErrorBoundary>
+                <UserContext.Consumer>
+                    {(context) => (
+                        <section className={styles['admin-section']}>
+                            <h1>Admin Panel</h1>
+                            <h2>Hello {context.email}</h2>
+
+                            <AdminComments />
+                        </section>
+                    )}
+                </UserContext.Consumer>
+            </ErrorBoundary>
+        );
+    }
 }
