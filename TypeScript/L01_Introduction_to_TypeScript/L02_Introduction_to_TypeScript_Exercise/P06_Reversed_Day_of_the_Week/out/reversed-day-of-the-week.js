077d58ae9981bd4917306827b44f6b8eb7a6a1a1
Solenkoff <solenkoff@gmail.com>
2025-05-15 18:50:04 +0300

    Add problem solution

---
+++ TypeScript/L01_Introduction_to_TypeScript/L02_Introduction_to_TypeScript_Exercise/P06_Reversed_Day_of_the_Week/out/reversed-day-of-the-week.js
@@ -0,0 +1,19 @@
+"use strict";
+var WeekDays;
+(function (WeekDays) {
+    WeekDays[WeekDays["Monday"] = 1] = "Monday";
+    WeekDays[WeekDays["Tuesday"] = 2] = "Tuesday";
+    WeekDays[WeekDays["Wednesday"] = 3] = "Wednesday";
+    WeekDays[WeekDays["Thursday"] = 4] = "Thursday";
+    WeekDays[WeekDays["Friday"] = 5] = "Friday";
+    WeekDays[WeekDays["Saturday"] = 6] = "Saturday";
+    WeekDays[WeekDays["Sunday"] = 7] = "Sunday";
+})(WeekDays || (WeekDays = {}));
+;
+function reversedWeekDay(dayString) {
+    console.log(WeekDays[dayString] || 'error');
+}
+reversedWeekDay('Monday');
+reversedWeekDay('Friday');
+reversedWeekDay('Invalid');
+//# sourceMappingURL=reversed-day-of-the-week.js.map
