8810839029b2bbb1e263075d13fa7c8715e42381
Solenkoff <solenkoff@gmail.com>
2025-05-22 16:58:01 +0300

    Add problem solution

---
+++ TypeScript/L01_Introduction_to_TypeScript/L04_Advanced_Data_Types_Exersise/P04_HTTP_Codes/dist/http-codes.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function printResponseText(response) {
+    // switch (response.code) {
+    //     case 200:
+    //     case 201:
+    //     case 301: console.log(response.text); return;
+    //     case 400:
+    //     case 404:
+    //     case 500: console.log(response.text.slice(0, response.printChars));
+    // }
+    if ('printChars' in response) {
+        console.log(response.text.slice(0, response.printChars));
+    }
+    else {
+        console.log(response.text);
+    }
+}
+printResponseText({ code: 200, text: 'OK' });
+printResponseText({ code: 201, text: 'Created' });
+printResponseText({ code: 400, text: 'Bad Request', printChars: 4 });
+printResponseText({ code: 404, text: 'Not Found' });
+printResponseText({ code: 404, text: 'Not Found', printChars: 3 });
+printResponseText({ code: 500, text: 'Internal Server Error', printChars: 1 });
+printResponseText({ code: 200, text: 'OK', printChars: 4 }); //  Does not fit any type structure
+//# sourceMappingURL=http-codes.js.map
