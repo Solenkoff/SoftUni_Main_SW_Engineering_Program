b82eeb182ceaac41619459d5e7b41b0550fdebc5
Solenkoff <solenkoff@gmail.com>
2025-05-23 16:37:52 +0300

    Add problem solution

---
+++ TypeScript/L01_Introduction_to_TypeScript/L04_Advanced_Data_Types_Exersise/P08_Extrtact_interfaces/extract-interfaces.ts
@@ -0,0 +1,53 @@
+
+interface User { 
+    username: string, 
+    signupDate: Date 
+};
+type TaskStatus = 'Logged' | 'Started' | 'InProgress' | 'Done';
+type Task = {
+    status: TaskStatus,
+    title: string,
+    daysRequired: number,
+    assignedTo: User | undefined,
+    changeStatus(newStatus: TaskStatus): void
+}
+
+function assignTask(
+    user: User,
+    task: Task
+) {
+    if (task.assignedTo == undefined) {
+        task.assignedTo = user;
+        console.log(`User ${user.username} assigned to task '${task.title}'`);
+    }
+}
+
+
+
+
+
+let user = {
+    username: 'Margaret',
+    signupDate: new Date(2022, 1, 13),
+    passwordHash: 'random'
+}
+let task1 = {
+    status: <'Logged' | 'Started' | 'InProgress' | 'Done'>'Logged',
+    title: 'Need assistance',
+    daysRequired: 1,
+    assignedTo: undefined,
+    changeStatus(newStatus: 'Logged' | 'Started' | 'InProgress' | 'Done') { this.status = newStatus; }
+}
+
+let task2 = {
+    status: <'Logged' | 'Started' | 'InProgress' | 'Done'>'Done',
+    title: 'Test',
+    daysRequired: 12,
+    assignedTo: undefined,
+    changeStatus(newStatus: 'Logged' | 'Started' | 'InProgress' | 'Done') { this.status = newStatus; },
+    moreProps: 300,
+    evenMore: 'wow'
+}
+
+assignTask(user, task1);
+assignTask(user, task2);
