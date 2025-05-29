56a288374874fba5280c417ff723bb2af3df0488
Solenkoff <solenkoff@gmail.com>
2025-05-29 12:25:40 +0300

    Add problem solution

---
+++ TypeScript/L01_Introduction_to_TypeScript/L06_OOP_in_TypeScript-Exercise/P04_Abstraction/dist/abstraction.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Dog {
+    makeSound() {
+        return 'Woof';
+    }
+}
+const dog = new Dog();
+console.log(dog.makeSound());
+//# sourceMappingURL=abstraction.js.map
