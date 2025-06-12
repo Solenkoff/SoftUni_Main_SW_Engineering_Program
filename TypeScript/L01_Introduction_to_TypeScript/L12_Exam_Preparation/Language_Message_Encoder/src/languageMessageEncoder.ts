81880da76aed953c640ff87b1507c515c29ec8ef
Solenkoff <solenkoff@gmail.com>
2025-06-12 12:14:29 +0300

    Add encodeMessage to LanguageMessageEncoder

--- TypeScript/L01_Introduction_to_TypeScript/L12_Exam_Preparation/Language_Message_Encoder/src/languageMessageEncoder.ts
+++ TypeScript/L01_Introduction_to_TypeScript/L12_Exam_Preparation/Language_Message_Encoder/src/languageMessageEncoder.ts
@@ -1,1 +1,32 @@
-export class LanguageMessageEncoder {}
+import { Cipher } from "./contracts/cipher";
+import { PartialMessageEncoder } from "./contracts/implemented/partialMessageEncoder";
+import { Language } from "./contracts/language";
+
+
+export class LanguageMessageEncoder<TLang extends Language, TCipher extends Cipher<TLang>> extends PartialMessageEncoder {
+    private encodedCharsCount = 0;                       
+
+    constructor( lang: TLang, cipher: TCipher){          
+        super(lang, cipher);
+    }     
+
+    public encodeMessage(secretMessage: unknown) {
+        if(typeof secretMessage !== 'string' || secretMessage.length === 0){
+            return 'No message.';
+        }
+
+        const strippedMessage = this.stripForbiddenSymbols(secretMessage);            
+        const isCompatible = this.language.isCompatibleToCharset(strippedMessage);    
+
+        if(!isCompatible){
+            return 'Message not compatible.';                                         
+        }
+
+        const encodedMessage = this.cipher.encipher(strippedMessage);                 
+        this.encodedCharsCount += encodedMessage.length;                                   
+        return encodedMessage;                                                        
+    }
+
+    
+
+} 
