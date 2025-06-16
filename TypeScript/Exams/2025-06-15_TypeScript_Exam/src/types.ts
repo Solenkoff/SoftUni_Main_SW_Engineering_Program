97bfd9affd4bce54c573c015b6460066b2caf3d3
Solenkoff <solenkoff@gmail.com>
2025-06-16 11:34:02 +0300

    Add separate types file

---
+++ TypeScript/Exams/2025-06-15_TypeScript_Exam/src/types.ts
@@ -0,0 +1,4 @@
+import { SummerMonth, WinterMonth } from "./contracts/util";
+
+export type SeasonalMonth = SummerMonth | WinterMonth;
+export type RoomNumber = 'A01' | 'A02' | 'A03' | 'B01' | 'B02' | 'B03';
