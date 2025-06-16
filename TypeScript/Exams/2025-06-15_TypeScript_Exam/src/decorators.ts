2101daad8d210c1a8d52fb76d472d9e3240b8bb3
Solenkoff <solenkoff@gmail.com>
2025-06-16 15:55:20 +0300

    Implement decorator2 and add signature for the rest

--- TypeScript/Exams/2025-06-15_TypeScript_Exam/src/decorators.ts
+++ TypeScript/Exams/2025-06-15_TypeScript_Exam/src/decorators.ts
@@ -1,5 +1,19 @@
-export function decorator1() {}
-export function decorator2() {}
-export function decorator3() {}
-export function decorator4() {}
-export function decorator5() {}
+export function decorator1<T extends new (...args: any[]) => {}>(constructor: T) { }
+
+
+export function decorator2(target: any, propName: string, descriptor: PropertyDescriptor) {
+    const originalGetter = descriptor.get;
+
+    descriptor.get = function () {
+        return originalGetter?.call(this) * 1.2;
+    }
+
+    return descriptor;
+}
+
+export function decorator3(target: Object, methodName: string, paramIndex: number) {}
+
+export function decorator4(target: Object, methodName: string, paramIndex: number) {}
+
+export function decorator5(target: Object, methodName: string, paramIndex: number) {}
+
