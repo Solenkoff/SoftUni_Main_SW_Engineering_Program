d22d78dba4edd8484b5e6c8fbb47295d36ca5779
Solenkoff <solenkoff@gmail.com>
2025-06-16 11:37:43 +0300

    Setting MonthlyMotel with generic types and introducing constructor with initialized data

--- TypeScript/Exams/2025-06-15_TypeScript_Exam/src/monthlyMotel.ts
+++ TypeScript/Exams/2025-06-15_TypeScript_Exam/src/monthlyMotel.ts
@@ -1,2 +1,21 @@
-export class MonthlyMotel {}
+import { PartialMonthlyMotel } from "./contracts/partialMonthlyMotel"
+import { Room } from "./contracts/room";
+import { RoomNumber, SeasonalMonth } from "./types";
+
+export class MonthlyMotel<T extends SeasonalMonth> extends PartialMonthlyMotel {
+
+    private totalBudget: number = 0;
+    private rooms: Map<RoomNumber, Room>;
+    private bookings: Map<RoomNumber, Set<T>>;
+
+    constructor() {
+        super();
+        this.rooms = new Map<RoomNumber, Room>();
+        this.bookings = new Map<RoomNumber, Set<T>>();
+    }
+
+
+   
+
+}
 
