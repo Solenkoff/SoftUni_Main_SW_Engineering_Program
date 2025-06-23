a4f64a48c239dc7ce3a81adef563d3a9f0bd635a
Solenkoff <solenkoff@gmail.com>
2025-06-23 10:28:34 +0300

    Add FlyingMachine signature with generics and constuctor with initialized data

--- TypeScript/Exams/2025-06-22_TypeScript_Exam_Retake/src/flyingMachine.ts
+++ TypeScript/Exams/2025-06-22_TypeScript_Exam_Retake/src/flyingMachine.ts
@@ -1,2 +1,27 @@
-export class FlyingMachine{}
+import { Flyer } from "./contracts/flyer";
+import { GasForMode, LiftDeviceForMode, LiftMode } from "./types";
+
+export class FlyingMachine<T extends LiftMode> implements Flyer {
+
+    private static _totalMetersMoved: number = 0;
+    private _altitude!: number;
+    private _weight!: number;
+    private _liftDevice: LiftDeviceForMode<T>;
+    private _gas: GasForMode<T>;
+
+    constructor(
+        liftDevice: LiftDeviceForMode<T>,
+        _gas: GasForMode<T>,
+        baseWeight: number,
+        altitude: number,
+    ) {
+        this._liftDevice = liftDevice;
+        this._gas = _gas;
+        this._weight = baseWeight;
+        this.altitude = altitude;
+    }
+
+    
+
+}
 
