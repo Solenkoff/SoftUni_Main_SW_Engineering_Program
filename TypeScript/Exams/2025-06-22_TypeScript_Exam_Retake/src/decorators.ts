1998eb53c56dc68bc4ee519b323e9091e5c47044
Solenkoff <solenkoff@gmail.com>
2025-06-24 16:07:38 +0300

    Implement decorator2 and add signiture for the rest of decorators

--- TypeScript/Exams/2025-06-22_TypeScript_Exam_Retake/src/decorators.ts
+++ TypeScript/Exams/2025-06-22_TypeScript_Exam_Retake/src/decorators.ts
@@ -1,7 +1,24 @@
-export function decorator1() {}
+import { SolidRocketBooster } from "./contracts/implemented/solidRocketBooster.js";
 
-export function decorator2() {}
 
-export function decorator3() {}
+export function decorator1(target: object, propertyKey: string, descriptor: PropertyDescriptor) {}
 
-export function decorator4() {}
+
+export function decorator2(target: object, methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (flyerWeight?: number) {
+        let originalResult = originalMethod.call(this);
+        const optimalWeight = (this as SolidRocketBooster).optimalWeight;
+
+        if (optimalWeight != undefined && flyerWeight != undefined && flyerWeight > optimalWeight) {
+            originalResult *= 0.5;
+        }
+
+        return originalResult;
+    }
+}
+
+export function decorator3(target: object, propertyKey: string, descriptor: PropertyDescriptor) {}
+
+export function decorator4(target: object, propertyKey: string, descriptor: PropertyDescriptor) {}
