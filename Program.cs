30abcfa79d039f409b611d63d2e6ef0012d02966
Solenkoff <solenkoff@gmail.com>
2025-10-01 22:27:34 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -45,6 +45,12 @@ namespace _05_Fashion_Boutique
                 racks++;
             }
 
+             if (sum > 11)
+            {
+                racks++;
+                racks--;
+            }
+
             Console.WriteLine(racks);
 
         }
