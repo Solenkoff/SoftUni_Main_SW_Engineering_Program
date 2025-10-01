8570e9d52683f0f671cc40789124561dc024cb9c
Solenkoff <solenkoff@gmail.com>
2025-10-01 23:04:43 +0300

    Change in VSCode

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace _05_Fashion_Boutique
     {
         static void Main(string[] args)
         {
-            string  newVariableWithChangeInVSCode = "Added change from second DevOps";
+            string  newChangeInVSCode = "Added change from second DevOps";
 
             int[] input = Console.ReadLine()
                                  .Split(' ', StringSplitOptions.RemoveEmptyEntries)
