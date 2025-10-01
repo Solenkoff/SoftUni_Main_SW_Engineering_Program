6228bf63dab32bf9809ea210b2938e3dff8d9d3f
Solenkoff <solenkoff@gmail.com>
2025-10-01 22:35:38 +0300

    Added variable as a change

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace _05_Fashion_Boutique
     {
         static void Main(string[] args)
         {
+            string  newVariable = "Added change from second DevOps";
 
             int[] input = Console.ReadLine()
                                  .Split(' ', StringSplitOptions.RemoveEmptyEntries)
