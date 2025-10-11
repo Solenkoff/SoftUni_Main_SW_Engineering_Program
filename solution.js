31d0101de3f812c358fb586149a54a635ff69ee2
sevenoff7777 <sevenoff7777@proton.me>
2025-10-11 23:48:38 +0300

    Implemented Confirm Reservation form functionality

--- solution.js
+++ solution.js
@@ -58,6 +58,7 @@ function findRoom(e) {
     reservation.roomType = roomInfo;
     console.log(reservation);
     changeContent('guest-details-form-content');
+}
 
 document.querySelector('#guest-details-back-btn').addEventListener('click', (e) => fillRoomForm(e));
 
@@ -111,4 +112,18 @@ function searchFormData(e) {
         console.log(reservation);
         changeContent('search-result-form-content');
     }
+}
+
+document.querySelector('#confirm-back-btn').addEventListener('click', (e) => getBackToPersonalData(e));
+
+function getBackToPersonalData(e) {
+    e.preventDefault();
+    changeContent('guest-details-form-content');
+}
+
+document.querySelector('#confirm-reservation').addEventListener('click', (e) => showThanksPage(e));
+
+function showThanksPage(e) {
+    e.preventDefault();
+    changeContent('thank-you-content');
 }
