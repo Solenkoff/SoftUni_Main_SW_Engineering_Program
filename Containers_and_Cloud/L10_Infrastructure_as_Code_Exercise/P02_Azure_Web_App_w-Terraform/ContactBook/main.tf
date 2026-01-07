48dcd422b25f0bfb1ae36b5320f278d50dba05b4
Solenkoff <solenkoff@yahoo.com>
2026-01-07 13:34:44 +0200

    Add problem solution

---
+++ Containers_and_Cloud/L10_Infrastructure_as_Code_Exercise/P02_Azure_Web_App_w-Terraform/ContactBook/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.54.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+  subscription_id = "a807f40c-6671-4b6e-9c3e-411f84153bba"
+}
+
+resource "random_integer" "ri" {
+  min = 1
+  max = 9999
+}
+
+resource "azurerm_resource_group" "arg" {
+  name     = "ContactBook${random_integer.ri.result}"
+  location = "Poland Central"
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = "ContactBookServicePlan${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_resource_group.arg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "alwa" {
+  name                = "ContactBookApp${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_service_plan.asp.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack {
+      node_version = "16-lts"
+    }
+    always_on = false
+  }
+}
+
+resource "azurerm_app_service_source_control" "aassc" {
+  app_id                 = azurerm_linux_web_app.alwa.id
+  repo_url               = "https://github.com/nakov/ContactBook"
+  branch                 = "master"
+  use_manual_integration = true
+}
+
