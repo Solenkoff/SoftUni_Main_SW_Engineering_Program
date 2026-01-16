63f85bd23b9958a0b6ace1ad172e969b8c10c8f4
Solenkoff <solenkoff@yahoo.com>
2026-01-16 12:23:43 +0200

    Add problem solution

---
+++ Containers_and_Cloud/Exams/2025-12-13_CC_Exam_M/P02_ASP_NET_Core_MVC_app_to_Azure_via_Terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "app_url" {
+  description = "The fully qualified domain name of the Linux Web App"
+  value       = azurerm_linux_web_app.alwa.default_hostname
+}
+
+output "IP_address" {
+  description = "The IP address of the Web App"
+  value       = azurerm_linux_web_app.alwa.outbound_ip_addresses
+}    
