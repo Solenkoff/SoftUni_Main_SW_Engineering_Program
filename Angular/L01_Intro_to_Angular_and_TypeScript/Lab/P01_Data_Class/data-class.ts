0ebfbb2f4f646afae5772c478ba7011e058aa589
Solenkoff <solenkoff@yahoo.com>
2026-03-19 18:34:09 +0200

    New course folder with Lab submissions

---
+++ Angular/L01_Intro_to_Angular_and_TypeScript/Lab/P01_Data_Class/data-class.ts
@@ -0,0 +1,25 @@
+class Data {
+    method: string;
+    uri: string;
+    version: string;
+    message: string;
+    response: string | undefined;
+    fulfilled: boolean;
+
+    constructor(
+        method: string,
+        uri: string,
+        version: string,
+        message: string,
+    ){
+        this.method = method;
+        this.uri = uri;
+        this.version = version;
+        this.message = message;
+        this.response = undefined;
+        this.fulfilled = false;
+    }
+}
+
+const result = new Data('GET', 'http://google.com', 'HTTP/1.1', ''); 
+console.log(result);
