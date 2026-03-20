194383df90d4294bc9a0bbb556bf23152f9b91c7
Solenkoff <solenkoff@yahoo.com>
2026-03-20 21:43:28 +0200

    Create my first Angular app

---
+++ Angular/L01_Intro_to_Angular_and_TypeScript/my-first-app/src/app/hello/hello.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.html',
+  styleUrl: './hello.css',
+  standalone: true,
+})
+export class Hello {}
