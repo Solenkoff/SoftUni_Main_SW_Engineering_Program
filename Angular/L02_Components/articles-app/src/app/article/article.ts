5b8d2086971346884b4147747eb715104f150cf8
Solenkoff <solenkoff@yahoo.com>
2026-03-21 17:13:10 +0200

    Add step 4.2-Create the Needed Properties

--- Angular/L02_Components/articles-app/src/app/article/article.ts
+++ Angular/L02_Components/articles-app/src/app/article/article.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ArticleModel } from '../models/article.model';
 
 @Component({
-  selector: 'app-article',
-  imports: [],
-  templateUrl: './article.html',
-  styleUrl: './article.css',
+    selector: 'app-article',
+    imports: [],
+    templateUrl: './article.html',
+    styleUrl: './article.css',
 })
-export class Article {}
+export class Article {
+    private symbols: number = 250;
+    @Input() article!: ArticleModel;
+    @Input() articleDesc!: string;
+    descToShow: string;
+    articleDescLen: number;
+    showReadMoreBtn: boolean = true;
+    showHideBtn: boolean = false;
+    imageIsShown: boolean = false;
+    imageButtonTitle: string = "Show Image";
+
+        constructor() {
+        this.articleDescLen = 0;
+        this.descToShow = '';
+    }
+
+}
