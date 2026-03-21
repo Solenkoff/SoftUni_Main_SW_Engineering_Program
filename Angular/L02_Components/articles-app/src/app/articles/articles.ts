8a3d2c1a6f1c5bfb1d38b68b23188d0c6c759e04
Solenkoff <solenkoff@yahoo.com>
2026-03-21 18:26:41 +0200

    Add step 5.1-Implement Logic Inside the Component

--- Angular/L02_Components/articles-app/src/app/articles/articles.ts
+++ Angular/L02_Components/articles-app/src/app/articles/articles.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../article/article';
+import { ArticleData } from '../data/data';
+import { ArticleModel } from '../models/article.model';
 
 @Component({
-  selector: 'app-articles',
-  imports: [],
-  templateUrl: './articles.html',
-  styleUrl: './articles.css',
+    selector: 'app-articles',
+    imports: [Article],
+    templateUrl: './articles.html',
+    styleUrl: './articles.css',
 })
-export class Articles {}
+
+export class Articles implements OnInit {
+    articles!: ArticleModel[];
+    constructor() { }
+
+    ngOnInit() {
+        this.articles = new ArticleData().getData();
+    }
+}
